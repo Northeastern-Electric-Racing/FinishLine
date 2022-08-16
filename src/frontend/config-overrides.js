930e43cc9ee6ecd75239b9b9eafa6f37799b169a
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-08-16 12:31:16 -0400

    #125 - run prettier on all files

--- src/frontend/config-overrides.js
+++ src/frontend/config-overrides.js
@@ -3,14 +3,14 @@ const path = require('path');
 const fs = require('fs');
 const { override, babelInclude } = require('customize-cra');
 module.exports = (config, env) => {
-    return Object.assign(
-        config,
-        override(
-            /* Makes sure Babel compiles the stuff in the common folder */
-            babelInclude([
-                path.resolve('src'),
-                fs.realpathSync('../shared/src'), // THIS
-            ])
-        )(config, env)
-    );
+  return Object.assign(
+    config,
+    override(
+      /* Makes sure Babel compiles the stuff in the common folder */
+      babelInclude([
+        path.resolve('src'),
+        fs.realpathSync('../shared/src') // THIS
+      ])
+    )(config, env)
+  );
 };
