544bc044752a2a52ba872d963bee091e500c8362
O'Neal Kpodar <onealkpodar@ONeals-MacBook-Air.local>
2022-08-30 20:10:33 -0400

    Added button

--- src/frontend/src/layouts/PageBlock.tsx
+++ src/frontend/src/layouts/PageBlock.tsx
@@ -3,7 +3,7 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { Card } from 'react-bootstrap';
 import { useTheme } from '../hooks/Theme.hooks';
 
@@ -15,6 +15,29 @@ interface PageBlockProps {
 const styles = {
   header: {
     overflow: 'hidden'
+  },
+  'down-arrow': {
+    border: 'solid black',
+    'border-width': '0 2px 2px 0',
+    display: 'inline-block',
+    padding: '5px',
+    transform: 'rotate(45deg)',
+    '-webkit-transform': 'rotate(45deg)',
+    cursor: 'pointer',
+    'border-radius': '0px 0px 2px 0px',
+    'border-color': 'white',
+    'margin-top': '7px'
+  },
+  'up-arrow': {
+    border: 'solid black',
+    'border-width': '0 2px 2px 0',
+    display: 'inline-block',
+    padding: '5px',
+    transform: 'rotate(-135deg)',
+    '-webkit-transform': 'rotate(-135deg)',
+    cursor: 'pointer',
+    'border-radius': '0px 0px 2px 0px',
+    'margin-top': '7px'
   }
 };
 
@@ -25,15 +48,21 @@ const styles = {
  */
 const PageBlock: React.FC<PageBlockProps> = ({ title, headerRight, children }) => {
   const theme = useTheme();
+  const [collapse, setCollapse] = useState(false);
 
   return (
     <Card className={'mb-3'} border={theme.cardBorder} bg={theme.cardBg}>
       <Card.Body>
         <Card.Title style={styles.header}>
           <h5 className={'float-left mb-0'}>{title}</h5>
+          <i
+            style={collapse ? styles['up-arrow'] : styles['down-arrow']}
+            className={'float-right mx-3 cursor-pointer'}
+            onClick={() => setCollapse(!collapse)}
+          />
           <div className={'float-right'}>{headerRight}</div>
         </Card.Title>
-        {children}
+        {collapse ? '' : children}
       </Card.Body>
     </Card>
   );
