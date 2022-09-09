30f2a6ee067b82b95770e73b12af002ddf101916
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2022-09-08 23:28:32 -0400

    #83 - Added License text

--- src/frontend/src/pages/ChangeRequestDetailPage/ProposedSolutionsList.tsx
+++ src/frontend/src/pages/ChangeRequestDetailPage/ProposedSolutionsList.tsx
@@ -1,3 +1,8 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
 import { ProposedSolution } from 'shared';
 import ProposedSolutionForm from './ProposedSolutionForm';
 import { useState } from 'react';
