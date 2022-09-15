84c6ee4ed86e69107bbce40cdc3a770d4aa9c0ac
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-09-15 12:43:06 -0400

    #142: apply MUI grid to create WP form

--- src/frontend/src/pages/CreateWorkPackagePage/CreateWPFormView.tsx
+++ src/frontend/src/pages/CreateWorkPackagePage/CreateWPFormView.tsx
@@ -5,17 +5,18 @@
 
 import { useState } from 'react';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import InputAdornment from '@mui/material/InputAdornment';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { routes } from '../../utils/Routes';
+import { datePipe } from '../../utils/Pipes';
 import { EditableTextInputListUtils, FormStates } from './CreateWPForm';
 import EditableTextInputList from '../../components/EditableTextInputList';
 import PageTitle from '../../layouts/PageTitle/PageTitle';
 import PageBlock from '../../layouts/PageBlock';
-import { datePipe } from '../../utils/Pipes';
 
 interface CreateWPFormViewProps {
   states: FormStates;
@@ -52,93 +53,102 @@ const CreateWPFormView: React.FC<CreateWPFormViewProps> = ({
       />
       <PageBlock title={''}>
         <form onSubmit={onSubmit}>
-          <TextField
-            required
-            id="name"
-            name="name"
-            type="text"
-            label="Work Package Name"
-            placeholder="Enter work package name..."
-            onChange={(e) => name(e.target.value)}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={9}>
+              <TextField
+                required
+                fullWidth
+                id="name"
+                name="name"
+                type="text"
+                label="Work Package Name"
+                placeholder="Enter work package name..."
+                onChange={(e) => name(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                required
+                id="crId"
+                name="crId"
+                type="text"
+                label="Change Request ID"
+                placeholder="Enter change request ID..."
+                onChange={(e) => crId(parseInt(e.target.value))}
+                inputProps={{ inputMode: 'numeric', pattern: '[1-9][0-9]*' }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="wbsNum"
+                name="wbsNum"
+                type="text"
+                label="Project WBS Number"
+                placeholder="Enter project WBS number..."
+                onChange={(e) => wbsNum(e.target.value)}
+              />
 
-          <TextField
-            required
-            id="wbsNum"
-            name="wbsNum"
-            type="text"
-            label="Project WBS Number"
-            placeholder="Enter project WBS number..."
-            onChange={(e) => wbsNum(e.target.value)}
-          />
-          <TextField
-            required
-            id="crId"
-            name="crId"
-            type="text"
-            label="Change Request ID"
-            placeholder="Enter change request ID..."
-            onChange={(e) => crId(parseInt(e.target.value))}
-            inputProps={{ inputMode: 'numeric', pattern: '[1-9][0-9]*' }}
-          />
+              <DatePicker
+                label="Start Date"
+                inputFormat="yyyy-MM-dd"
+                value={startDateVal}
+                onChange={(val) => {
+                  setStartDateVal(val);
+                  startDate(datePipe(val!));
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
 
-          <DatePicker
-            label="Start Date"
-            inputFormat="yyyy-MM-dd"
-            value={startDateVal}
-            onChange={(val) => {
-              setStartDateVal(val);
-              startDate(datePipe(val!));
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          />
+              <TextField
+                required
+                id="duration"
+                name="duration"
+                type="text"
+                label="Duration"
+                placeholder="Enter duration..."
+                onChange={(e) => duration(parseInt(e.target.value))}
+                inputProps={{
+                  inputMode: 'numeric',
+                  pattern: '[1-9][0-9]*'
+                }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">weeks</InputAdornment>
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Box marginBottom={1}>
+                <Typography variant="caption">Dependencies</Typography>
+              </Box>
+              <EditableTextInputList
+                items={dependencies}
+                add={depUtils.add}
+                remove={depUtils.remove}
+                update={depUtils.update}
+              />
 
-          <TextField
-            required
-            id="duration"
-            name="duration"
-            type="text"
-            label="Duration"
-            placeholder="Enter duration..."
-            onChange={(e) => duration(parseInt(e.target.value))}
-            inputProps={{
-              inputMode: 'numeric',
-              pattern: '[1-9][0-9]*'
-            }}
-            InputProps={{
-              endAdornment: <InputAdornment position="end">weeks</InputAdornment>
-            }}
-          />
+              <Box marginBottom={1}>
+                <Typography variant="caption">Expected Activities</Typography>
+              </Box>
+              <EditableTextInputList
+                items={expectedActivities}
+                add={eaUtils.add}
+                remove={eaUtils.remove}
+                update={eaUtils.update}
+              />
 
-          <Box marginBottom={1}>
-            <Typography variant="caption">Dependencies</Typography>
-          </Box>
-          <EditableTextInputList
-            items={dependencies}
-            add={depUtils.add}
-            remove={depUtils.remove}
-            update={depUtils.update}
-          />
-
-          <Box marginBottom={1}>
-            <Typography variant="caption">Expected Activities</Typography>
-          </Box>
-          <EditableTextInputList
-            items={expectedActivities}
-            add={eaUtils.add}
-            remove={eaUtils.remove}
-            update={eaUtils.update}
-          />
-
-          <Box marginBottom={1}>
-            <Typography variant="caption">Deliverabless</Typography>
-          </Box>
-          <EditableTextInputList
-            items={deliverables}
-            add={delUtils.add}
-            remove={delUtils.remove}
-            update={delUtils.update}
-          />
+              <Box marginBottom={1}>
+                <Typography variant="caption">Deliverabless</Typography>
+              </Box>
+              <EditableTextInputList
+                items={deliverables}
+                add={delUtils.add}
+                remove={delUtils.remove}
+                update={delUtils.update}
+              />
+            </Grid>
+          </Grid>
 
           <Box display="flex" flexDirection="row-reverse" gap={2}>
             <Button variant="contained" color="primary" type="submit" disabled={!allowSubmit}>
