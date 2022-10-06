8fdf669e31e3f04168e2ff8dce88930ec2a308ba
Peyton-McKee <mckee.p@northeastern.edu>
2022-10-05 22:25:03 -0400

    #194 updated function call

--- src/backend/src/routes/description-bullets.routes.ts
+++ src/backend/src/routes/description-bullets.routes.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { editDescriptionBullets } from '../utils/projects.utils';
+import { checkWorkPackageDescriptionBullet } from '../controllers/Description-Bullets.controllers';
 
 const descriptionBulletsRouter = express.Router();
 
-descriptionBulletsRouter.post("\check",
-   body('submitterId').isInt({ min: 0 }).not().isString(),
-   body('descriptionBulletId').isInt({ min: 0 }).not().isString(),
-   editDescriptionBullets
+descriptionBulletsRouter.post(
+  '/check',
+  body('submitterId').isInt({ min: 0 }).not().isString(),
+  body('descriptionBulletId').isInt({ min: 0 }).not().isString(),
+  checkWorkPackageDescriptionBullet
 );
 
+export default descriptionBulletsRouter;
