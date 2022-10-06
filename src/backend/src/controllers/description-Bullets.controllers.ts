f7201f6d5bec2c0ff6d559006d12aea2951b8c6a
Peyton-McKee <mckee.p@northeastern.edu>
2022-10-05 22:22:55 -0400

    #194 Created Endpoint description-bullets/check

---
+++ src/backend/src/controllers/description-Bullets.controllers.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+
+export const checkWorkPackageDescriptionBullet = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { body } = req;
+  const {
+    submitterId,
+    descriptionBulletId,
+  } = body;
+
+  return res.status(200).json(body);
+
+};
