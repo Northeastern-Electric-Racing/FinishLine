44cbeaadc163eb9a61d7429c8e36090cb0e3ad6a
Anurag Arasan <anurag.arasan@outlook.com>
2022-10-09 19:19:57 -0400

    updated with main

---
+++ src/frontend/src/pages/CreateChangeRequestPage/CreateProposedSolutionsList.tsx
@@ -0,0 +1,57 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import { ProposedSolution } from 'shared';
+import ProposedSolutionForm from '../ChangeRequestDetailPage/ProposedSolutionForm';
+import { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import ProposedSolutionView from '../ChangeRequestDetailPage/ProposedSolutionView';
+import styles from '../../stylesheets/pages/ChangeRequestDetailPage/ProposedSolutionsList.module.css';
+
+interface CreateProposedSolutionsListProps {
+  proposedSolutions: ProposedSolution[];
+  setProposedSolutions: (ps: ProposedSolution[]) => void;
+}
+
+const CreateProposedSolutionsList: React.FC<CreateProposedSolutionsListProps> = ({
+  proposedSolutions,
+  setProposedSolutions
+}) => {
+  const [showEditableForm, setShowEditableForm] = useState<boolean>(false);
+
+  const addProposedSolution = async (data: ProposedSolution) => {
+    setProposedSolutions([...proposedSolutions, data]);
+    setShowEditableForm(false);
+  };
+
+  const removeProposedSolution = (data: ProposedSolution) => {
+    setProposedSolutions(proposedSolutions.filter((proposedSolution) => proposedSolution !== data));
+  };
+
+  return (
+    <>
+      <Button onClick={() => setShowEditableForm(true)} variant="success" className="mb-3">
+        + Add Proposed Solution
+      </Button>
+      <div className={styles.proposedSolutionsList}>
+        {proposedSolutions.map((proposedSolution, i) => (
+          <ProposedSolutionView
+            key={i}
+            proposedSolution={proposedSolution}
+            onDelete={removeProposedSolution}
+            showDeleteButton
+          />
+        ))}
+      </div>
+      {showEditableForm ? (
+        <Modal size="xl" centered show={showEditableForm} onHide={() => setShowEditableForm(false)}>
+          <ProposedSolutionForm onAdd={addProposedSolution} />
+        </Modal>
+      ) : null}
+    </>
+  );
+};
+
+export default CreateProposedSolutionsList;
