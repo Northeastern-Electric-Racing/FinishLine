b9cc7415cf04f5a9ca228fbe15c9b91717e11119
Jared Lyon <jaredalyon@gmail.com>
2022-10-12 20:07:21 -0400

    #244 - Add tests for editRisk endpoints

--- src/backend/tests/risks.test.ts
+++ src/backend/tests/risks.test.ts
@@ -6,17 +6,18 @@ const app = express();
 app.use(express.json());
 app.use('/', riskRouter);
 
+// requested edits
 const editRiskTrue = {
   userId: 1,
-  id: 'asdf',
-  detail: 'build a car',
+  id: '4800c304-2f8d-4376-a4c6-d7a157134c83',
+  detail: 'This one might be a bit too expensive',
   resolved: true
 };
 
 const editRiskFalse = {
   userId: 1,
-  id: 'asdf',
-  detail: 'build a car',
+  id: '4800c304-2f8d-4376-a4c6-d7a157134c83',
+  detail: 'This one might be a bit too expensive',
   resolved: false
 };
 
@@ -24,24 +25,24 @@ describe('Risks', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  test('the original risk was resolved and the payload is trying to unresolve it', async () => {
-    const risk = { ...editRiskTrue, resolved: false };
-    const res = await request(app).post('/edit').send(risk);
-
-    expect(res.statusCode).toBe(400);
-  });
 
   test(`the original risk wasn't resolved and the payload is trying to resolve it`, async () => {
-    const risk = { ...editRiskFalse, resolved: true };
-    const res = await request(app).post('/edit').send(risk);
+    // const risk = { ...editRiskTrue};
+    const res = await request(app).post('/edit').send(editRiskTrue);
 
-    expect(res.statusCode).toBe(400);
+    expect(res.body.isResolved).toBe(true);
+  });
+
+  test('the original risk was resolved and the payload is trying to unresolve it', async () => {
+    // const risk = { ...editRiskTrue};
+    const res = await request(app).post('/edit').send(editRiskFalse);
+
+    expect(res.body.isResolved).toBe(false);
   });
 
   test('the original risk and payload have the same resolved value', async () => {
-    const risk = { ...editRiskTrue, resolved: false };
-    const res = await request(app).post('/edit').send(risk);
+    const res = await request(app).post('/edit').send(editRiskFalse);
 
-    expect(res.statusCode).toBe(400);
+    expect(res.body.isResolved).toBe(false);
   });
 });
