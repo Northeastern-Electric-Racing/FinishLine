73b5e162f7aadf2043cca5b9b925d49861cfcc36
Jared Lyon <jaredalyon@gmail.com>
2022-10-19 18:26:48 -0400

    #244 move constants to test data file

--- src/backend/tests/risks.test.ts
+++ src/backend/tests/risks.test.ts
@@ -1,24 +1,11 @@
 import request from 'supertest';
 import express from 'express';
 import riskRouter from '../src/routes/risks.routes';
+import { editRiskTrue, editRiskFalse } from './test-data/risks.test-data';
 
 const app = express();
 app.use(express.json());
 app.use('/', riskRouter);
-
-const editRiskTrue = {
-  userId: 1,
-  id: '4800c304-2f8d-4376-a4c6-d7a157134c83',
-  detail: 'This one might be a bit too expensive',
-  resolved: true
-};
-
-const editRiskFalse = {
-  userId: 1,
-  id: '4800c304-2f8d-4376-a4c6-d7a157134c83',
-  detail: 'This one might be a bit too expensive',
-  resolved: false
-};
 
 describe('Risks', () => {
   afterEach(() => {
---
+++ src/backend/tests/test-data/risks.test-data.ts
@@ -0,0 +1,13 @@
+export const editRiskTrue = {
+  userId: 1,
+  id: '4800c304-2f8d-4376-a4c6-d7a157134c83',
+  detail: 'This one might be a bit too expensive',
+  resolved: true
+};
+
+export const editRiskFalse = {
+  userId: 1,
+  id: '4800c304-2f8d-4376-a4c6-d7a157134c83',
+  detail: 'This one might be a bit too expensive',
+  resolved: false
+};
