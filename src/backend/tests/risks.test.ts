638ac758d0e681d5e3dfe119ec65e73dd16aee7b
Chloe <113949833+chloezhong3@users.noreply.github.com>
2022-10-19 19:14:51 -0400

    #239:TestgetRisksForProjectEndpoint

---
+++ src/backend/tests/risks.test.ts
@@ -0,0 +1,69 @@
+import request from 'supertest';
+import express from 'express';
+import risksRouter from '../src/routes/risks.routes';
+import prisma from '../src/prisma/prisma';
+// import { Role } from '@prisma/client';
+// import { getChangeRequestReviewState, getHighestProjectNumber } from '../src/utils/projects.utils';
+
+const app = express();
+app.use(express.json());
+app.use('/', risksRouter);
+
+describe('Risks', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('test1', async () => {
+    const res = await request(app).get('/1');
+    console.log(res.body);
+    expect(res.body).toStrictEqual([
+      {
+        id: '458c5e95-b06c-47d9-8399-dcd409f9ff5a',
+        project: {
+          id: 1,
+          name: 'Impact Attenuator',
+          wbsNum: { carNumber: 1, projectNumber: 1, workPackageNumber: 0 }
+        },
+        detail: 'This one might be a bit too expensive',
+        isResolved: false,
+        dateCreated: '2022-09-20T00:25:53.207Z',
+        createdBy: {
+          userId: 1,
+          firstName: 'Thomas',
+          lastName: 'Emrax',
+          email: 'emrax.t@husky.neu.edu',
+          emailId: 'emrax.t',
+          role: 'APP_ADMIN'
+        }
+      },
+      {
+        id: '68209f03-5a6a-461b-85db-ebc02e419f61',
+        project: {
+          id: 1,
+          name: 'Impact Attenuator',
+          wbsNum: { carNumber: 1, projectNumber: 1, workPackageNumber: 0 }
+        },
+        detail: 'Risky Risk 123',
+        isResolved: false,
+        dateCreated: '2022-09-20T00:25:53.212Z',
+        createdBy: {
+          userId: 1,
+          firstName: 'Thomas',
+          lastName: 'Emrax',
+          email: 'emrax.t@husky.neu.edu',
+          emailId: 'emrax.t',
+          role: 'APP_ADMIN'
+        }``
+      }
+    ]);
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('test2', async () => {
+    const res = await request(app).get('/123123123');
+    console.log(res.body);
+    expect(res.body.message).toBe('Project with id 123123123 not found!');
+    expect(res.statusCode).toBe(404);
+  });
+});
