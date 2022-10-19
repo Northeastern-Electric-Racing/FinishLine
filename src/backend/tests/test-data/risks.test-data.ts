17a063faafd3d295e1062b945907d72ecc2d4f4d
Jared Lyon <jaredalyon@gmail.com>
2022-10-19 19:30:48 -0400

    #244 update tests

--- src/backend/tests/test-data/risks.test-data.ts
+++ src/backend/tests/test-data/risks.test-data.ts
@@ -1,47 +1,27 @@
-export const editRiskTrue = {
+import { Risk } from '@prisma/client';
+
+export const editRiskTrue: Risk = {
   id: '56c939bf-d95a-4e43-b0bc-0f0581db9cfb',
-  project: {
-    id: 1,
-    name: 'Impact Attenuator',
-    wbsNum: {
-      carNumber: 1,
-      projectNumber: 1,
-      workPackageNumber: 0
-    }
-  },
+  projectId: 1,
   detail: 'This one might be a bit too expensive',
   isResolved: true,
-  dateCreated: '2022-10-19T22:50:45.580Z',
-  createdBy: {
-    userId: 1,
-    firstName: 'Thomas',
-    lastName: 'Emrax',
-    email: 'emrax.t@husky.neu.edu',
-    emailId: 'emrax.t',
-    role: 'APP_ADMIN'
-  }
+  dateDeleted: null,
+  dateCreated: new Date('2022-10-19T22:50:45.580Z'),
+  createdByUserId: 1,
+  resolvedByUserId: null,
+  resolvedAt: null,
+  deletedByUserId: null
 };
 
-export const editRiskFalse = {
+export const editRiskFalse: Risk = {
   id: '56c939bf-d95a-4e43-b0bc-0f0581db9cfb',
-  project: {
-    id: 1,
-    name: 'Impact Attenuator',
-    wbsNum: {
-      carNumber: 1,
-      projectNumber: 1,
-      workPackageNumber: 0
-    }
-  },
+  projectId: 1,
   detail: 'This one might be a bit too expensive',
   isResolved: false,
-  dateCreated: '2022-10-19T22:50:45.580Z',
-  createdBy: {
-    userId: 1,
-    firstName: 'Thomas',
-    lastName: 'Emrax',
-    email: 'emrax.t@husky.neu.edu',
-    emailId: 'emrax.t',
-    role: 'APP_ADMIN'
-  }
+  dateDeleted: null,
+  dateCreated: new Date('2022-10-19T22:50:45.580Z'),
+  createdByUserId: 1,
+  resolvedByUserId: null,
+  resolvedAt: null,
+  deletedByUserId: null
 };
