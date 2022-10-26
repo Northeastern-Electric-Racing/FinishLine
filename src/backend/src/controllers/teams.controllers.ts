01bf8defc6b819981f947a2f2a91154efba13c50
leshemo <81444861+leshemo@users.noreply.github.com>
2022-10-26 17:24:00 -0400

    #312-got-prisma-call-and-add-teams-test-file

--- src/backend/src/controllers/teams.controllers.ts
+++ src/backend/src/controllers/teams.controllers.ts
@@ -1,6 +1,7 @@
 import prisma from '../prisma/prisma';
 import { Request, Response } from 'express';
 import { teamRelationArgs, teamTransformer } from '../utils/teams.utils';
+import { projectTransformer } from '../utils/projects.utils';
 
 export const getAllTeams = async (_req: Request, res: Response) => {
   const teams = await prisma.team.findMany(teamRelationArgs);
@@ -18,4 +19,65 @@ export const getSingleTeam = async (req: Request, res: Response) => {
   }
 
   return res.status(200).json(teamTransformer(team));
+};
+
+export const getAllProjectsByTeam = async (req: Request, res: Response) => {
+  const team = await prisma.team.findUnique({
+    where: { teamId: req.params.teamId },
+    include: {
+      projects: {
+        include: {
+          wbsElement: {
+            include: {
+              projectLead: true,
+              projectManager: true,
+              changes: {
+                include: {
+                  implementer: true
+                }
+              }
+            }
+          },
+          team: true,
+          goals: true,
+          features: true,
+          risks: {
+            include: {
+              project: {
+                include: {
+                  wbsElement: true
+                }
+              },
+              createdBy: true,
+              resolvedBy: true,
+              deletedBy: true
+            }
+          },
+          otherConstraints: true,
+          workPackages: {
+            include: {
+              wbsElement: {
+                include: {
+                  projectLead: true,
+                  projectManager: true,
+                  changes: { include: { implementer: true } }
+                }
+              },
+              dependencies: true,
+              expectedActivities: true,
+              deliverables: true
+            }
+          }
+        }
+      }
+    }
+  });
+
+  if (!team) {
+    return res.status(404).json({ message: `Team with id ${req.params.teamId} not found!` });
+  }
+
+  const projectReturn = team.projects.map(projectTransformer);
+
+  return res.status(200).json(projectReturn);
 };
--- src/backend/src/routes/teams.routes.ts
+++ src/backend/src/routes/teams.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllTeams, getSingleTeam } from '../controllers/teams.controllers';
+import { getAllTeams, getSingleTeam, getAllProjectsByTeam } from '../controllers/teams.controllers';
 
 const teamsRouter = express.Router();
 
 teamsRouter.get('/', getAllTeams);
 teamsRouter.get('/:teamId', getSingleTeam);
+teamsRouter.get('/:teamId/projects', getAllProjectsByTeam);
 
 export default teamsRouter;
