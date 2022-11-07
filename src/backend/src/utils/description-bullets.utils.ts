03088b1c0eff897118e0339bda653c866bbb3a7d
Peyton-McKee <mckee.p@northeastern.edu>
2022-11-07 17:00:43 -0500

    #409 Allow project leads to check description bullets

--- src/backend/src/utils/description-bullets.utils.ts
+++ src/backend/src/utils/description-bullets.utils.ts
@@ -19,14 +19,29 @@ export const hasBulletCheckingPermissions = async (userId: number, descriptionId
   const user = await prisma.user.findUnique({ where: { userId } });
 
   const descriptionBullet = await prisma.description_Bullet.findUnique({
-    where: { descriptionId }
+    where: { descriptionId },
+    include: {
+      workPackageDeliverables: { include: { wbsElement: { include: { projectLead: true } } } },
+      workPackageExpectedActivities: { include: { wbsElement: {include: {projectLead: true } } } } 
+    }
   });
 
   if (!descriptionBullet) return false;
 
   if (!user) return false;
 
-  if (user.role === Role.APP_ADMIN || user.role === Role.ADMIN || user.role === Role.LEADERSHIP) {
+  const leader =
+    descriptionBullet.workPackageDeliverables?.wbsElement.projectLead ||
+    descriptionBullet.workPackageExpectedActivities?.wbsElement.projectLead;
+  
+  console.log(leader);
+  if (
+    user.role === Role.APP_ADMIN ||
+    user.role === Role.ADMIN ||
+    user.role === Role.LEADERSHIP ||
+    (leader &&
+      leader.userId === user.userId)
+  ) {
     return true;
   }
   return false;
