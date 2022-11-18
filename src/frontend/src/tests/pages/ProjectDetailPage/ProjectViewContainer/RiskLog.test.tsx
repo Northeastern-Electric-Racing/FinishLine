319c9d01ba8b5d46297198e5b111a0916e7da6c6
nezamj03 <jazayeri.ne@northeastern.edu>
2022-11-18 17:32:21 -0500

    #165: Fixed final Test import one again

--- src/frontend/src/tests/pages/ProjectDetailPage/ProjectViewContainer/RiskLog.test.tsx
+++ src/frontend/src/tests/pages/ProjectDetailPage/ProjectViewContainer/RiskLog.test.tsx
@@ -14,7 +14,7 @@ import { useAuth } from '../../../../hooks/auth.hooks';
 import { mockAuth, mockPromiseAxiosResponse } from '../../../test-support/test-data/test-utils.stub';
 import { exampleAdminUser, exampleGuestUser, exampleMemberUser } from '../../../test-support/test-data/users.stub';
 import { exampleProject1 } from '../../../test-support/test-data/projects.stub';
-import { getRisksForProject } from '../../../../apis/Risks.api';
+import { getRisksForProject } from '../../../../apis/risks.api';
 import { AxiosResponse } from 'axios';
 import { Risk } from 'shared';
 import { useGetRisksForProject } from '../../../../hooks/risks.hooks';
