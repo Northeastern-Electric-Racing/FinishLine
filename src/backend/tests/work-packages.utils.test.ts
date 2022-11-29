563dd8805bc927b7a050a640cbaac78eae36c171
ryan howe <ryan.wm.howe@gmail.com>
2022-11-28 19:34:19 -0500

    #288-so-i-can-pull

--- src/backend/tests/work-packages.utils.test.ts
+++ src/backend/tests/work-packages.utils.test.ts
@@ -2,9 +2,22 @@ import { prisma } from "@prisma/client";
 import { workPackageTransformer } from "../src/utils/work-packages.utils";
 import { createWorkPackagePayload } from "./test-data/work-packages.test-data";
 import * as utils from "../src/utils/work-packages.utils";
+import express from "express";
 
-// test('workPackageTransformer', () => {
-//     // const transformer = workPackageTransformer(createWorkPackagePayload);
-//     jest.spyOn(utils, 'workPackageTransformer').mockResolvedValue(createWorkPackagePayload);
+const app = express();
+app.use(express.json());
+
+describe('Work Package Utils', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('workPackageTransformer', () => {    
+    const workPackages1 = [createWorkPackagePayload]
+    // workPackages1.map(workPackageTransformer)
     
-//   });
+  
+    expect(workPackageTransformer).toBeCalledTimes(1);
+      
+    });
+})
