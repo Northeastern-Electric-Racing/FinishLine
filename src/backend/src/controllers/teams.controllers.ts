670869aa7d02002041a8881368605932e61a231b
Ji-min Kim <kimjimin1226@gmail.com>
2022-11-30 23:19:33 -0500

    #368-initial-draft-implementation-for-team-edit-endpoint

--- src/backend/src/controllers/teams.controllers.ts
+++ src/backend/src/controllers/teams.controllers.ts
@@ -1,6 +1,7 @@
 import prisma from '../prisma/prisma';
 import { Request, Response } from 'express';
 import { teamRelationArgs, teamTransformer } from '../utils/teams.utils';
+import { Role, User } from '@prisma/client';
 
 export const getAllTeams = async (_req: Request, res: Response) => {
   const teams = await prisma.team.findMany(teamRelationArgs);
@@ -18,4 +19,52 @@ export const getSingleTeam = async (req: Request, res: Response) => {
   }
 
   return res.status(200).json(teamTransformer(team));
+};
+
+export const setMembers = async (req: Request, res: Response) => {
+  const { body } = req;
+  const { userId, userIds } = body; // userId in body represents a submitter for team edit form
+  let missingUserId;
+  let newUsers: User[];
+
+  // find and vertify the given teamId exist
+  const team = await prisma.team.findUnique({
+    where: { teamId: req.params.teamId },
+    ...teamRelationArgs
+  });
+
+  if (!team) {
+    return res.status(404).json({ message: `Team with id ${req.params.teamId} not found!` });
+  }
+
+  // verify the submitter is allowed to edit team
+  const submitter = await prisma.user.findUnique({ where: { userId } });
+  if (!submitter) return res.status(404).json({ message: `user with id ${userId} not found` });
+  if (submitter.role !== Role.ADMIN || submitter !== team.leader) return res.status(403).json({ message: 'Access Denied' });
+
+  userIds.every(async (userId: number) => {
+    const user = await prisma.user.findUnique({ where: { userId } });
+
+    // vertify if the user exists
+    if (!user) {
+      // exit the every function
+      missingUserId = userId;
+      return false;
+    }
+
+    newUsers.push(user);
+    return true;
+  });
+
+  if (!missingUserId) return res.status(404).json({ message: `user with id ${missingUserId} not found` });
+
+  // update the team with the input fields
+  const updateTeam = await prisma.team.update({
+    where: {
+      teamId: req.params.teamId
+    },
+    data: {
+      members: { connect: newUsers }
+    }
+  });
 };
--- src/backend/src/routes/teams.routes.ts
+++ src/backend/src/routes/teams.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllTeams, getSingleTeam } from '../controllers/teams.controllers';
+import { getAllTeams, getSingleTeam, setMembers } from '../controllers/teams.controllers';
 
 const teamsRouter = express.Router();
 
 teamsRouter.get('/', getAllTeams);
 teamsRouter.get('/:teamId', getSingleTeam);
+teamsRouter.post('/:teamId/set-members', setMembers);
 
 export default teamsRouter;
