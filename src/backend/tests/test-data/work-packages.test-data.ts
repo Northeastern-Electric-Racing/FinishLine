cbeb5d7fe5ea29312727c66944ad6db40756f715
anthonybernardi <anthony.bernardi09@gmail.com>
2022-12-03 16:59:01 -0500

    #288: testing stuff

--- src/backend/tests/test-data/work-packages.test-data.ts
+++ src/backend/tests/test-data/work-packages.test-data.ts
@@ -1,11 +1,7 @@
 import { batman } from './users.test-data';
 import { WBS_Element_Status } from '@prisma/client';
-import { project1, wbsElement1 } from './projects.test-data';
 
 export const createWorkPackagePayload = {
-  projectId: 1,
-  workPackageId: 22,
-  wbsElementId: 5,
   projectWbsNum: {
     carNumber: 1,
     projectNumber: 2,
@@ -30,27 +26,3 @@ export const createWorkPackagePayload = {
   expectedActivities: ['ayo'],
   deliverables: ['ajdhjakfjafja']
 };
-
-export const createWorkPackage = {
-  workPackageId: 10,
-  wbsElemendId: wbsElement1.wbsElementId,
-  wbsElement: wbsElement1,
-  projectId: project1.projectId,
-  project: project1,
-  orderInProject: 7,
-  startDate: '2022-09-10',
-  duration: 10,
-  dependencies: [
-    {
-      wbsElementId: 65,
-      dateCreated: new Date('11/24/2021'),
-      carNumber: 1,
-      projectNumber: 1,
-      workPackageNumber: 1,
-      name: 'prereq',
-      status: WBS_Element_Status.COMPLETE
-    }
-  ],
-  expectedActivities: ['ayo'],
-  deliverables: ['nothing']
-}
