f2e0e73ed0b05244ed023b8ea10df4e337c72884
Zack Roth <zack27r@gmail.com>
2022-12-05 10:06:42 -0500

    #386 - Removed React Bootstrap import and changed some CSS

--- src/frontend/src/layouts/Sidebar/Sidebar.tsx
+++ src/frontend/src/layouts/Sidebar/Sidebar.tsx
@@ -3,14 +3,13 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Nav } from 'react-bootstrap';
 import { faExchangeAlt, faFolder, faHome, faQuestionCircle, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { routes } from '../../utils/Routes';
 import { LinkItem } from '../../utils/Types';
 import NavPageLinks from './NavPageLinks';
 import styles from '../../stylesheets/layouts/sidebar/sidebar.module.css';
 
-const Sidebar: React.FC = () => {
+const Sidebar = () => {
   const linkItems: LinkItem[] = [
     {
       name: 'Home',
@@ -39,9 +38,9 @@ const Sidebar: React.FC = () => {
     }
   ];
   return (
-    <Nav className={styles.sidebar}>
+    <div className={styles.sidebar}>
       <NavPageLinks linkItems={linkItems} />
-    </Nav>
+    </div>
   );
 };
 
