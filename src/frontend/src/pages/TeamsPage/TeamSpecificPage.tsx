3bbcccbbbc4a05dd25f899244fbdec8d513ed466
Peyton-McKee <mckee.p@northeastern.edu>
2022-12-08 11:56:06 -0500

    #359 Small updates

--- src/frontend/src/pages/TeamsPage/TeamSpecificPage.tsx
+++ src/frontend/src/pages/TeamsPage/TeamSpecificPage.tsx
@@ -6,17 +6,15 @@ import ErrorPage from '../ErrorPage';
 import PageBlock from '../../layouts/PageBlock';
 import { useParams } from 'react-router-dom';
 import { fullNamePipe } from '../../utils/Pipes';
-import { Block } from '@mui/icons-material';
-import { spacing } from '@mui/system';
+import ReactMarkdown from 'react-markdown';
+
+interface ParamTypes {
+  teamId: string;
+}
 
 const TeamSpecificPage: React.FC = () => {
-  interface ParamTypes {
-    teamId: string;
-  }
   const { teamId } = useParams<ParamTypes>();
-
   const { isLoading, isError, data, error } = useSingleTeam(teamId);
-
   if (isError) return <ErrorPage message={error?.message} />;
   if (isLoading || !data) return <LoadingIndicator />;
 
@@ -32,19 +30,18 @@ const TeamSpecificPage: React.FC = () => {
                 <p style={{ display: 'inline' }}>{fullNamePipe(data.leader)}</p>
               </Grid>
               <Grid item xs={12} md={12}>
-                <b style={{ display: 'inline'}}>Members: </b>
+                <b style={{ display: 'inline' }}>Members: </b>
                 <p style={{ display: 'inline' }}>
-                  {data.members.map((member, idx) => {
-                    const seperator = idx === data.members.length - 1 ? '' : ', ';
-                    return fullNamePipe(member) + seperator;
-                  })}
+                  {data.members.map((member) => {
+                    return fullNamePipe(member)
+                  }).join(', ')}
                 </p>
               </Grid>
             </Grid>
           </PageBlock>
           <PageBlock title={'Active Projects'}></PageBlock>
           <PageBlock title={'Description'}>
-            <p>{data.description}</p>
+            <ReactMarkdown>{data.description}</ReactMarkdown>
           </PageBlock>
         </Grid>
       </Grid>
