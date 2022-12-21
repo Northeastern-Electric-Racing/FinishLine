887fec6289123c90625cf2392bfb20f915a9b472
ryan howe <ryan.wm.howe@gmail.com>
2022-12-20 22:26:46 -0500

    #268: wrapped link and tag in div

--- src/frontend/src/pages/HomePage/UsefulLinks.tsx
+++ src/frontend/src/pages/HomePage/UsefulLinks.tsx
@@ -13,40 +13,77 @@ import {
   CurrencyExchange,
   AttachMoney
 } from '@mui/icons-material';
-import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import { Grid } from '@mui/material';
-import ExternalLink from '../../components/ExternalLink';
 import PageBlock from '../../layouts/PageBlock';
 
 const UsefulLinks: React.FC = () => {
   const theme = useTheme();
   const links = [
-    <Link href="https://docs.google.com/document/d/1M5Ldy9L1BifBo18tdKpv3CH-frRneyEK26hUXbtMg7Q/edit" 
-    underline="hover" fontSize={19}>
-      <ShoppingCart sx={{fontSize:17, color: theme.palette.text.primary}}></ShoppingCart>
-      Purchasing Guidelines
-    </Link>,
-    <Link href="https://docs.google.com/document/d/1HvLnVNzZTftgoAXppIEp-gTmUBQGt-V6n97prziWWrs/edit" underline="hover" fontSize={19}>
-      <CurrencyExchange sx={{fontSize:17, color:theme.palette.text.primary}}></CurrencyExchange>
-      Reimbursement Guidelines
-    </Link>,
-    <Link href="https://forms.gle/6ztRoa1iL7p1KHwP6" underline="hover" fontSize={19}>
-      <AttachMoney sx={{fontSize:17, color:theme.palette.text.primary}}></AttachMoney>
+    <>
+      <ShoppingCart sx={{ fontSize: 17, color: theme.palette.text.primary }} />
+      <Link
+        href="https://docs.google.com/document/d/1M5Ldy9L1BifBo18tdKpv3CH-frRneyEK26hUXbtMg7Q/edit"
+        underline="hover"
+        fontSize={19}
+        sx={{ pl: 1 }}
+      >
+        Purchasing Guidelines
+      </Link>
+    </>,
+    <>
+      <CurrencyExchange sx={{fontSize:17, color:theme.palette.text.primary}} />
+      <Link 
+        href="https://docs.google.com/document/d/1HvLnVNzZTftgoAXppIEp-gTmUBQGt-V6n97prziWWrs/edit" 
+        underline="hover" 
+        fontSize={19}
+        sx={{ pl: 1 }}
+        >
+          Reimbursement Guidelines
+        </Link>  
+    </>,
+    <>
+      <AttachMoney sx={{fontSize:17, color:theme.palette.text.primary}} />
+      <Link 
+        href="https://forms.gle/6ztRoa1iL7p1KHwP6"
+        underline="hover" 
+        fontSize={19}
+        sx={{ pl: 1 }}>
       Procurement Form
-    </Link>,
-    <Link href="https://docs.google.com/spreadsheets/d/1kqpnw8jZDx2GO5NFUtqefRXqT1XX46iMx5ZI4euPJgY/edit" underline="hover" fontSize={19}>
-      <Receipt sx={{fontSize:17, color:theme.palette.text.primary}}></Receipt>
-      McMaster Order Sheet
-    </Link>,
-    <Link href="https://docs.google.com/document/d/1w0B6upZRY28MlbVA4hyU3X_NRNP0cagmLWqjHn6B8OA/edit" underline="hover" fontSize={19}>
-      <Description sx={{fontSize:17, color:theme.palette.text.primary}}></Description>
-      Project Update Log
-    </Link>,
-    <Link href="https://nerdocs.atlassian.net/wiki/spaces/NER/pages/4554841/Hardware+Guidelines" underline="hover" fontSize={19}>
-      <Settings sx={{fontSize:17, color:theme.palette.text.primary}}></Settings>
-      Hardware Guidelines
-    </Link>
+      </Link>
+    </>,
+    <>
+      <Receipt sx={{fontSize:17, color:theme.palette.text.primary}} />
+      <Link
+        href="https://docs.google.com/spreadsheets/d/1kqpnw8jZDx2GO5NFUtqefRXqT1XX46iMx5ZI4euPJgY/edit"
+        underline='hover'
+        fontSize={19}
+        sx={{ pl: 1 }}>
+        McMaster Order Sheet
+      </Link>
+    </>,
+    <>
+      <Description sx={{fontSize:17, color:theme.palette.text.primary}} />
+      <Link
+      href="https://docs.google.com/document/d/1w0B6upZRY28MlbVA4hyU3X_NRNP0cagmLWqjHn6B8OA/edit"
+      underline='hover'
+      fontSize={19}
+      sx={{ pl: 1 }}
+      >
+        Project Update Log
+      </Link>
+    </>,
+    <>
+      <Settings sx={{fontSize:17, color:theme.palette.text.primary}} />
+      <Link
+      href="https://nerdocs.atlassian.net/wiki/spaces/NER/pages/4554841/Hardware+Guidelines"
+      underline='hover'
+      fontSize={19}
+      sx={{ pl: 1 }}
+      >
+        Hardware Guidelines
+      </Link>
+    </>
   ];
 
   return (
