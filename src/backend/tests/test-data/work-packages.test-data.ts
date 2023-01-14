f8c20d795a9b06e73041f868fa9b592f74a05c93
Peyton-McKee <mckee.p@northeastern.edu>
2023-01-14 17:22:04 -0500

    #354 fix test

--- src/backend/tests/test-data/work-packages.test-data.ts
+++ src/backend/tests/test-data/work-packages.test-data.ts
@@ -1,10 +1,23 @@
-import { Work_Package as PrismaWorkPackage } from '@prisma/client';
+import { Prisma } from '@prisma/client';
+import workPackageQueryArgs from '../../src/prisma-query-args/work-packages.query-args';
+import { wbsElement1, project1 } from './projects.test-data';
+import { batman } from './users.test-data';
 
-export const prismaWorkPackage1: PrismaWorkPackage = {
+export const prismaWorkPackage1: Prisma.Work_PackageGetPayload<typeof workPackageQueryArgs> = {
   workPackageId: 1,
   wbsElementId: 65,
   projectId: 1,
   orderInProject: 1,
   startDate: new Date('10/10/2022'),
-  duration: 10
+  duration: 10,
+  wbsElement: {
+    ...wbsElement1,
+    projectLead: batman,
+    projectManager: batman,
+    changes: []
+  },
+  project: project1,
+  dependencies: [],
+  deliverables: [],
+  expectedActivities: []
 };
