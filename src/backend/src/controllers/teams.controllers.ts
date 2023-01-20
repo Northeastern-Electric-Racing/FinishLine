cdba0ee0bbdec74738f43f2e913016687741ef1c
Peyton-McKee <mckee.p@northeastern.edu>
2023-01-20 16:14:10 -0500

    Revert "#369 - temporary changes, there will definitely be some issues"
    This reverts commit f2ffa2b6dfc56fcfa85286db51ffca482d47bb2c.

--- src/backend/src/controllers/teams.controllers.ts
+++ src/backend/src/controllers/teams.controllers.ts
@@ -1,9 +1,5 @@
-import { prisma, Role, User } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import TeamsService from '../services/teams.services';
-import teamTransformer from '../transformers/teams.transformer';
-import { getCurrentUser } from '../utils/utils';
-import { getSingleUser } from './users.controllers';
 
 export default class TeamsController {
   static async getAllTeams(_req: Request, res: Response, next: NextFunction) {
@@ -27,37 +23,47 @@ export default class TeamsController {
       next(error);
     }
   }
-
-  static async editDescription(req: Request, res: Response) {
-    const { userId, teamId, newDescription } = req.params;
-
-    //const user = await prisma.user.findUnique({ where: { userId } });
-    const team = await TeamsService.getSingleTeam(teamId);
-    const user = await prisma.user.findUnique({ where: { userId } });
-    if (!user) return res.status(404).json({ message: `User with id #${userId} not found!` });
-    if (!team) return res.status(404).json({ message: `Team with id #${teamId} not found!` });
-
-    const canAccess = user.role === Role.ADMIN || user.role === Role.APP_ADMIN || user === team.leader;
-    if (!canAccess) return res.status(403).json({ message: 'Access Denied' });
-
-    let updatedTeam = team;
-    updatedTeam = await prisma.team.update({
-      where: { teamId },
-      include: {
-        leader: true,
-        projects: {
-          include: {
-            wbsElement: true
-          }
-        },
-        members: true,
-        leaderId: true
-      },
-      data: {
-        description: newDescription
-      }
-    });
-
-    return res.status(200).json(teamTransformer(updatedTeam));
-  }
 }
+
+export const editDescription = async (req: Request, res: Response) => {
+  const { body } = req;
+  const { userId, teamId, newDescription } = body;
+
+  const user = await prisma.user.findUnique({ where: { userId } });
+  const team = await prisma.team.findUnique({
+    where: { teamId },
+    include: {
+      leader: true,
+      projects: {
+        include: {
+          wbsElement: true
+        }
+      },
+      members: true
+    }
+  });
+  if (!user) return res.status(404).json({ message: `User with id #${userId} not found!` });
+  if (!team) return res.status(404).json({ message: `Team with id #${teamId} not found!` });
+
+  const canAccess = (user.role === Role.ADMIN || user.role === Role.APP_ADMIN) && userId === team?.leaderId;
+  if (!canAccess) return res.status(403).json({ message: 'Access Denied' });
+
+  let updatedTeam = team;
+  updatedTeam = await prisma.team.update({
+    where: { teamId },
+    include: {
+      leader: true,
+      projects: {
+        include: {
+          wbsElement: true
+        }
+      },
+      members: true
+    },
+    data: {
+      description: newDescription
+    }
+  });
+
+  return res.status(200).json(teamTransformer(updatedTeam));
+};
