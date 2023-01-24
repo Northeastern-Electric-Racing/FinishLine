1bd63bd399f253d323c6facffab9a50d9eef1b6c
Jonathan Chen <jonafish10@gmail.com>
2023-01-23 20:29:56 -0500

    #223: discard last login path query if empty

--- src/frontend/src/app/AppPublic.tsx
+++ src/frontend/src/app/AppPublic.tsx
@@ -37,6 +37,8 @@ const AppPublic: React.FC = () => {
     // otherwise, the user needs to login manually
     // prepare query args to store path after login
     const pathParts: string[] = history.location.pathname.split('/').slice(1);
+    // if the URL ended in an extra '/' we don't want to clutter the query args
+    if (pathParts[pathParts.length - 1] === '') pathParts.pop();
     const asQueryArg = (pathPart: string, idx: number): string => {
       if (idx === 0) return `page=${pathPart}`;
       else return `value${idx}=${pathPart}`;
