16ad22e2705caa0202f84a65bc8fbb969b7f949d
Andrew <tsai.an@northeastern.edu>
2023-01-26 12:12:31 -0500

    fixed syntax for color: added # before hex

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -73,7 +73,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Madeline Engle', color: '#FF2D76' },
     { name: 'Parth Kabra', color: '#e53774' },
     { name: 'Riley Platz', color: '#2d2fa6' },
-    { name: 'Andrew Tsai', color: '3281a8'}
+    { name: 'Andrew Tsai', color: '#3281a8'}
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
