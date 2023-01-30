ec1894c15abd38b80a58a55babd2d142daee51aa
Peyton-McKee <mckee.p@northeastern.edu>
2023-01-30 10:08:12 -0500

    #759 Added Prisma Model and Schema

---
+++ src/shared/src/types/task-types.ts
@@ -0,0 +1,34 @@
+/*
+ * This file is part of NER's FinishLine and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import { ProjectPreview } from './project-types';
+import { UserPreview } from './user-types';
+
+export enum TaskPriority {
+  Low = 'LOW',
+  Medium = 'MEDIUM',
+  High = 'HIGH'
+}
+
+export enum TaskStatus {
+  IN_BACKLOG = 'IN BACKLOG',
+  IN_PROGRESS = 'IN PROGRESS',
+  DONE = 'DONE'
+}
+
+export interface Task {
+  taskId: string;
+  project: ProjectPreview;
+  title: string;
+  notes: string;
+  dateDeleted?: Date;
+  dateCreated: Date;
+  createdBy: UserPreview;
+  deletedBy?: UserPreview;
+  assignees: UserPreview[];
+  deadline: Date;
+  priority: TaskPriority;
+  status: TaskStatus;
+}
