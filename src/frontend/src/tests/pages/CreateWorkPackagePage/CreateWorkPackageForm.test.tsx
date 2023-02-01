42f9c29a819ce53d1523824e707540760eead525
ahnfikd7 <99137905+ahnfikd7@users.noreply.github.com>
2023-01-31 22:56:47 -0500

    #637 Added the jest mock for toast in CreateWorkPackageForm

--- src/frontend/src/tests/pages/CreateWorkPackagePage/CreateWorkPackageForm.test.tsx
+++ src/frontend/src/tests/pages/CreateWorkPackagePage/CreateWorkPackageForm.test.tsx
@@ -14,6 +14,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('../../../hooks/auth.hooks');
 jest.mock('../../../hooks/utils.hooks');
+jest.mock('../../../hooks/toasts.hooks');
 
 jest.mock('../../../components/ReactHookTextField', () => {
   return {
