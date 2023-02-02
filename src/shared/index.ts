c1d5f077d8b68f9d9263588a66b4921d5bf7f590
Andrew <tsai.an@northeastern.edu>
2023-02-02 17:21:05 -0500

    #662: fixed prettier check in index.ts

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -15,4 +15,4 @@ export * from './src/validate-wbs';
 export * from './src/backend-supports/project-supports';
 export * from './src/word-count';
 
-export * from './src/rank-user-role'
+export * from './src/rank-user-role';
