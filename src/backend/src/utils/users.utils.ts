ccea9a63533c6ac002eb3bee97cd47c8f78b0ae0
Andrew <tsai.an@northeastern.edu>
2023-02-02 17:37:52 -0500

    #662: removed rankUserRole from backend

--- src/backend/src/utils/users.utils.ts
+++ src/backend/src/utils/users.utils.ts
@@ -9,21 +9,6 @@ export const getUserFullName = async (userId: number | null) => {
   return `${user.firstName} ${user.lastName}`;
 };
 
-export const rankUserRole = (role: Role) => {
-  switch (role) {
-    case 'APP_ADMIN':
-      return 5;
-    case 'ADMIN':
-      return 4;
-    case 'LEADERSHIP':
-      return 3;
-    case 'MEMBER':
-      return 2;
-    default:
-      return 1;
-  }
-};
-
 /**
  * Produce a array of User with given userIds
  * @param userIds array of userIds as an array of integers
