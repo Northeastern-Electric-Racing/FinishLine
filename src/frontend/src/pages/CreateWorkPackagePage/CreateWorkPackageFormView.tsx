b3fd5960d1bb3a52cfd0a3bc95331f4abfb3375b
Ethan Moskowitz <ethanmoskowitz0@gmail.com>
2023-02-01 21:14:13 -0500

    #715 Changed Buttons to NER Buttons

--- src/frontend/src/pages/CreateWorkPackagePage/CreateWorkPackageFormView.tsx
+++ src/frontend/src/pages/CreateWorkPackagePage/CreateWorkPackageFormView.tsx
@@ -20,8 +20,9 @@ import ReactHookTextField from '../../components/ReactHookTextField';
 import { FormControl, FormLabel, IconButton } from '@mui/material';
 import ReactHookEditableList from '../../components/ReactHookEditableList';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { SubmitButton } from '../../components/SubmitButton';
 import { wbsTester } from '../../utils/form';
+import NERFailButton from '../../components/NERFailButton';
+import NERSuccessButton from '../../components/NERSuccessButton';
 
 const schema = yup.object().shape({
   name: yup.string().required('Name is required'),
@@ -221,12 +222,12 @@ const CreateWorkPackageFormView: React.FC<CreateWorkPackageFormViewProps> = ({ a
           </Grid>
         </Grid>
         <Box display="flex" gap={2} sx={{ mt: 2 }}>
-          <SubmitButton variant="contained" color="primary" type="submit" disabled={!allowSubmit}>
+          <NERSuccessButton variant="contained" color="primary" type="submit" disabled={!allowSubmit}>
             Create
-          </SubmitButton>
-          <Button variant="outlined" color="secondary" onClick={onCancel}>
+          </NERSuccessButton>
+          <NERFailButton variant="outlined" color="secondary" onClick={onCancel}>
             Cancel
-          </Button>
+          </NERFailButton>
         </Box>
       </PageBlock>
     </form>
