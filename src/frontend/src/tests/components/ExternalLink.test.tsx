e98b70de57ec750563bee20ee86383d512b2ab43
RChandler234 <reid.c.chandler@gmail.com>
2023-02-04 18:24:10 -0500

    #814 - removed fontawesome icons from external link

--- src/frontend/src/tests/components/ExternalLink.test.tsx
+++ src/frontend/src/tests/components/ExternalLink.test.tsx
@@ -3,23 +3,22 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { faScroll } from '@fortawesome/free-solid-svg-icons';
 import { render, screen, fireEvent } from '../test-support/test-utils';
 import ExternalLink from '../../components/ExternalLink';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 
 const TEST_DESCRIPTION = 'test';
 const TEST_LINK = 'github.com';
 
 // Sets up the component under test with the desired values and renders it.
 const renderComponent = () => {
-  return render(<ExternalLink icon={faScroll} link={TEST_LINK} description={TEST_DESCRIPTION} />);
+  return render(<ExternalLink icon={<InsertDriveFileIcon />} link={TEST_LINK} description={TEST_DESCRIPTION} />);
 };
 
 describe('external link', () => {
   it('has correct href', () => {
     renderComponent();
     expect(screen.getByText(TEST_DESCRIPTION)).toBeInTheDocument();
-    expect(screen.getByTestId('icon')).toHaveAttribute('data-icon', 'scroll');
     expect(screen.getByText(TEST_DESCRIPTION)).toHaveAttribute('href', TEST_LINK);
     expect(screen.getByText(TEST_DESCRIPTION)).toHaveAttribute('target', '_blank');
     expect(screen.getByText(TEST_DESCRIPTION)).toHaveAttribute('rel', 'noopener noreferrer');
