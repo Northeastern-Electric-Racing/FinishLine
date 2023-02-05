cfb92c5f855ada5dbdc32cfd26c486a3e9ab023b
Andrew <tsai.an@northeastern.edu>
2023-02-05 14:39:35 -0500

    #662: passes prettier check now

--- src/shared/src/rank-user-role.ts
+++ src/shared/src/rank-user-role.ts
@@ -1,16 +1,16 @@
 import { Role } from '@prisma/client';
 
 export const rankUserRole = (role: Role) => {
-    switch (role) {
-      case 'APP_ADMIN':
-        return 5;
-      case 'ADMIN':
-        return 4;
-      case 'LEADERSHIP':
-        return 3;
-      case 'MEMBER':
-        return 2;
-      default:
-        return 1;
-    }
-  };
+  switch (role) {
+    case 'APP_ADMIN':
+      return 5;
+    case 'ADMIN':
+      return 4;
+    case 'LEADERSHIP':
+      return 3;
+    case 'MEMBER':
+      return 2;
+    default:
+      return 1;
+  }
+};
