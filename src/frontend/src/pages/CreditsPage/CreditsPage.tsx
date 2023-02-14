eebf9127528894d3aa3a08479d1970ab532f651d
Ethan Szeto <ethan.szeto@gmail.com>
2023-02-14 14:11:48 -0500

    #587: Ethan Szeto Credits, added comma to list

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -39,7 +39,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Megan Liu', color: '#3d609e' },
     { name: 'Peyton McKee', color: '#6a3941' },
     { name: 'Ryan Howe', color: '#3cce20' },
-    { name: 'Ethan Szeto', color: '#404040' }
+    { name: 'Ethan Szeto', color: '#404040' },
     { name: 'Adarsh Jayaram', color: '#ff0000' },
     { name: 'Harish Sundar', color: '#7f00ff' },
     { name: 'Sahil Shah', color: '#288BA8' },
