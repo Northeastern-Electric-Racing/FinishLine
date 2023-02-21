f658527886060489b4bd8bd0068ad4ebe1c4cb1a
Aaryan1203 <aaryanjain1203@gmail.com>
2023-02-21 10:10:53 -0500

    #883 Validated the set team members route

--- src/backend/src/routes/teams.routes.ts
+++ src/backend/src/routes/teams.routes.ts
@@ -7,7 +7,13 @@ const teamsRouter = express.Router();
 
 teamsRouter.get('/', TeamsController.getAllTeams);
 teamsRouter.get('/:teamId', TeamsController.getSingleTeam);
-teamsRouter.post('/:teamId/set-members', TeamsController.setTeamMembers);
+teamsRouter.post(
+  '/:teamId/set-members',
+  body('userIds').isArray(),
+  body('userIds.*').isInt({ min: 1 }),
+  validateInputs,
+  TeamsController.setTeamMembers
+);
 teamsRouter.post(
   '/:teamId/edit-description',
   body('newDescription').isString(),
