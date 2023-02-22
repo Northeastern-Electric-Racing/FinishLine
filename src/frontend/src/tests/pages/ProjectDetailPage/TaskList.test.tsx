119868da53b1ae82c61b18946fac784453a69e4d
Peyton-McKee <mckee.p@northeastern.edu>
2023-02-21 20:09:38 -0500

    #763 I DESPISE FRONTEND TESTS

--- src/frontend/src/tests/pages/ProjectDetailPage/TaskList.test.tsx
+++ src/frontend/src/tests/pages/ProjectDetailPage/TaskList.test.tsx
@@ -9,10 +9,38 @@ import * as authHooks from '../../../hooks/auth.hooks';
 import { mockAuth } from '../../test-support/test-data/test-utils.stub';
 import { exampleAdminUser, exampleGuestUser, exampleLeadershipUser } from '../../test-support/test-data/users.stub';
 import TaskList from '../../../pages/ProjectDetailPage/ProjectViewContainer/TaskList';
+import { useAuth } from '../../../hooks/auth.hooks';
+import { Auth } from '../../../utils/types';
+import { useAllUsers } from '../../../hooks/users.hooks';
+import { UseQueryResult } from 'react-query';
+import { User } from 'shared';
+import { mockUseQueryResult } from '../../test-support/test-data/test-utils.stub';
+import { useEditTask } from '../../../hooks/tasks.hooks';
+import { UseMutationResult } from 'react-query';
+import { mockUseMutationResult } from '../../test-support/test-data/test-utils.stub';
 
 jest.mock('../../../hooks/auth.hooks');
 // TODO: delete me when you actually implement onClick
 jest.mock('../../../hooks/toasts.hooks');
+
+jest.mock('../../../hooks/auth.hooks');
+const mockedUseAuth = useAuth as jest.Mock<Auth>;
+const mockAuthHook = (user = exampleAdminUser) => {
+  mockedUseAuth.mockReturnValue(mockAuth(false, user));
+};
+
+jest.mock('../../../hooks/tasks.hooks');
+const mockedEditTask = useEditTask as jest.Mock<UseMutationResult>;
+const mockEditTaskHook = (isLoading: boolean, isError: boolean, error?: Error) => {
+  mockedEditTask.mockReturnValue(mockUseMutationResult<{ in: string }>(isLoading, isError, { in: 'hi' }, error));
+};
+
+jest.mock('../../../hooks/users.hooks');
+const mockedUseAllUsers = useAllUsers as jest.Mock<UseQueryResult<User[]>>;
+const mockUserHook = (isLoading: boolean, isError: boolean, data?: User[], error?: Error) => {
+  mockedUseAllUsers.mockReturnValue(mockUseQueryResult<User[]>(isLoading, isError, data, error));
+};
+const users = [exampleAdminUser, exampleLeadershipUser];
 
 /**
  * Sets up the component under test with the desired values and renders it.
@@ -31,6 +59,9 @@ describe('TaskList component', () => {
 
   beforeEach(() => {
     spyUseAuthHook.mockReturnValue(mockAuth(false, exampleAdminUser));
+    mockAuthHook();
+    mockUserHook(false, false, users);
+    mockEditTaskHook(false, false);
   });
 
   it('renders "Task List" on top', () => {
