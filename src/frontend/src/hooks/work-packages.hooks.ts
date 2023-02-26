f08daff65730cf69001dad9d8a4bd2ec83e52992
nezamj03 <jazayeri.ne@northeastern.edu>
2023-02-25 21:00:10 -0500

    #420 added hooks for work packages

--- src/frontend/src/hooks/work-packages.hooks.ts
+++ src/frontend/src/hooks/work-packages.hooks.ts
@@ -7,6 +7,7 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { WorkPackage, WbsNumber } from 'shared';
 import {
   createSingleWorkPackage,
+  deleteWorkPackage,
   editWorkPackage,
   getAllWorkPackages,
   getSingleWorkPackage
@@ -62,6 +63,25 @@ export const useEditWorkPackage = (wbsNum: WbsNumber) => {
     {
       onSuccess: () => {
         queryClient.invalidateQueries(['work packages', wbsNum]);
+      }
+    }
+  );
+};
+
+/**
+ * Custom React Hook to delete a work package.
+ */
+export const useDeleteChangeRequest = () => {
+  const queryClient = useQueryClient();
+  return useMutation<{ message: string }, Error, any>(
+    ['work package', 'delete'],
+    async (wbsNumber: WbsNumber) => {
+      const { data } = await deleteWorkPackage(wbsNumber);
+      return data;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['work package']);
       }
     }
   );
