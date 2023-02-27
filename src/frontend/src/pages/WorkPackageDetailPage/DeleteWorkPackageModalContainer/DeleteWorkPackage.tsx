5c24adf96dbcf9ce9b56a2bdaa0f758f2fda5ce6
nezamj03 <jazayeri.ne@northeastern.edu>
2023-02-27 02:09:24 -0500

    #422 forgot to stage a couple files

---
+++ src/frontend/src/pages/WorkPackageDetailPage/DeleteWorkPackageModalContainer/DeleteWorkPackage.tsx
@@ -0,0 +1,52 @@
+/*
+ * This file is part of NER's FinishLine and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import { validateWBS, WbsNumber, wbsPipe } from 'shared';
+import { useHistory } from 'react-router-dom';
+import { useToast } from '../../../hooks/toasts.hooks';
+import { useDeleteWorkPackage } from '../../../hooks/work-packages.hooks';
+import LoadingIndicator from '../../../components/LoadingIndicator';
+import ErrorPage from '../../ErrorPage';
+import DeleteWorkPackageView from './DeleteWorkPackageView';
+
+interface DeleteWorkPackageRequestProps {
+  wbsNum: WbsNumber;
+  modalShow: boolean;
+  handleClose: () => void;
+}
+
+export interface DeleteWorkPackageInputs {
+  wbsNum: string;
+}
+
+const DeleteWorkPackage: React.FC<DeleteWorkPackageRequestProps> = ({
+  wbsNum,
+  modalShow,
+  handleClose
+}: DeleteWorkPackageRequestProps) => {
+  const history = useHistory();
+  const toast = useToast();
+  const { isLoading, isError, error, mutateAsync } = useDeleteWorkPackage();
+
+  const handleConfirm = async ({ wbsNum }: DeleteWorkPackageInputs) => {
+    handleClose();
+    const wbsNumber = validateWBS(wbsNum);
+    await mutateAsync(wbsNumber).catch((error) => {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    });
+    history.goBack();
+    toast.success(`Work Package #${wbsPipe(wbsNumber)} Deleted Successfully!`);
+  };
+
+  if (isLoading) return <LoadingIndicator />;
+
+  if (isError) return <ErrorPage message={error?.message} />;
+
+  return <DeleteWorkPackageView wbsNum={wbsNum} modalShow={modalShow} onHide={handleClose} onSubmit={handleConfirm} />;
+};
+
+export default DeleteWorkPackage;
