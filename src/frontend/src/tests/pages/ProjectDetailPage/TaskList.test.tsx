d5c454c7ef1bd6e460ff080b5cbf93a8b1592487
Jonathan Chen <jonafish10@gmail.com>
2023-03-05 15:42:51 -0500

    #956: fix mock hooks business

--- src/frontend/src/tests/pages/ProjectDetailPage/TaskList.test.tsx
+++ src/frontend/src/tests/pages/ProjectDetailPage/TaskList.test.tsx
@@ -3,25 +3,25 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-// import { render } from '@testing-library/react';
-// import { routerWrapperBuilder } from '../../test-support/test-utils';
+import { render, screen } from '@testing-library/react';
+import { routerWrapperBuilder } from '../../test-support/test-utils';
 import * as authHooks from '../../../hooks/auth.hooks';
 import { mockAuth } from '../../test-support/test-data/test-utils.stub';
-import { exampleAdminUser, exampleLeadershipUser } from '../../test-support/test-data/users.stub';
-// import TaskList from '../../../pages/ProjectDetailPage/ProjectViewContainer/TaskList';
+import { exampleAdminUser, exampleGuestUser, exampleLeadershipUser } from '../../test-support/test-data/users.stub';
+import TaskList from '../../../pages/ProjectDetailPage/ProjectViewContainer/TaskList';
 import { useAuth } from '../../../hooks/auth.hooks';
 import { Auth } from '../../../utils/types';
 import { useAllUsers } from '../../../hooks/users.hooks';
 import { UseQueryResult } from 'react-query';
 import { User } from 'shared';
 import { mockUseQueryResult } from '../../test-support/test-data/test-utils.stub';
-import { useEditTask } from '../../../hooks/tasks.hooks';
+import { useCreateTask, useDeleteTask, useEditTask, useEditTaskAssignees } from '../../../hooks/tasks.hooks';
 import { UseMutationResult } from 'react-query';
 import { mockUseMutationResult } from '../../test-support/test-data/test-utils.stub';
-// import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { exampleWbs1 } from '../../test-support/test-data/wbs-numbers.stub';
 
 jest.mock('../../../hooks/auth.hooks');
-// TODO: delete me when you actually implement onClick
 jest.mock('../../../hooks/toasts.hooks');
 
 jest.mock('../../../hooks/auth.hooks');
@@ -31,9 +31,21 @@ const mockAuthHook = (user = exampleAdminUser) => {
 };
 
 jest.mock('../../../hooks/tasks.hooks');
+const mockedCreateTask = useCreateTask as jest.Mock<UseMutationResult>;
+const mockCreateTaskHook = (isLoading: boolean, isError: boolean, error?: Error) => {
+  mockedCreateTask.mockReturnValue(mockUseMutationResult<{ in: string }>(isLoading, isError, { in: 'hi' }, error));
+};
 const mockedEditTask = useEditTask as jest.Mock<UseMutationResult>;
 const mockEditTaskHook = (isLoading: boolean, isError: boolean, error?: Error) => {
   mockedEditTask.mockReturnValue(mockUseMutationResult<{ in: string }>(isLoading, isError, { in: 'hi' }, error));
+};
+const mockedEditTaskAssignees = useEditTaskAssignees as jest.Mock<UseMutationResult>;
+const mockEditTaskHookAssignees = (isLoading: boolean, isError: boolean, error?: Error) => {
+  mockedEditTaskAssignees.mockReturnValue(mockUseMutationResult<{ in: string }>(isLoading, isError, { in: 'hi' }, error));
+};
+const mockedDeleteTask = useDeleteTask as jest.Mock<UseMutationResult>;
+const mockDeleteTaskHook = (isLoading: boolean, isError: boolean, error?: Error) => {
+  mockedDeleteTask.mockReturnValue(mockUseMutationResult<{ in: string }>(isLoading, isError, { in: 'hi' }, error));
 };
 
 jest.mock('../../../hooks/users.hooks');
@@ -46,17 +58,17 @@ const users = [exampleAdminUser, exampleLeadershipUser];
 /**
  * Sets up the component under test with the desired values and renders it.
  */
-// const renderComponent = () => {
-//   const RouterWrapper = routerWrapperBuilder({});
-//   const queryClient = new QueryClient();
-//   return render(
-//     <QueryClientProvider client={queryClient}>
-//       <RouterWrapper>
-//         <TaskList tasks={[]} team={undefined} hasPerms={true} />
-//       </RouterWrapper>
-//     </QueryClientProvider>
-//   );
-// };
+const renderComponent = () => {
+  const RouterWrapper = routerWrapperBuilder({});
+  const queryClient = new QueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <RouterWrapper>
+        <TaskList tasks={[]} team={undefined} hasTaskPermissions={true} currentWbsNumber={exampleWbs1} />
+      </RouterWrapper>
+    </QueryClientProvider>
+  );
+};
 
 describe('TaskList component', () => {
   const spyUseAuthHook = jest.spyOn(authHooks, 'useAuth');
@@ -65,39 +77,40 @@ describe('TaskList component', () => {
     spyUseAuthHook.mockReturnValue(mockAuth(false, exampleAdminUser));
     mockAuthHook();
     mockUserHook(false, false, users);
+    mockCreateTaskHook(false, false);
     mockEditTaskHook(false, false);
+    mockDeleteTaskHook(false, false);
+    mockEditTaskHookAssignees(false, false);
   });
 
-  it('test test', () => {
-    expect('this').toEqual('this');
+  it('renders "Task List" on top', () => {
+    renderComponent();
+    expect(screen.getByText('Task List')).toBeInTheDocument();
   });
-  // it('renders "Task List" on top', () => {
-  //   renderComponent();
-  //   expect(screen.getByText('Task List')).toBeInTheDocument();
-  // });
 
-  // it('renders all 3 labels', () => {
-  //   renderComponent();
-  //   expect(screen.getByText('In Progress')).toBeInTheDocument();
-  //   expect(screen.getByText('In Backlog')).toBeInTheDocument();
-  //   expect(screen.getByText('Done')).toBeInTheDocument();
-  // });
+  it('renders all 3 labels', () => {
+    renderComponent();
+    expect(screen.getByText('In Progress')).toBeInTheDocument();
+    expect(screen.getByText('In Backlog')).toBeInTheDocument();
+    expect(screen.getByText('Done')).toBeInTheDocument();
+  });
 
-  // describe('New Task button', () => {
-  //   it('renders New Task button', () => {
-  //     renderComponent();
-  //     expect(screen.getByText('New Task')).toBeInTheDocument();
-  //   });
+  describe('New Task button', () => {
+    it('renders New Task button', () => {
+      renderComponent();
+      expect(screen.getByText('New Task')).toBeInTheDocument();
+    });
 
-  // it('enables New Task button for leadership', () => {
-  //   spyUseAuthHook.mockReturnValue(mockAuth(false, exampleLeadershipUser));
-  //   renderComponent();
-  //   expect(screen.getByText('New Task')).toBeEnabled();
-  // });
+    it('enables New Task button for leadership', () => {
+      spyUseAuthHook.mockReturnValue(mockAuth(false, exampleLeadershipUser));
+      renderComponent();
+      expect(screen.getByText('New Task')).toBeEnabled();
+    });
 
-  // it('disables New Task button for guests', () => {
-  //   spyUseAuthHook.mockReturnValue(mockAuth(false, exampleGuestUser));
-  //   renderComponent();
-  //   expect(screen.getByText('New Task')).toBeDisabled();
-  // });
+    it('disables New Task button for guests', () => {
+      spyUseAuthHook.mockReturnValue(mockAuth(false, exampleGuestUser));
+      renderComponent();
+      expect(screen.getByText('New Task')).toBeDisabled();
+    });
+  });
 });
