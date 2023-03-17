0bf8ea0bdabf2c1d728a9889f7331965cbab1473
RChandler234 <reid.c.chandler@gmail.com>
2023-03-17 15:51:53 -0400

    merge conflict

--- src/frontend/src/pages/HomePage/WorkPackagesByTimelineStatus.tsx
+++ src/frontend/src/pages/HomePage/WorkPackagesByTimelineStatus.tsx
@@ -5,20 +5,13 @@
 
 import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Link from '@mui/material/Link';
-import { Link as RouterLink } from 'react-router-dom';
-import { TimelineStatus, WbsElementStatus } from 'shared';
+import { TimelineStatus, WbsElementStatus, wbsPipe } from 'shared';
 import { useAllWorkPackages } from '../../hooks/work-packages.hooks';
-import { datePipe, wbsPipe, fullNamePipe, projectWbsPipe } from '../../utils/pipes';
-import { routes } from '../../utils/routes';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import PageBlock from '../../layouts/PageBlock';
 import ErrorPage from '../ErrorPage';
-import { Chip, Grid, FormControl, InputLabel, MenuItem, Select, Typography, useTheme } from '@mui/material';
-import { Construction, Work } from '@mui/icons-material';
-import { CircularProgressWithLabel } from '../../components/CircularProgress';
+import { FormControl, InputLabel, MenuItem, Select, useTheme } from '@mui/material';
+import WorkPackageCard from './WorkPackageCard';
 
 const WorkPackagesByTimelineStatus: React.FC = () => {
   const [timelineStatus, setTimelineStatus] = useState<TimelineStatus>(TimelineStatus.VeryBehind);
@@ -58,76 +51,7 @@ const WorkPackagesByTimelineStatus: React.FC = () => {
     >
       {workPackages.data?.length === 0
         ? `No ${timelineStatus} work packages`
-        : [
-            ...(workPackages.data ?? []),
-            ...(workPackages.data ?? []),
-            ...(workPackages.data ?? []),
-            ...(workPackages.data ?? [])
-          ]?.map((wp) => (
-            <Card
-              variant="outlined"
-              key={wbsPipe(wp.wbsNum)}
-              sx={{
-                maxWidth: '450px',
-                mr: 3,
-                background: theme.palette.background.default
-              }}
-            >
-              <CardContent sx={{ padding: 2 }}>
-                <Grid container>
-                  <Grid container item xs={9} spacing={0} zeroMinWidth>
-                    <Grid container item>
-                      <Grid item xs={12} zeroMinWidth>
-                        <Typography fontWeight={'regular'} variant="subtitle2" noWrap>
-                          <Link
-                            color={'text.primary'}
-                            component={RouterLink}
-                            to={`${routes.PROJECTS}/${projectWbsPipe(wp.wbsNum)}`}
-                          >
-                            {projectWbsPipe(wp.wbsNum)} - {wp.projectName}
-                          </Link>
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12} zeroMinWidth>
-                        <Link component={RouterLink} to={`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`}>
-                          <Typography fontWeight={'regular'} variant="h5" noWrap>
-                            {wbsPipe(wp.wbsNum)} - {wp.name}
-                          </Typography>
-                        </Link>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={12} zeroMinWidth>
-                      <Typography fontWeight={'regular'} fontSize={20} variant="h6" noWrap>
-                        {datePipe(wp.startDate) + ' |一 ' + wp.duration + ' wks 一| ' + datePipe(wp.endDate)}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-
-                  <Grid container item xs={3} textAlign={'end'} direction={'column'} spacing={0} zeroMinWidth>
-                    <Grid item xs={1} zeroMinWidth>
-                      <Typography noWrap variant="subtitle1">
-                        {wp.timelineStatus}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={3} zeroMinWidth>
-                      <CircularProgressWithLabel variant="determinate" value={wp.progress} />
-                    </Grid>
-                  </Grid>
-                  <Grid item xs={5} sm={4} md={4} zeroMinWidth>
-                    <Chip
-                      sx={{ marginTop: 1, marginRight: 2 }}
-                      icon={<Construction />}
-                      label={fullNamePipe(wp.projectLead)}
-                      size="medium"
-                    />
-                  </Grid>
-                  <Grid item xs={5} sm={4} md={4} zeroMinWidth>
-                    <Chip sx={{ marginTop: 1 }} icon={<Work />} label={fullNamePipe(wp.projectManager)} size="medium" />
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          ))}
+        : workPackages.data?.map((wp) => <WorkPackageCard key={wbsPipe(wp.wbsNum)} wp={wp} />)}
     </Box>
   );
 
