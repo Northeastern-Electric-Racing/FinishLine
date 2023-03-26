71c4bba0f82495086b7ac4d7bf6a4292cec7ffa3
Aaryan1203 <aaryanjain1203@gmail.com>
2023-03-25 23:17:19 -0400

    error when shrinking the webpage

--- src/frontend/src/pages/WorkPackageDetailPage/WorkPackageEditContainer/WorkPackageEditDetails.tsx
+++ src/frontend/src/pages/WorkPackageDetailPage/WorkPackageEditContainer/WorkPackageEditDetails.tsx
@@ -19,7 +19,7 @@ interface Props {
 
 const WorkPackageEditDetails: React.FC<Props> = ({ users, control, errors }) => {
   const StageSelect = () => (
-    <FormControl sx={{ width: '95%' }}>
+    <FormControl fullWidth>
       <FormLabel>Stage Select</FormLabel>
       <Controller
         name="stage"
@@ -40,8 +40,8 @@ const WorkPackageEditDetails: React.FC<Props> = ({ users, control, errors }) => 
   return (
     <PageBlock title="Work Package Details">
       <Grid container xs={12}>
-        <Grid item xs={12} md={4} sx={{ mt: 1 }}>
-          <FormControl sx={{ width: '95%' }}>
+        <Grid item xs={12} md={5} sx={{ mt: 2, mr: 2 }}>
+          <FormControl fullWidth>
             <FormLabel>Work Package Name</FormLabel>
             <ReactHookTextField
               name="name"
@@ -51,8 +51,8 @@ const WorkPackageEditDetails: React.FC<Props> = ({ users, control, errors }) => 
             />
           </FormControl>
         </Grid>
-        <Grid item xs={12} md={3} sx={{ mt: 1 }}>
-          <FormControl sx={{ width: '95%' }}>
+        <Grid item xs={12} md={2} sx={{ mt: 2, mr: 2 }}>
+          <FormControl fullWidth>
             <FormLabel>Start Date (YYYY-MM-DD)</FormLabel>
             <Controller
               name="startDate"
@@ -72,24 +72,23 @@ const WorkPackageEditDetails: React.FC<Props> = ({ users, control, errors }) => 
             />
           </FormControl>
         </Grid>
-        <Grid item xs={12} md={3} sx={{ mt: 1, mb: 1 }}>
+        <Grid item xs={12} md={2} sx={{ mt: 2, mr: 2 }}>
           <StageSelect />
         </Grid>
-        <Grid item xs={12} md={1} sx={{ my: 1 }}>
-          <FormControl>
+        <Grid item xs={12} md={2} sx={{ mt: 2, mr: 2 }}>
+          <FormControl fullWidth>
             <FormLabel>Duration</FormLabel>
             <ReactHookTextField
               name="duration"
               control={control}
               type="number"
               placeholder="Enter duration..."
-              sx={{ width: '140%' }}
               errorMessage={errors.budget}
             />
           </FormControl>
         </Grid>
-        <Grid item xs={12} md={2} sx={{ mt: 2, mb: 1 }}>
-          <FormControl sx={{ width: '90%' }}>
+        <Grid item xs={12} md={2} sx={{ mt: 2, mr: 2 }}>
+          <FormControl fullWidth>
             <FormLabel>Project Lead</FormLabel>
             <Controller
               name="projectLead"
@@ -107,8 +106,8 @@ const WorkPackageEditDetails: React.FC<Props> = ({ users, control, errors }) => 
             />
           </FormControl>
         </Grid>
-        <Grid item xs={12} md={2} sx={{ mt: 2, mb: 1 }}>
-          <FormControl sx={{ width: '90%' }}>
+        <Grid item xs={12} md={2} sx={{ mt: 2, mr: 2 }}>
+          <FormControl fullWidth>
             <FormLabel>Project Manager</FormLabel>
             <Controller
               name="projectManager"
