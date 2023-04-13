025bea4424f16d9e7157dba07a45f41955f85e0e
Edward Ibarra <122840604+ibarra000@users.noreply.github.com>
2023-04-12 21:01:43 -0400

    #967: unit tests

--- src/frontend/src/tests/pages/WorkPackageDetailPage/WorkPackageViewContainer/WorkPackageDetails.test.tsx
+++ src/frontend/src/tests/pages/WorkPackageDetailPage/WorkPackageViewContainer/WorkPackageDetails.test.tsx
@@ -49,9 +49,7 @@ describe('Work Package Details Component', () => {
     it('does not render a stage label when work package has no stage', () => {
       mockHook(false, false, users);
       const { container } = renderComponent();
-      const chips = Array.from(container.getElementsByClassName('MuiChip-label'));
-      expect(chips.length).toBe(2);
-      expect(chips[0]).toHaveTextContent('No Stage');
+      expect(container.getElementsByClassName('MuiChip-label').length).toBe(1);
     });
 
     it('renders work package research stage label', () => {
