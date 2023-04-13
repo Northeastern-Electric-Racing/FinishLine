d19dda42fb69ad4b771ee06d0b10ba406a541ff1
Megan Liu <meganliu@Megans-MacBook.local>
2023-04-12 20:44:55 -0400

    #1081: added page block, created text field with onChange function that handles input, created button with disabled and toggle to hide it

--- src/frontend/src/pages/AdminToolsPage/AdminToolsPage.tsx
+++ src/frontend/src/pages/AdminToolsPage/AdminToolsPage.tsx
@@ -4,13 +4,39 @@
  */
 
 import PageTitle from '../../layouts/PageTitle/PageTitle';
-import AdminToolsUserMangaement from './AdminToolsUserManagement';
+import AdminToolsUserManagement from './AdminToolsUserManagement';
+import PageBlock from '../../layouts/PageBlock';
+import { NERButton } from '../../components/NERButton';
+import { Box, TextField } from '@mui/material';
 
 const AdminToolsPage: React.FC = () => {
+  const [days, setDays] = React.useState<string | null>(null);
+
+  const [hideButton, setHideButton] = React.useState(true);
+
+  const toggleButton = () => {
+    setHideButton(!hideButton);
+  };
+
   return (
     <>
       <PageTitle title={'Admin Tools'} previousPages={[]} />
-      <AdminToolsUserMangaement />
+      <AdminToolsUserManagement />
+      <PageBlock title={'Slack Upcoming Deadlines'}>
+        <Box sx={{ display: 'flex' }}>
+          <TextField
+            title={'Days Until Deadline'}
+            type="number"
+            value={days}
+            onChange={(event) => {
+              setDays(event.target.value);
+            }}
+          ></TextField>
+          <NERButton sx={{ mt: '20px', float: 'right' }} variant="contained" disabled={!days} onClick={toggleButton}>
+            Send
+          </NERButton>
+        </Box>
+      </PageBlock>
     </>
   );
 };
