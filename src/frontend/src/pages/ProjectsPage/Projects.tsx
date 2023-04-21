39a5021a30c884111d809639fad62bca1b49b61e
nezamj03 <jazayeri.ne@northeastern.edu>
2023-04-20 22:56:33 -0400

    #992 im all done kings

--- src/frontend/src/pages/ProjectsPage/Projects.tsx
+++ src/frontend/src/pages/ProjectsPage/Projects.tsx
@@ -5,10 +5,10 @@
 
 import { Route, Switch } from 'react-router-dom';
 import { routes } from '../../utils/routes';
-import ProjectsTable from './ProjectsTable';
 import WBSDetails from '../WBSDetails';
 import CreateProjectForm from '../CreateProjectPage/CreateProjectForm';
 import CreateWorkPackageForm from '../CreateWorkPackagePage/CreateWorkPackageForm';
+import ProjectsPage from './ProjectsPage';
 
 const Projects: React.FC = () => {
   return (
@@ -16,7 +16,7 @@ const Projects: React.FC = () => {
       <Route path={routes.WORK_PACKAGE_NEW} component={CreateWorkPackageForm} />
       <Route path={routes.PROJECTS_NEW} component={CreateProjectForm} />
       <Route path={routes.PROJECTS_BY_WBS} component={WBSDetails} />
-      <Route path={routes.PROJECTS} component={ProjectsTable} />
+      <Route path={routes.PROJECTS} component={ProjectsPage} />
     </Switch>
   );
 };
