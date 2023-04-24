90642175139777a127dd934c5ada27c1b38014b4
anthonybernardi <anthony.bernardi09@gmail.com>
2023-04-23 20:37:02 -0400

    #1045: project details i love grids

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/ProjectDetails.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/ProjectDetails.tsx
@@ -11,9 +11,8 @@ import Link from '@mui/material/Link';
 import { Project, WorkPackage, wbsPipe } from 'shared';
 import { datePipe, dollarsPipe, fullNamePipe, weeksPipe } from '../../../utils/pipes';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { Box, useTheme } from '@mui/material';
+import { useTheme } from '@mui/material';
 import WorkPackageSummary from './WorkPackageSummary';
 
 interface ProjectDetailsProps {
@@ -23,183 +22,148 @@ interface ProjectDetailsProps {
 const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
   const theme = useTheme();
   return (
-    <Box>
-      <div style={{ display: 'flex', flexDirection: 'row', marginTop: '10px' }}>
-        <Box width="50%" sx={{ paddingRight: 2 }}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
-            <Typography
-              variant="h5"
-              sx={{
-                cursor: 'pointer'
-              }}
-            >
-              Details
+    <Grid container display="flex" flexDirection="row" sx={{ mt: '10px' }}>
+      <Grid item sm={12} md={6} sx={{ mb: 2 }}>
+        <Typography
+          variant="h5"
+          sx={{
+            cursor: 'pointer',
+            mb: 1
+          }}
+        >
+          Details
+        </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item display="flex" alignItems="center" xs={12} sm={6}>
+            <Construction />
+            <Typography sx={{ ml: 2, mr: 1, fontWeight: 'bold' }} display="inline" textAlign={'right'}>
+              Project Lead:
             </Typography>
-          </Box>
-
-          <Grid container spacing={2}>
-            <Grid style={{ display: 'flex', alignItems: 'center' }} item display="flex" xs={12} md={6} xl={4}>
-              <Box width="50%" justifyContent="space-between" style={{ display: 'flex', alignItems: 'center' }}>
-                <Construction />
-                <Typography sx={{ fontWeight: 'bold' }} display="inline" textAlign={'right'}>
-                  Project Lead:
-                </Typography>
-              </Box>
-              <Box textAlign={'center'} width="50%">
-                <Typography sx={{ fontWeight: 'normal', display: 'inline' }} textAlign={'center'}>
-                  {fullNamePipe(project.projectLead)}
-                </Typography>
-              </Box>
-            </Grid>
-
-            <Grid display="flex" item xs={12} md={6} xl={4} style={{ display: 'flex', alignItems: 'center' }}>
-              <Box width="50%" justifyContent="space-between" style={{ display: 'flex', alignItems: 'center' }}>
-                <ScheduleIcon />
-                <Typography sx={{ fontWeight: 'bold' }} display="inline" textAlign={'right'}>
-                  Start Date:
-                </Typography>
-              </Box>
-              <Box textAlign={'center'} width="50%">
-                <Typography sx={{ fontWeight: 'normal', display: 'inline' }} textAlign={'center'}>
-                  {datePipe(project.startDate) || 'n/a'}
-                </Typography>
-              </Box>
-            </Grid>
-
-            <Grid display="flex" item xs={12} md={6} xl={4} style={{ display: 'flex', alignItems: 'center' }}>
-              <Box width="50%" justifyContent="space-between" style={{ display: 'flex', alignItems: 'center' }}>
-                <ScheduleIcon />
-                <Typography sx={{ fontWeight: 'bold' }} display="inline" textAlign={'center'}>
-                  Duration:
-                </Typography>
-              </Box>
-              <Box textAlign={'center'} width="50%">
-                <Typography sx={{ fontWeight: 'normal', display: 'inline' }}>{weeksPipe(project.duration)}</Typography>
-              </Box>
-            </Grid>
-
-            <Grid item display="flex" xs={12} md={6} xl={4} style={{ display: 'flex', alignItems: 'center' }}>
-              <Box width="50%" justifyContent="space-between" style={{ display: 'flex', alignItems: 'center' }}>
-                <Work />
-                <Typography sx={{ fontWeight: 'bold' }} display="inline" textAlign={'right'}>
-                  Project Manager:
-                </Typography>
-              </Box>
-              <Box textAlign={'center'} width="50%">
-                <Typography sx={{ fontWeight: 'normal', display: 'inline' }}>
-                  {fullNamePipe(project.projectManager)}
-                </Typography>
-              </Box>
-            </Grid>
-
-            <Grid display="flex" item xs={12} md={6} xl={4} style={{ display: 'flex', alignItems: 'center' }}>
-              <Box width="50%" justifyContent="space-between" style={{ display: 'flex', alignItems: 'center' }}>
-                <ScheduleIcon />
-                <Typography sx={{ fontWeight: 'bold' }} display="inline" textAlign={'center'}>
-                  End Date:
-                </Typography>
-              </Box>
-              <Box textAlign={'center'} width="50%">
-                <Typography sx={{ fontWeight: 'normal', display: 'inline' }}>
-                  {datePipe(project.endDate) || 'n/a'}
-                </Typography>
-              </Box>
-            </Grid>
-
-            <Grid display="flex" item xs={12} md={6} xl={4} style={{ display: 'flex', alignItems: 'center' }}>
-              <Box width="50%" justifyContent="space-between" style={{ display: 'flex', alignItems: 'center' }}>
-                <AttachMoneyIcon />
-                <Typography sx={{ fontWeight: 'bold' }} display="inline" textAlign={'center'}>
-                  Budget:
-                </Typography>
-              </Box>
-              <Box textAlign={'center'} width="50%">
-                <Typography sx={{ fontWeight: 'normal', display: 'inline' }} textAlign={'center'}>
-                  {dollarsPipe(project.budget)}
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-
-        <Box width="50%">
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
-            <Typography
-              variant="h5"
-              sx={{
-                cursor: 'pointer'
-              }}
-            >
-              Summary
+            <Typography sx={{ fontWeight: 'normal', display: 'inline' }} textAlign={'center'}>
+              {fullNamePipe(project.projectLead)}
             </Typography>
-          </Box>
-          {project.summary}
-          <Grid container spacing={1}>
-            <Grid item xs={12}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 1 }}>
-                <Typography
-                  variant="h5"
-                  sx={{
-                    cursor: 'pointer'
-                  }}
-                >
-                  Links
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <Stack direction="row" alignItems="center">
-                <CoPresent sx={{ fontSize: 22, color: theme.palette.text.primary }} />
-                <Link href={project.slideDeckLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
-                  Slide Deck
-                </Link>
-              </Stack>
-            </Grid>
-            <Grid item xs={6}>
-              <Stack direction="row" alignItems="center">
-                <FormatListBulleted sx={{ fontSize: 22, color: theme.palette.text.primary }} />
-                <Link href={project.taskListLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
-                  Task List
-                </Link>
-              </Stack>
-            </Grid>
-            <Grid item xs={6}>
-              <Stack direction="row" alignItems="center">
-                <FormatListNumbered sx={{ fontSize: 22, color: theme.palette.text.primary }} />
-                <Link href={project.bomLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
-                  BOM
-                </Link>
-              </Stack>
-            </Grid>
-            <Grid item xs={6}>
-              <Stack direction="row" alignItems="center">
-                <Folder sx={{ fontSize: 22, color: theme.palette.text.primary }} />
-                <Link href={project.gDriveLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
-                  Google Drive
-                </Link>
-              </Stack>
-            </Grid>
           </Grid>
-        </Box>
-      </div>
-      <Box sx={{ marginTop: 2 }}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
+
+          <Grid item display="flex" alignItems="center" xs={12} sm={6}>
+            <ScheduleIcon />
+            <Typography sx={{ ml: 2, mr: 1, fontWeight: 'bold' }} display="inline" textAlign={'right'}>
+              Start Date:
+            </Typography>
+            <Typography sx={{ fontWeight: 'normal', display: 'inline' }} textAlign={'center'}>
+              {datePipe(project.startDate) || 'n/a'}
+            </Typography>
+          </Grid>
+
+          <Grid item display="flex" alignItems="center" xs={12} sm={6}>
+            <Work />
+            <Typography sx={{ ml: 2, mr: 1, fontWeight: 'bold' }} display="inline" textAlign={'right'}>
+              Project Manager:
+            </Typography>
+            <Typography sx={{ fontWeight: 'normal', display: 'inline' }}>{fullNamePipe(project.projectManager)}</Typography>
+          </Grid>
+
+          <Grid item display="flex" alignItems="center" xs={12} sm={6}>
+            <ScheduleIcon />
+            <Typography sx={{ ml: 2, mr: 1, fontWeight: 'bold' }} display="inline" textAlign={'right'}>
+              End Date:
+            </Typography>
+            <Typography sx={{ fontWeight: 'normal', display: 'inline' }}>{datePipe(project.endDate) || 'n/a'}</Typography>
+          </Grid>
+
+          <Grid item display="flex" alignItems="center" xs={12} sm={6}>
+            <AttachMoneyIcon />
+            <Typography sx={{ ml: 2, mr: 1, fontWeight: 'bold' }} display="inline" textAlign={'right'}>
+              Budget:
+            </Typography>
+            <Typography sx={{ fontWeight: 'normal', display: 'inline' }} textAlign={'center'}>
+              {dollarsPipe(project.budget)}
+            </Typography>
+          </Grid>
+
+          <Grid item display="flex" alignItems="center" xs={12} sm={6}>
+            <ScheduleIcon />
+            <Typography sx={{ ml: 2, mr: 1, fontWeight: 'bold' }} display="inline" textAlign={'right'}>
+              Duration:
+            </Typography>
+            <Typography sx={{ fontWeight: 'normal', display: 'inline' }}>{weeksPipe(project.duration)}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container display="flex" flexDirection="row" item sm={12} md={6} sx={{ mb: 2 }}>
+        <Grid item xs sx={{ mb: 2 }}>
           <Typography
             variant="h5"
             sx={{
+              mb: 1,
+              cursor: 'pointer'
+            }}
+          >
+            Summary
+          </Typography>
+          <Typography>{project.summary}</Typography>
+        </Grid>
+
+        <Grid container item xs={12}>
+          <Grid item xs={12}>
+            <Typography
+              variant="h5"
+              sx={{
+                mb: 1,
+                cursor: 'pointer'
+              }}
+            >
+              Links
+            </Typography>
+          </Grid>
+          <Grid item xs={6} display="flex" alignItems="center">
+            <Folder sx={{ fontSize: 22, color: theme.palette.text.primary }} />
+            <Link href={project.gDriveLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
+              Google Drive
+            </Link>
+          </Grid>
+
+          <Grid item xs={6} display="flex" alignItems="center">
+            <FormatListBulleted sx={{ fontSize: 22, color: theme.palette.text.primary }} />
+            <Link href={project.taskListLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
+              Task List
+            </Link>
+          </Grid>
+          <Grid item xs={6} display="flex" alignItems="center">
+            <CoPresent sx={{ fontSize: 22, color: theme.palette.text.primary }} />
+            <Link href={project.slideDeckLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
+              Slide Deck
+            </Link>
+          </Grid>
+          <Grid item xs={6} display="flex" alignItems="center">
+            <FormatListNumbered sx={{ fontSize: 22, color: theme.palette.text.primary }} />
+            <Link href={project.bomLink!} target="_blank" underline="always" fontSize={19} sx={{ pl: 1 }}>
+              BOM
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container item display="flex">
+        <Grid item xs={12}>
+          <Typography
+            variant="h5"
+            sx={{
+              mb: 1,
               cursor: 'pointer'
             }}
           >
             Work Packages
           </Typography>
-        </Box>
+        </Grid>
         {project.workPackages.map((ele: WorkPackage) => (
-          <div key={wbsPipe(ele.wbsNum)} className="mt-3">
+          <Grid item xs={12} key={wbsPipe(ele.wbsNum)} sx={{ mb: 0.5 }}>
             <WorkPackageSummary workPackage={ele} />
-          </div>
+          </Grid>
         ))}
-      </Box>
-    </Box>
+      </Grid>
+    </Grid>
   );
 };
 
