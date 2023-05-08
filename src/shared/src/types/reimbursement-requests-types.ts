2a6ef3977caf562939c1bea7ee43e942d1aeff7f
Megan Liu <meganliu@megans-macbook.mynetworksettings.com>
2023-05-07 23:13:13 -0400

    #1127: First draft of shared ReimbursementRequest type

--- src/shared/src/types/reimbursement-requests-types.ts
+++ src/shared/src/types/reimbursement-requests-types.ts
@@ -3,5 +3,22 @@ export enum Club_Account {
   BUDGET = 'BUDGET'
 }
 
-// create shared type here
-export interface ReimbursementRequest {}
+export interface ReimbursementRequest {
+  reimbursementRequestId: string;
+  saboId?: number;
+  dateCreated: Date;
+  dateDeleted?: Date;
+  dateOfExpense: Date;
+  reimbursementsStatuses: Reimbursement_Status[]; // ReimbursementStatus
+  recepientId: number; // get rid of
+  recepient: User; // RecipientPreview
+  vendorId: string; // get rid of
+  vendor: Vendor; // VendorPreview
+  account: Club_Accounts; // ClubAccounts
+  totalCost: number;
+  receiptPictures: string[];
+  reimbursementProducts: Reimbursement_Product[]; // ReimbursementProduct
+  dateDelivered?: Date;
+  expenseTypeId: string; // get rid of
+  expenseType: Expense_Type; // ExpenseTypePreview
+}
