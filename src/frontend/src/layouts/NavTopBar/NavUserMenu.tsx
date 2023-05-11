4f16c75d5aea84032e1142c76fa0d4e7ed0915bf
ninest <parth.kabra@gmail.com>
2023-05-10 20:07:10 -0400

    #619 Fix import.meta

--- src/frontend/src/layouts/NavTopBar/NavUserMenu.tsx
+++ src/frontend/src/layouts/NavTopBar/NavUserMenu.tsx
@@ -27,7 +27,7 @@ const NavUserMenu: React.FC = () => {
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget);
   const handleClose = () => setAnchorEl(null);
 
-  const googleAuthClientId = import.meta.REACT_APP_GOOGLE_AUTH_CLIENT_ID;
+  const googleAuthClientId = import.meta.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID;
 
   const logout = () => {
     if (!auth) return;
