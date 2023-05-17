fb7d7ee4040ab43599efd41386b01eab176cf01f
Megan Liu <meganliu@Megans-MacBook.local>
2023-05-18 00:20:02 +0800

    #1127: Created more interfaces in types file based on prisma

--- src/shared/src/types/reimbursement-requests-types.ts
+++ src/shared/src/types/reimbursement-requests-types.ts
@@ -3,6 +3,13 @@ export enum Club_Account {
   BUDGET = 'BUDGET'
 }
 
+export enum ReimbursementStatusType {
+  PENDING_FINANCE = 'PENDING FINANCE',
+  SABO_SUBMITTED = 'SABO SUBMITTED',
+  ADVISOR_APPROVED = 'ADVISOR_APPROVED',
+  REIMBURSED = 'REIMBURSED'
+}
+
 export interface ReimbursementRequest {
   reimbursementRequestId: string;
   saboId?: number;
@@ -10,15 +17,23 @@ export interface ReimbursementRequest {
   dateDeleted?: Date;
   dateOfExpense: Date;
   reimbursementsStatuses: Reimbursement_Status[]; // ReimbursementStatus
-  recepientId: number; // get rid of
+  //recepientId: number; // get rid of
   recepient: User; // RecipientPreview
-  vendorId: string; // get rid of
+  //vendorId: string; // get rid of
   vendor: Vendor; // VendorPreview
-  account: Club_Accounts; // ClubAccounts
+  account: Club_Account;
   totalCost: number;
   receiptPictures: string[];
   reimbursementProducts: Reimbursement_Product[]; // ReimbursementProduct
   dateDelivered?: Date;
-  expenseTypeId: string; // get rid of
+  //expenseTypeId: string; // get rid of
   expenseType: Expense_Type; // ExpenseTypePreview
+}
+
+export interface ReimbursementStatus {
+  reimbursementStatusId: number;
+  type: ReimbursementStatusType;
+  user: UserPreview;
+  dateCreated: DateTime;
+  reimbursementRequest: ReimbursementRequest;
 }
