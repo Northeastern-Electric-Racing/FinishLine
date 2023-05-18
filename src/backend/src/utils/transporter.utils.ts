272c9e2556ee7b91a785a96bb6ab44fad248a49c
Peyton-McKee <mckee.p@northeastern.edu>
2023-05-18 12:01:29 -0400

    #1135 feedback Part 2

--- src/backend/src/utils/transporter.utils.ts
+++ src/backend/src/utils/transporter.utils.ts
@@ -4,50 +4,6 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 const { OAuth2 } = google.auth;
 
-// const sendMail = async () => {
-//   const clientId = process.env.OAUTH_CLIENT_ID;
-//   const clientSecret = process.env.OAUTH_CLIENT_SECRET;
-//   const redirectUri = 'http://localhost:3000/home';
-//   const refreshToken = process.env.OAUTH_REFRESH_TOKEN;
-
-//   const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
-
-//   oAuth2Client.setCredentials({ refresh_token: refreshToken });
-//   try {
-//     const accessToken = await oAuth2Client.getAccessToken();
-//     if (!accessToken.token) throw new Error('Failed to get access token');
-
-//     console.log(accessToken.token);
-
-//     const transporter = nodemailer.createTransport({
-//       host: 'smtp.gmail.com',
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         type: 'OAuth2',
-//         user: 'aqua.retro1@gmail.com',
-//         clientId,
-//         clientSecret,
-//         refreshToken,
-//         accessToken: accessToken.token
-//       }
-//     });
-
-//     const mailOptions = {
-//       from: 'aqua.retro1@gmail.com',
-//       to: 'mckee.p@northeastern.edu',
-//       subject: 'Test email',
-//       text: 'This is a test email'
-//     };
-
-//     const info = await transporter.sendMail(mailOptions);
-
-//     console.log(`Message sent: ${info.messageId}`);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
 const createTransporter = async () => {
   try {
     const oauth2Client = new OAuth2(
@@ -87,7 +43,7 @@ const createTransporter = async () => {
   }
 };
 
-const sendMail = async (subject: string, text: string) => {
+const sendMailToAdvisor = async (subject: string, text: string) => {
   try {
     const mailOptions = {
       from: process.env.USER_EMAIL,
@@ -103,4 +59,4 @@ const sendMail = async (subject: string, text: string) => {
   }
 };
 
-export default sendMail;
+export default sendMailToAdvisor;
