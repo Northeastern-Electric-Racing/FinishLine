bdcedc77a72468d51d52bd87fefac3ce1f999a32
Peyton-McKee <mckee.p@northeastern.edu>
2023-05-17 20:42:28 -0400

    #1132 Remove weird thing

--- src/frontend/src/pages/ProjectDetailPage/ProjectEdit/ProjectEditContainer.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectEdit/ProjectEditContainer.tsx
@@ -25,7 +25,6 @@ import { bulletsToObject, mapBulletsToPayload } from '../../../utils/form';
 import NERSuccessButton from '../../../components/NERSuccessButton';
 import NERFailButton from '../../../components/NERFailButton';
 import { useToast } from '../../../hooks/toasts.hooks';
-import { EditSingleProjectPayload } from '../../../utils/types';
 
 const schema = yup.object().shape({
   name: yup.string().required('Name is required!'),
@@ -136,7 +135,7 @@ const ProjectEditContainer: React.FC<ProjectEditContainerProps> = ({ project, ex
     const features = mapBulletsToPayload(data.features);
     const otherConstraints = mapBulletsToPayload(data.constraints);
 
-    const payload: EditSingleProjectPayload = {
+    const payload = {
       name,
       budget,
       summary,
