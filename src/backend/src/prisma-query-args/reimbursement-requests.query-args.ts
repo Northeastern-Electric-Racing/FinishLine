d054ed1af894ba649d404dfc90685ef6c8030b32
Megan Liu <meganliu@Megans-MacBook.local>
2023-05-22 22:49:51 +0800

    #1127: made first draft of reimbursement req query args

--- src/backend/src/prisma-query-args/reimbursement-requests.query-args.ts
+++ src/backend/src/prisma-query-args/reimbursement-requests.query-args.ts
@@ -1,12 +1,61 @@
 import { Prisma } from '@prisma/client';
+import changeRequestQueryArgs from './change-requests.query-args';
+import descriptionBulletQueryArgs from './description-bullets.query-args';
+import teamQueryArgs from './teams.query-args';
 
-const reimbursementRequestQueryArgs = Prisma.validator<Prisma.reimbursementRequestArgs>()({
+const reimbursementRequestQueryArgs = Prisma.validator<Prisma.Reimbursement_RequestArgs>()({
   include: {
-    reimbursementRequestId: true,
-    createdBy: true,
-    deletedBy: true,
-    assignees: true
+    recepient: true,
+    /*include: {
+        submittedChangeRequests: {
+          where: {
+            dateDeleted: null
+          },
+          ...changeRequestQueryArgs
+        },
+        reviewedChangeRequests: {
+          where: {
+            dateDeleted: null
+          },
+          ...changeRequestQueryArgs
+        },
+        deletedChangeRequests: {
+          where: {
+            dateDeleted: null
+          },
+          ...changeRequestQueryArgs
+        },
+        projectLead: {
+          where: {
+            dateDeleted: null
+          }
+        },
+        projectManager: {
+          where: {
+            dateDeleted: null
+          }
+        },
+        teams: {
+          ...teamQueryArgs
+        },
+        deletedWBSElements: {
+          where: {
+            dateDeleted: null
+          }
+        },
+        checkedDescriptionBullets: {
+          where: {
+            dateDeleted: null
+          },
+          ...descriptionBulletQueryArgs
+        },
+        createdProposedSolutions: {}
+      }*/
+    // should include any fields?
+    vendor: true, // should include any fields?
+    expenseType: true // need to include the Reimbursement_Request[]? should include any other fields?
+    // should include any other fields?
   }
 });
 
-export default taskQueryArgs;
+export default reimbursementRequestQueryArgs;
