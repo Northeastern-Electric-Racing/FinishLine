030aead88c0c7402c066c99a84ec11c12f4bc504
Zach Marino <zachmarino234@gmail.com>
2023-05-31 22:45:34 -0700

    "#1130 tests progress (NOT DONE)"

--- src/backend/tests/reimbursement-requests.test.ts
+++ src/backend/tests/reimbursement-requests.test.ts
@@ -1,6 +1,7 @@
 import prisma from '../src/prisma/prisma';
 import ReimbursementRequestService from '../src/services/reimbursement-requests.services';
-import { AccessDeniedAdminOnlyException } from '../src/utils/errors.utils';
+import { AccessDeniedAdminOnlyException, AccessDeniedException, NotFoundException } from '../src/utils/errors.utils';
+import { validateReimbursementProducts } from '../src/utils/reimbursement-requests.utils';
 import { Parts, PopEyes } from './test-data/reimbursement-requests.test-data';
 import { batman, wonderwoman } from './test-data/users.test-data';
 
@@ -49,6 +50,28 @@ describe('Reimbursement Requests', () => {
       const expenseType = await ReimbursementRequestService.createExpenseType(batman, Parts.name, Parts.code, Parts.allowed);
 
       expect(expenseType.expenseTypeId).toBe(Parts.expenseTypeId);
+    });
+  });
+
+  describe('Delivered Tests', () => {
+    test('Mark as delivered fails for non admins', async () => {
+      await expect(
+        ReimbursementRequestService.markReimbursementRequestAsDelivered(wonderwoman, PopEyes.vendorId)
+      ).rejects.toThrow(new AccessDeniedException('Only the creator of the reimbursement request can mark as delivered'));
+    });
+
+    test('Mark as delivered fails for undefined ID', async () => {
+      await expect(ReimbursementRequestService.markReimbursementRequestAsDelivered(wonderwoman, '')).rejects.toThrow(
+        new NotFoundException('Reimbursement Request', reimbursementRequestId /* TODO */)
+      );
+    });
+
+    test('Mark request as delivered successfully', async () => {
+      jest.spyOn(prisma.reimbursement_Request, 'findUnique').mockResolvedValue(/* TODO: reimbursement_Request value*/);
+
+      const expenseType = await ReimbursementRequestService.markReimbursementRequestAsDelivered(batman, PopEyes.vendorId);
+
+      expect(/* TODO */).toBe(/* TODO */);
     });
   });
 });
