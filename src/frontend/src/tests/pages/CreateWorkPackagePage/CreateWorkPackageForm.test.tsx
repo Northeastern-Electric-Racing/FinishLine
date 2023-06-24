0db16f28c72b15a11bb4df03ed39de10af0bac7b
Peyton-McKee <mckee.p@northeastern.edu>
2023-06-24 10:34:31 -0400

    #926 Linting and prettier

--- src/frontend/src/tests/pages/CreateWorkPackagePage/CreateWorkPackageForm.test.tsx
+++ src/frontend/src/tests/pages/CreateWorkPackagePage/CreateWorkPackageForm.test.tsx
@@ -3,13 +3,13 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { render, screen } from '../../test-support/test-utils';
-import { exampleAdminUser, exampleGuestUser } from '../../test-support/test-data/users.stub';
-import CreateWorkPackageForm from '../../../pages/CreateWorkPackagePage/CreateWorkPackageForm';
-import { useQuery } from '../../../hooks/utils.hooks';
-import { BrowserRouter } from 'react-router-dom';
-import { useCurrentUser } from '../../../hooks/users.hooks';
-import { User } from 'shared';
+// import { render, screen } from '../../test-support/test-utils';
+// import { exampleAdminUser, exampleGuestUser } from '../../test-support/test-data/users.stub';
+// import CreateWorkPackageForm from '../../../pages/CreateWorkPackagePage/CreateWorkPackageForm';
+// import { useQuery } from '../../../hooks/utils.hooks';
+// import { BrowserRouter } from 'react-router-dom';
+// import { useCurrentUser } from '../../../hooks/users.hooks';
+// import { User } from 'shared';
 
 jest.mock('../../../hooks/utils.hooks');
 jest.mock('../../../hooks/toasts.hooks');
@@ -22,34 +22,33 @@ jest.mock('../../../components/ReactHookTextField', () => {
   };
 });
 
-const mockedUseCurrentUser = useCurrentUser as jest.Mock<User>;
-const mockedUseQuery = useQuery as jest.Mock<URLSearchParams>;
+// const mockedUseCurrentUser = useCurrentUser as jest.Mock<User>;
+// const mockedUseQuery = useQuery as jest.Mock<URLSearchParams>;
 
-const mockCurrentUser = (user = exampleAdminUser) => {
-  mockedUseCurrentUser.mockReturnValue(user);
-};
+// const mockCurrentUser = (user = exampleAdminUser) => {
+//   mockedUseCurrentUser.mockReturnValue(user);
+// };
 
-const mockUseQuery = () => {
-  mockedUseQuery.mockReturnValue(new URLSearchParams(''));
-};
+// const mockUseQuery = () => {
+//   mockedUseQuery.mockReturnValue(new URLSearchParams(''));
+// };
 
 /**
  * Sets up the component under test with the desired values and renders it.
  */
-const renderComponent = () => {
-  return render(
-    <BrowserRouter>
-      <CreateWorkPackageForm />
-    </BrowserRouter>
-  );
-};
+// const renderComponent = () => {
+//   return render(
+//     <BrowserRouter>
+//       <CreateWorkPackageForm />
+//     </BrowserRouter>
+//   );
+// };
 
 describe('create wp form test suite', () => {
   it('disables submit button for guest users', () => {
     // mockCurrentUser(exampleGuestUser);
     // mockUseQuery();
     // renderComponent();
-
     // expect(screen.getByText('Create')).toBeDisabled();
   });
 
@@ -57,7 +56,6 @@ describe('create wp form test suite', () => {
     // mockCurrentUser();
     // mockUseQuery();
     // renderComponent();
-
     // expect(screen.getByText('Create')).not.toBeDisabled();
   });
 });
