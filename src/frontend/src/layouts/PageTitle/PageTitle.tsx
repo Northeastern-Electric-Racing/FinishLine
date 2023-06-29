709aa39aef340dea6946e5d5b8d2c3f8b1cf7920
Zack Roth <zack27r@gmail.com>
2023-06-29 19:17:00 -0400

    #1178 - Fixed pagetitle responsiveness bug

--- src/frontend/src/layouts/PageTitle/PageTitle.tsx
+++ src/frontend/src/layouts/PageTitle/PageTitle.tsx
@@ -22,37 +22,18 @@ interface PageTitleProps {
  * @param headerRight The button to display on the right side of the page title
  */
 const PageTitle: React.FC<PageTitleProps> = ({ title, previousPages, headerRight, tabs }) => {
-  const theme = useTheme();
-
   return (
     <>
       <PageBreadcrumbs currentPageTitle={title} previousPages={previousPages} />
-      <Grid container sx={{ mb: 2 }} alignItems="center">
-        <Grid item>
+      <Box sx={{ mb: 2, display: 'flex', flexDirection: 'column' }}>
+        <Box sx={{ display: 'flex', flexDirection: ['column', 'row'], justifyContent: 'space-between', gap: 1 }}>
           <Typography variant="h4" fontSize={30} sx={{ verticalAlign: 'middle', display: 'inline-flex' }}>
             {title}
           </Typography>
-        </Grid>
-        <Grid
-          item
-          lg={'auto'}
-          xs={12}
-          sx={{
-            margin: 'auto',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '0 16px',
-            maxWidth: '100%',
-            mb: theme.breakpoints.down('xs') ? 2 : 0
-          }}
-        >
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>{tabs}</Box>
-        </Grid>
-        <Grid item sx={{ mx: 0 }}>
-          {headerRight}
-        </Grid>
-      </Grid>
+          <Box>{headerRight}</Box>
+        </Box>
+        {tabs && <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>{tabs}</Box>}
+      </Box>
     </>
   );
 };
