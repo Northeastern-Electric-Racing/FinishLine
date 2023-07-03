6136049422cc0af9d1dac66b60190f239fb94ffe
RChandler234 <reid.c.chandler@gmail.com>
2023-07-03 15:02:29 -0400

    #926 - prettier

--- src/frontend/src/tests/pages/CreateWorkPackagePage/CreateWorkPackageFormView.test.tsx
+++ src/frontend/src/tests/pages/CreateWorkPackagePage/CreateWorkPackageFormView.test.tsx
@@ -3,10 +3,10 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
- import { render, screen } from '../../test-support/test-utils';
- import CreateWorkPackageFormView from '../../../pages/CreateWorkPackagePage/CreateWorkPackageFormView';
- import { useQuery } from '../../../hooks/utils.hooks';
- import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '../../test-support/test-utils';
+import CreateWorkPackageFormView from '../../../pages/CreateWorkPackagePage/CreateWorkPackageFormView';
+import { useQuery } from '../../../hooks/utils.hooks';
+import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('../../../hooks/utils.hooks');
 
@@ -17,29 +17,29 @@ jest.mock('../../../components/ReactHookTextField', () => {
   };
 });
 
- const mockedUseQuery = useQuery as jest.Mock<URLSearchParams>;
+const mockedUseQuery = useQuery as jest.Mock<URLSearchParams>;
 
- const mockUseQuery = () => {
-   mockedUseQuery.mockReturnValue(new URLSearchParams(''));
- };
- const setWbsNum = (str: string) => {};
+const mockUseQuery = () => {
+  mockedUseQuery.mockReturnValue(new URLSearchParams(''));
+};
+const setWbsNum = (str: string) => {};
 
 /**
  * Sets up the component under test with the desired values and renders it.
  */
- const renderComponent = (allowSubmit: boolean) => {
-   return render(
-     <BrowserRouter>
-       <CreateWorkPackageFormView
-         onSubmit={() => null}
-         onCancel={() => null}
-         allowSubmit={allowSubmit}
-         wbsNum=""
-         setWbsNum={setWbsNum}
-       />
-     </BrowserRouter>
-   );
- };
+const renderComponent = (allowSubmit: boolean) => {
+  return render(
+    <BrowserRouter>
+      <CreateWorkPackageFormView
+        onSubmit={() => null}
+        onCancel={() => null}
+        allowSubmit={allowSubmit}
+        wbsNum=""
+        setWbsNum={setWbsNum}
+      />
+    </BrowserRouter>
+  );
+};
 
 describe.skip('create wp form view test suite', () => {
   it('disables submit button when allowSubmit is false', () => {
