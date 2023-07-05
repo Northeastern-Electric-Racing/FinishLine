576ab30cb0339738682ea88c9e1cb643efbd42e8
ninest <parth.kabra@gmail.com>
2023-07-05 19:13:02 -0400

    #619 Remove backend jsdom test setup

--- src/backend/vite.config.js
+++ src/backend/vite.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['**/*.test.ts'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'tests/setup-tests.ts'
+    globals: true
   }
 });
