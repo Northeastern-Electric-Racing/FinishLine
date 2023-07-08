ec0870aa680a932f131d6c4653d3819540f51e15
RChandler234 <reid.c.chandler@gmail.com>
2023-07-08 15:38:51 -0400

    #619 - rollback change

--- src/frontend/src/app/AppPublic.tsx
+++ src/frontend/src/app/AppPublic.tsx
@@ -26,8 +26,6 @@ const AppPublic: React.FC = () => {
       return <AppAuthenticated userId={auth.user.userId} />;
     }
 
-    console.log(`env${import.meta.env.VITE_REACT_APP_BACKEND_URL}`);
-
     // if we're on development and the userId is stored in localStorage,
     // then dev login right away (no login page redirect needed!)
     if (import.meta.env.MODE === 'development' && devUserId) {
