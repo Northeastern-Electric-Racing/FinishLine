5d9876dc495aca0d1a6891a10a9ddd3901de6017
RChandler234 <reid.c.chandler@gmail.com>
2023-07-08 15:20:06 -0400

    #619 - qa is broken

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+
+
+const reactEnv = {};
+
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    reactEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+});
+
 
 export default defineConfig({
+  alias: {
+    '@': require('path').resolve(__dirname, 'src')
+  },
+  define: reactEnv,
   plugins: [react()],
   server: {
     port: 3000
