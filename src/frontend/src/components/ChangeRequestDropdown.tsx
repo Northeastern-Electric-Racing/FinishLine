4950118716214aba2280e580475f325b570e970a
ninest <parth.kabra@gmail.com>
2023-08-08 17:24:45 -0400

    #1027 Add '5 days ago' filtering for CRs

--- src/frontend/src/components/ChangeRequestDropdown.tsx
+++ src/frontend/src/components/ChangeRequestDropdown.tsx
@@ -1,7 +1,9 @@
 import { Control, Controller } from 'react-hook-form';
+import { subDays, isWithinInterval } from 'date-fns';
 import { wbsPipe } from 'shared';
 import { useAllChangeRequests } from '../hooks/change-requests.hooks';
 import NERAutocomplete from './NERAutocomplete';
+import LoadingIndicator from './LoadingIndicator';
 
 interface ChangeRequestDropdownProps {
   control: Control<any, any>;
@@ -9,10 +11,15 @@ interface ChangeRequestDropdownProps {
 }
 
 const ChangeRequestDropdown = ({ control, name }: ChangeRequestDropdownProps) => {
-  const { data: changeRequests } = useAllChangeRequests();
+  const { isLoading, data: changeRequests } = useAllChangeRequests();
+  if (isLoading || !changeRequests) return <LoadingIndicator />;
+
+  const today = new Date();
+  const fiveDaysAgo = subDays(today, 5);
+
   const options =
     changeRequests
-      ?.filter((cr) => cr.accepted)
+      ?.filter((cr) => !!cr.dateReviewed && isWithinInterval(cr.dateReviewed, { start: fiveDaysAgo, end: today }))
       .map((cr) => ({
         label: `${cr.crId} - ${wbsPipe(cr.wbsNum)} - ${cr.submitter.firstName} ${cr.submitter.lastName}`,
         id: `${cr.crId}`
@@ -22,12 +29,12 @@ const ChangeRequestDropdown = ({ control, name }: ChangeRequestDropdownProps) =>
     <Controller
       control={control}
       name={name}
-      render={({ field: { onChange, value, name } }) => (
+      render={({ field: { onChange, value } }) => (
         <NERAutocomplete
-          id="cr"
+          id="cr-autocomplete"
           options={options}
           value={options.find((option) => option.id === value)}
-          onChange={(event, value) => {
+          onChange={(_event, value) => {
             if (value) onChange(value.id);
           }}
           size={'small'}
