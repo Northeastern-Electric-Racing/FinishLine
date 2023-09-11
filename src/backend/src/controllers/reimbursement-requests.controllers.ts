235652df636a7347eb72dbff40ac7ebfe1bc3043
RChandler234 <reid.c.chandler@gmail.com>
2023-09-10 20:08:37 -0400

    rollback-upload-receipt-change

--- src/backend/src/controllers/reimbursement-requests.controllers.ts
+++ src/backend/src/controllers/reimbursement-requests.controllers.ts
@@ -182,7 +182,6 @@ export default class ReimbursementRequestsController {
 
       const receipt = await ReimbursementRequestService.uploadReceipt(requestId, file, user);
 
-      res.header('Access-Control-Allow-Origin', 'true');
       res.status(200).json(receipt);
     } catch (error: unknown) {
       next(error);
