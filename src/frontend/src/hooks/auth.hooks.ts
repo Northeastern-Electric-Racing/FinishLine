d2280d1fdd9c12dd7ae0ed74e1bee62fd618bfd7
Sharon Yang <sharonyyang0@gmail.com>
2023-09-14 17:32:55 -0400

    #1250: Reverted changes to auth.hooks.ts

--- src/frontend/src/hooks/auth.hooks.ts
+++ src/frontend/src/hooks/auth.hooks.ts
@@ -8,38 +8,24 @@ import { AuthenticatedUser } from 'shared';
 import { AuthContext } from '../app/AppContextAuth';
 import { useLogUserIn, useLogUserInDev } from './users.hooks';
 import { Auth } from '../utils/types';
-import { useToast } from './toasts.hooks';
 
 // Provider hook that creates auth object and handles state
 export const useProvideAuth = () => {
   const { isLoading, mutateAsync } = useLogUserIn();
   const { isLoading: isLoadingDev, mutateAsync: mutateAsyncDev } = useLogUserInDev();
   const [user, setUser] = useState<AuthenticatedUser | undefined>(undefined);
-  const toast = useToast();
 
   const devSignin = async (userId: number) => {
-    try {
-      const user = await mutateAsyncDev(userId);
-      setUser(user);
-      localStorage.setItem('devUserId', userId.toString());
-      return user;
-    } catch (e) {
-      if (e instanceof Error) {
-        toast.error(e.message);
-      }
-    }
+    const user = await mutateAsyncDev(userId);
+    setUser(user);
+    localStorage.setItem('devUserId', userId.toString());
+    return user;
   };
 
   const signin = async (id_token: string) => {
-    try {
-      const user = await mutateAsync(id_token);
-      setUser(user);
-      return user;
-    } catch (e) {
-      if (e instanceof Error) {
-        toast.error(e.message);
-      }
-    }
+    const user = await mutateAsync(id_token);
+    setUser(user);
+    return user;
   };
 
   const signout = () => {
