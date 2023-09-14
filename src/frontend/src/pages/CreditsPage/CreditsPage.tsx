7a9ed4911875b8380570cb469f7cce23ae74db41
naakjgf <naakjgf@gmail.com>
2023-09-13 21:28:56 -0400

    #1382 Added my name to credit

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -105,6 +105,15 @@ const CreditsPage: React.FC = () => {
     { name: 'Sharon Yang', color: '#ed8a5f' },
     { name: 'Kevin Yang', color: '#0000FF' },
     { name: 'Hareg Aderie', color: '#34b46c' },
+    {
+      name: 'Kaan Tural',
+      color: 'transparent',
+      sx: {
+        background: 'linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(0,128,255,1) 100%)',
+        '-webkit-background-clip': 'text',
+        textShadow: '0 0 5px rgba(255,0,0,1), 0 0 10px rgba(0,128,255,1)'
+      }
+    },
     { name: 'Kevin Polackal', color: '#800080' }
   ];
 
