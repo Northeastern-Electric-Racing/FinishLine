548fc92261298b413b9455ee85e7d12c92cc2ff4
hanguyen04 <nganha.nguyen3011@gmail.com>
2023-09-13 20:50:53 -0400

    #1381 Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -102,7 +102,8 @@ const CreditsPage: React.FC = () => {
     { name: 'Waasif Mahmood', color: '#114a13' },
     { name: 'Matthew Wang', color: '#c657f2' },
     { name: 'Sharon Yang', color: '#ed8a5f' },
-    { name: 'Kevin Polackal', color: '#800080' }
+    { name: 'Kevin Polackal', color: '#800080' },
+    { name: 'Ha Nguyen', color: '#ff9812' }
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
