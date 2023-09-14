2d74b422ea2a3bdcc82abb18a5a45756b7afbb69
Teera Tesharojanasup <teerareal74@gmail.com>
2023-09-13 20:37:18 -0400

    #1375: Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -94,7 +94,8 @@ const CreditsPage: React.FC = () => {
       }
     },
     { name: 'Ethan Mouri', color: '#00bbff' },
-    { name: 'Laith Taher', color: '#000080' }
+    { name: 'Laith Taher', color: '#000080' },
+    { name: 'Teera Tesharojanasup', color: '#DC143C', sx: { px: 1, backgroundColor: '#E8D8CC', borderRadius: 10 } }
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
