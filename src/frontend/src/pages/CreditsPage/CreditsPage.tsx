72ea02f7acb4ad44f29612c8a4d60435ea78e567
Jack Turner <redjackt@outlook.com>
2023-09-14 11:11:33 -0400

    #1386 Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -112,6 +112,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Kevin Polackal', color: '#800080' },
     { name: 'Raghav Mathur', color: '#009933' },
     { name: 'Anika Sharma', color: '#ff0000' },
+    { name: 'William (Jack) Turner', color: '#ff5733' },
     {
       name: 'Kaan Tural',
       color: 'transparent',
