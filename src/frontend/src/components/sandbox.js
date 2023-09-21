8850d5fe4386a4462e0acc7d5da6d24f7b0de30e
shreesinghal <119455146+shreesinghal@users.noreply.github.com>
2023-09-20 20:31:24 -0400

    #1464 Added my name to credits

---
+++ src/frontend/src/components/sandbox.js
@@ -0,0 +1,23 @@
+const person1 = {
+    "name": "John Doe",
+    "startLocation": {"x": 10, "y": 20},
+    "endLocation": {"x": -30, "y": -40},
+    "startHour": 8,
+    "startMinute": 30,
+    "endHour": 9,
+    "endMinute": 45,
+    "role": "Rider"
+}
+
+const person2 = {
+    "name": "Jane Smith",
+    "startLocation": {"x": 15, "y": -25},
+    "endLocation": {"x": -35, "y": 45},
+    "startHour": 10,
+    "startMinute": 0,
+    "endHour": 11,
+    "endMinute": 15,
+    "role": "Driver"
+}
+
+console.log(person2["name"]);
