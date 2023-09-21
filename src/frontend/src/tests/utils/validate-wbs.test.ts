a81b77de98848211961e10bb686aba687fb2ea7c
harish <harivarada1@gmail.com>
2023-09-21 15:11:58 -0400

    #536 changed describe statement

--- src/frontend/src/tests/utils/validate-wbs.test.ts
+++ src/frontend/src/tests/utils/validate-wbs.test.ts
@@ -2,7 +2,7 @@ import { wbsNumComparator } from 'shared';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('wbsNumComparator', () => {
-  it('should correctly compare two numbers', () => {
+  it('should correctly compare two WBS Numbers', () => {
     expect(wbsNumComparator('1.1.3', '1.3.4')).toBe(-1);
     expect(wbsNumComparator('1.2.1', '1.2.1')).toBe(0);
     expect(wbsNumComparator('1.3.4', '1.1.3')).toBe(1);
