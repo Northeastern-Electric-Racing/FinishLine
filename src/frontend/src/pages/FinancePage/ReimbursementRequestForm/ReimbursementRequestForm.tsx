720a49dac899bcfef43bb7a75c52168ce98baa59
Sharon Yang <sharonyyang0@gmail.com>
2023-09-24 18:41:31 -0400

    #1310: Cleaning code

--- src/frontend/src/pages/FinancePage/ReimbursementRequestForm/ReimbursementRequestForm.tsx
+++ src/frontend/src/pages/FinancePage/ReimbursementRequestForm/ReimbursementRequestForm.tsx
@@ -122,11 +122,10 @@ const ReimbursementRequestForm: React.FC<ReimbursementRequestFormProps> = ({
   } = useAllProjects();
 
   // checking the data here instead of using isError since function doesn't ever return an error
-  const { data, isLoading: checkSecureSettingsIsLoading } = useCurrentUserSecureSettings();
-  const userSecureSettings = data ?? {};
-
+  const { data : userSecureSettings, isLoading: checkSecureSettingsIsLoading } = useCurrentUserSecureSettings();
+  
   // checks to make sure none of the secure settings fields are empty, indicating not properly set
-  const hasSecureSettingsSet = Object.values(userSecureSettings).every((x) => x !== '') ? true : false;
+  const hasSecureSettingsSet = Object.values(userSecureSettings ?? {}).every((x) => x !== '') ? true : false;
 
   const toast = useToast();
   const history = useHistory();
