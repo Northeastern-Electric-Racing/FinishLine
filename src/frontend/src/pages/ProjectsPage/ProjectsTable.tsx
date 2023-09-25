42a161af596f39c282b9de16dbf10618a0ffb7c4
Sharon Yang <sharonyyang0@gmail.com>
2023-09-25 15:10:38 -0400

    #1167: Removed unnecessary import

--- src/frontend/src/pages/ProjectsPage/ProjectsTable.tsx
+++ src/frontend/src/pages/ProjectsPage/ProjectsTable.tsx
@@ -2,7 +2,6 @@
  * This file is part of NER's FinishLine and licensed under GNU AGPLv3.
  * See the LICENSE file in the repository root folder for details.
  */
-import React from 'react';
 import { Grid, Link, useTheme } from '@mui/material';
 import { DataGrid, GridColDef, GridFilterModel, GridRow, GridRowProps, GridToolbar } from '@mui/x-data-grid';
 import { useState } from 'react';
