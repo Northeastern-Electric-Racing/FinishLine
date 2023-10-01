148c43adb8767944cbca419f3578be768f36e022
UckTay <56901754+UckTay@users.noreply.github.com>
2023-09-30 21:37:28 -0700

    Update finance.hooks.ts
    Changed mutation type to Vendor

--- src/frontend/src/hooks/finance.hooks.ts
+++ src/frontend/src/hooks/finance.hooks.ts
@@ -369,7 +369,7 @@ export const useCreateAccountCode = () => {
  */
 export const useCreateVendor = () => {
   const queryClient = useQueryClient();
-  return useMutation<{ message: string }, Error, { name: string }>(
+  return useMutation<Vendor, Error, { name: string }>(
     ['vendors', 'create'],
     async (vendorData: { name: string }) => {
       const { data } = await createVendor(vendorData);
