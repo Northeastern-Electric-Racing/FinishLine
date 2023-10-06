62a472d8b2c5680758ed228bb3d7def500cce6a9
yashjay1 <120078061+yashjay1@users.noreply.github.com>
2023-10-06 14:15:17 -0400

    Updated CreditsPage.tsx to add my name

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -143,7 +143,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Neel Raut', color: '#023665' },
     { name: 'Qihong Wu', color: '#87CEEB' },
     { name: 'Megan Lai', color: '#52B2BF' },
-    { name: 'Eric Sun', color: '#FCCAED' }
+    { name: 'Eric Sun', color: '#FCCAED' },
     { name: 'Yash Jayaprakash', color: '#66b2b2' }
   ];
 
