3cbc671be0b932c50cd64acb230769180b1d391b
ninest <parth.kabra@gmail.com>
2023-10-16 11:10:50 -0400

    #1320 Remove commented create service

--- src/backend/src/controllers/projects.controllers.ts
+++ src/backend/src/controllers/projects.controllers.ts
@@ -26,18 +26,6 @@ export default class ProjectsController {
     }
   }
 
-  // static async createProject(req: Request, res: Response, next: NextFunction) {
-  //   try {
-  //     const user: User = await getCurrentUser(res);
-  //     const { crId, carNumber, name, summary, teamId } = req.body;
-
-  //     const createdWbsNumber: WbsNumber = await ProjectsService.createProject(user, crId, carNumber, name, summary, teamId);
-
-  //     return res.status(200).json(wbsPipe(createdWbsNumber));
-  //   } catch (error: unknown) {
-  //     next(error);
-  //   }
-  // }
   static async createProject(req: Request, res: Response, next: NextFunction) {
     try {
       const user: User = await getCurrentUser(res);
