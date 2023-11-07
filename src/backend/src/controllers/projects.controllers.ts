955146ffd7385854d103034deb40d824816e545c
aaryan1203 <aaryanjain1203@gmail.com>
2023-11-06 19:52:23 -0500

    fixed promise bug

--- src/backend/src/controllers/projects.controllers.ts
+++ src/backend/src/controllers/projects.controllers.ts
@@ -164,7 +164,7 @@ export default class ProjectsController {
   static async deleteMaterialType(req: Request, res: Response, next: NextFunction) {
     try {
       const { materialTypeId } = req.params;
-      const user = getCurrentUser(res);
+      const user = await getCurrentUser(res);
       const deletedMaterial = await ProjectsService.deleteMaterialType(materialTypeId, user);
       res.status(200).json(deletedMaterial);
     } catch (error: unknown) {
