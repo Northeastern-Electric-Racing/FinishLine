2acdefc1816f92cc3c9153ad150082738c160311
Liu von Engelbrechten <vonengelbrechten.l@northeastern.edu>
2023-11-08 20:23:47 -0500

    Revert "#1249: created displayEnum pipe and applied"
    This reverts commit 2dca1ca6a3cd9d491eac89aca859f34f2ab7bdb9.

--- src/frontend/src/utils/pipes.ts
+++ src/frontend/src/utils/pipes.ts
@@ -129,20 +129,6 @@ export const centsToDollar = (cents: number) => {
   return (cents / 100.0).toFixed(2);
 };
 
-// Takes in an enum string to capitalize first letter of each word and gets rid of underscore
-export const displayEnum = (enumString: string) => {
-  enumString = enumString.toLowerCase();
-  while (enumString.indexOf('_') !== -1) {
-    enumString =
-      enumString.substring(0, enumString.indexOf('_')) +
-      ' ' +
-      enumString.charAt(enumString.indexOf('_') + 1).toUpperCase() +
-      enumString.slice(enumString.indexOf('_') + 2);
-  }
-  enumString = enumString.charAt(0).toUpperCase() + enumString.slice(1);
-  return enumString;
-};
-
 /** Displays a refund source as a string "Code - Name" */
 export const codeAndRefundSourceName = (refundSource: ClubAccount) => {
   const CASH_ACCOUNT_CODE = 830667;
