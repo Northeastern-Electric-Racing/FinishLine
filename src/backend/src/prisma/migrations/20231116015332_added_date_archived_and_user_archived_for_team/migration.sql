33b5ca10b3c89bcf0ac0fe3d52fc60c4a44c4728
Megan Liu <meganliu@megans-macbook.mynetworksettings.com>
2023-11-28 21:30:10 -0500

    #1604: updated User model to reflect changes in Team model, deleted old migrations, and made one big new migration

--- src/backend/src/prisma/migrations/20231116015332_added_date_archived_and_user_archived_for_team/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Team" ADD COLUMN     "dateArchived" TIMESTAMP(3),
-ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Team" ADD CONSTRAINT "Team_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
