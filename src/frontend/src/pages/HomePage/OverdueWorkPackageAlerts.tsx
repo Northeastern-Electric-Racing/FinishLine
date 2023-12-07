83072a45ead6aa9c6cc759b48f102b34afff79c0
ItsEricSun <ericsun828@gmail.com>
2023-12-06 23:42:04 -0500

    #1565: Fix linting errors

--- src/frontend/src/pages/HomePage/OverdueWorkPackageAlerts.tsx
+++ src/frontend/src/pages/HomePage/OverdueWorkPackageAlerts.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { useAllWorkPackages } from '../../hooks/work-packages.hooks';
 import { WbsElementStatus } from 'shared';
 import { useCurrentUser } from '../../hooks/users.hooks';
-import { useState } from 'react';
 import { wbsPipe } from 'shared';
-import { Box, Alert, IconButton, Collapse, Typography, AlertTitle, Grid } from '@mui/material';
+import { Box, Alert, Typography, AlertTitle, Grid } from '@mui/material';
 import { routes } from '../../utils/routes';
 import { datePipe } from '../../utils/pipes';
 import { NERButton } from '../../components/NERButton';
@@ -14,7 +13,6 @@ const OverdueWorkPackageAlerts: React.FC = () => {
   const user = useCurrentUser();
   const workPackages = useAllWorkPackages({ status: WbsElementStatus.Active });
   const currentDate = new Date();
-  const [open, setOpen] = useState(true);
   const history = useHistory();
 
   // Filter for work packages that are overdue and the user is the project lead
@@ -28,40 +26,38 @@ const OverdueWorkPackageAlerts: React.FC = () => {
   } else {
     return (
       <Box sx={{ width: '100%', my: 2 }}>
-        <Collapse in={open}>
-          <Alert
-            variant="filled"
-            severity="warning"
-            sx={{
-              '& .MuiAlert-message': {
-                width: '100%'
-              }
-            }}
-          >
-            <Box>
-              <AlertTitle>
-                {userOverdueWorkPackages.length > 1 ? 'Overdue Work Packages:' : 'Overdue Work Package:'}
-              </AlertTitle>
-              <Grid container spacing={2}>
-                {userOverdueWorkPackages.map((wp) => (
-                  <Grid item xs={6} md={3} key={wp.id}>
-                    {wbsPipe(wp.wbsNum)} - {wp.name}
-                    <Typography fontWeight={'regular'} variant="inherit" noWrap my={0.5}>
-                      {'Due: ' + datePipe(wp.endDate)}
-                    </Typography>
-                    <NERButton
-                      variant="contained"
-                      size="small"
-                      onClick={() => history.push(`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`)}
-                    >
-                      Create Change Request
-                    </NERButton>
-                  </Grid>
-                ))}
-              </Grid>
-            </Box>
-          </Alert>
-        </Collapse>
+        <Alert
+          variant="filled"
+          severity="warning"
+          sx={{
+            '& .MuiAlert-message': {
+              width: '100%'
+            }
+          }}
+        >
+          <Box>
+            <AlertTitle>
+              {userOverdueWorkPackages.length > 1 ? 'Overdue Work Packages:' : 'Overdue Work Package:'}
+            </AlertTitle>
+            <Grid container spacing={2}>
+              {userOverdueWorkPackages.map((wp) => (
+                <Grid item xs={6} md={3} key={wp.id}>
+                  {wbsPipe(wp.wbsNum)} - {wp.name}
+                  <Typography fontWeight={'regular'} variant="inherit" noWrap my={0.5}>
+                    {'Due: ' + datePipe(wp.endDate)}
+                  </Typography>
+                  <NERButton
+                    variant="contained"
+                    size="small"
+                    onClick={() => history.push(`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`)}
+                  >
+                    Create Change Request
+                  </NERButton>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Alert>
       </Box>
     );
   }
