eaf67f5f829a6629525691f0b6fa2d5e2478f113
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-19 16:40:16 +0200

    #1640: linting and formatting

--- src/frontend/src/tests/pages/ChangeRequestDetailPage/ImplementedChangesList.test.tsx
+++ src/frontend/src/tests/pages/ChangeRequestDetailPage/ImplementedChangesList.test.tsx
@@ -7,7 +7,6 @@ import { render, screen, routerWrapperBuilder } from '../../test-support/test-ut
 import { ImplementedChange } from 'shared';
 import { exampleStandardImplementedChangeRequest } from '../../test-support/test-data/change-requests.stub';
 import ImplementedChangesList from '../../../pages/ChangeRequestDetailPage/ImplementedChangesList';
-import { datePipe } from '../../../utils/pipes';
 
 /**
  * Sets up the component under test with the desired values and renders it.
