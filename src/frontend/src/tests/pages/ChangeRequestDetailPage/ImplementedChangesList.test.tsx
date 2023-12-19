dcdb1c5a45905483d3070c19114b85b51b8776d7
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-19 17:25:47 +0200

    #1640: pray testing works

--- src/frontend/src/tests/pages/ChangeRequestDetailPage/ImplementedChangesList.test.tsx
+++ src/frontend/src/tests/pages/ChangeRequestDetailPage/ImplementedChangesList.test.tsx
@@ -28,7 +28,7 @@ describe('Rendering Implemented Changes List Component', () => {
     expect(
       screen.getByText(
         `Implemented Changes — ${
-          exampleStandardImplementedChangeRequest.dateImplemented !== null
+          exampleStandardImplementedChangeRequest.dateImplemented !== undefined
             ? datePipe(exampleStandardImplementedChangeRequest.dateImplemented)
             : 'N/A'
         }`
