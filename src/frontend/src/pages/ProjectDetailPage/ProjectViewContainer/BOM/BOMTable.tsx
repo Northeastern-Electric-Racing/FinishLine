1a9fd1b5bc1e50dacf79c057fc8ad15bc6f87579
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2024-01-14 13:27:00 -0500

    console log, oopsie

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOM/BOMTable.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOM/BOMTable.tsx
@@ -13,7 +13,6 @@ interface BOMTableProps {
 
 const BOMTable: React.FC<BOMTableProps> = ({ columns, materials, assemblies }) => {
   const noAssemblyMaterials = materials.filter((material) => !material.assembly);
-  console.log(materials);
 
   const rows: BomRow[] = noAssemblyMaterials.map(materialToRow);
 
