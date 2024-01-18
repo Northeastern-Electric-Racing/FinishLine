880b0c7abee828f983e1e23e2a872e27332cbd4f
Emma Shum <emmashum@Emmas-MacBook-Air-2.local>
2024-01-18 00:15:46 -0500

    #1826 Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -151,6 +151,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Lauren Phan', color: '#F3A7C0' },
     { name: 'Vidyuth Ramkumar', color: '#800000' },
     { name: 'Jake Hensley', color: '#FFA500' },
+    { name: 'Emma Shum', color: '#d4878f' },
     {
       name: 'Wyatt Bracy',
       color: '#080808',
