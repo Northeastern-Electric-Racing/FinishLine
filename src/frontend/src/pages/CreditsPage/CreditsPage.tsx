aa8e6f50a88710b927a216dea3551d317702c58c
alexkylander <kylander05@gmail.com>
2024-01-20 18:12:15 -0500

    #1865 Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -157,7 +157,8 @@ const CreditsPage: React.FC = () => {
       sx: {
         textShadow: '0 0 2px rgba(0,0,0), 0 0 20px rgba(220,220,220,1)'
       }
-    }
+    },
+    { name: 'Alexander Kylander-Kreiner', color: '#82EEFD' }
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
