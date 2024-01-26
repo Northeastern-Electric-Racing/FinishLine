6312abdfa0572f9525efefb17c56d165df029ba4
vsp05 <vspillai02@gmail.com>
2024-01-26 16:10:18 -0500

    #1499: changed props types to only React.ReactNode and null

--- src/frontend/src/tests/app/AppContext.test.tsx
+++ src/frontend/src/tests/app/AppContext.test.tsx
@@ -9,9 +9,7 @@ import AppContext from '../../app/AppContext';
 vi.mock('../../app/AppContextQuery', () => {
   return {
     __esModule: true,
-    default: (props: {
-      children: string | number | boolean | {} | React.ReactNodeArray | React.ReactPortal | null | undefined;
-    }) => {
+    default: (props: { children: React.ReactNode | null }) => {
       return <div>app context query {props.children}</div>;
     }
   };
@@ -20,9 +18,7 @@ vi.mock('../../app/AppContextQuery', () => {
 vi.mock('../../app/AppContextAuth', () => {
   return {
     __esModule: true,
-    default: (props: {
-      children: string | number | boolean | {} | React.ReactNodeArray | React.ReactPortal | null | undefined;
-    }) => {
+    default: (props: { children: React.ReactNode | null }) => {
       return <div>app context auth {props.children}</div>;
     }
   };
@@ -31,9 +27,7 @@ vi.mock('../../app/AppContextAuth', () => {
 vi.mock('../../app/AppContextTheme', () => {
   return {
     __esModule: true,
-    default: (props: {
-      children: string | number | boolean | {} | React.ReactNodeArray | React.ReactPortal | null | undefined;
-    }) => {
+    default: (props: { children: React.ReactNode | null }) => {
       return <div>app context theme {props.children}</div>;
     }
   };
