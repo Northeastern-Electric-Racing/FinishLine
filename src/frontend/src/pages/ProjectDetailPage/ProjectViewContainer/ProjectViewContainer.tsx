1a7ab010adeb7e038e5b81786ea3b0ff2d5fb581
aahil n <nishad.a@northeastern.edu>
2024-01-30 17:40:11 -0500

    #1785: Disabled Guest Project Actions--fixed prettier

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/ProjectViewContainer.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/ProjectViewContainer.tsx
@@ -157,7 +157,8 @@ const ProjectViewContainer: React.FC<ProjectViewContainerProps> = ({ project, en
         endIcon={<ArrowDropDownIcon style={{ fontSize: 28 }} />}
         variant="contained"
         id="project-actions-dropdown"
-        onClick={handleClick} disabled={isGuest(user.role)}
+        onClick={handleClick}
+        disabled={isGuest(user.role)}
       >
         Actions
       </NERButton>
