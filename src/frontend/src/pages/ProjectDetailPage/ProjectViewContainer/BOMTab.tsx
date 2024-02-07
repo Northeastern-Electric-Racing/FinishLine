9f00c96a0d8c2d951b8d636da4ad4b95f0bf8d5d
Sarah-Zhang1 <zhang.sarah1@northeastern.edu>
2024-02-07 18:11:36 -0500

    1788: fixing prettier check error

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOMTab.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOMTab.tsx
@@ -39,7 +39,7 @@ const BOMTab = ({ project }: { project: Project }) => {
             >
               New Entry
             </NERSuccessButton>
-            <NERButton variant="contained" onClick={() => setShowAddAssembly(true)} disabled={isGuest(useCurrentUser().role)}>
+            <NERButton variant="contained" onClick={() => setShowAddAssembly(true)} disabled={isGuest(user.role)}>
               New Assembly
             </NERButton>
           </Box>
