313efd15b88288889b853649c59a0faa80e05ef6
pidang1 <dang.pi@northeastern.edu>
2024-02-12 01:21:10 -0500

    #1300: show error message

--- src/frontend/src/pages/WorkPackageForm/WorkPackageFormDetails.tsx
+++ src/frontend/src/pages/WorkPackageForm/WorkPackageFormDetails.tsx
@@ -103,8 +103,14 @@ const WorkPackageFormDetails: React.FC<Props> = ({
                     onChange={(date) => onChange(date ?? new Date())}
                     className={'padding: 10'}
                     value={value}
-                    shouldDisableDate={disableStartDate}
-                    renderInput={(params) => <TextField autoComplete="off" {...params} />}
+                    renderInput={(params) => (
+                      <TextField
+                        autoComplete="off"
+                        {...params}
+                        error={!!errors.startDate}
+                        helperText={errors.startDate?.message}
+                      />
+                    )}
                   />
                 </>
               )}
