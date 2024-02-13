43a6565435e6c624ab9a5768c02ae11acb6461f6
Bradford Derby <derby.b@northeastern.edu>
2024-02-13 15:57:12 -0500

    #2011 delete design review endpoint

--- src/backend/src/routes/design-review.routes.ts
+++ src/backend/src/routes/design-review.routes.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import DesignReviewController from '../controllers/design-review.controllers';
 
 const designReviewRouter = express.Router();
+
+designReviewRouter.delete('/:drId/delete', DesignReviewController.deleteDesignReview);
 
 export default designReviewRouter;
--- src/backend/src/services/design-review.services.ts
+++ src/backend/src/services/design-review.services.ts
@@ -1,1 +1,30 @@
-export default class DesignReviewService {}
+import { isAdmin } from 'shared';
+import prisma from '../prisma/prisma';
+import { AccessDeniedAdminOnlyException, DeletedException, NotFoundException } from '../utils/errors.utils';
+import { User } from '@prisma/client';
+
+export default class DesignReviewService {
+  /**
+   * Deletes a design review
+   * @param submitter the user who deleted the design review
+   * @param designReviewId the id of the design review to be deleted
+   */
+
+  static async deleteDesignReview(submitter: User, designReviewId: string): Promise<void> {
+    const designReview = await prisma.design_Review.findUnique({
+      where: { designReviewId }
+    });
+
+    if (!designReview) throw new NotFoundException('Design Review', designReviewId);
+
+    if (!(isAdmin(submitter.role) || submitter.userId === designReview.userCreatedId))
+      throw new AccessDeniedAdminOnlyException('delete design reviews');
+
+    if (designReview.dateDeleted) throw new DeletedException('Design Review', designReviewId);
+
+    await prisma.design_Review.update({
+      where: { designReviewId },
+      data: { dateDeleted: new Date(), userDeleted: { connect: { userId: submitter.userId } } }
+    });
+  }
+}
