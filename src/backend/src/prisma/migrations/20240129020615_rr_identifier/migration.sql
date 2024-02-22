0e922cab340d400fcba09396b285edbbadc38528
Ji-min Kim <kimjimin1226@gmail.com>
2024-02-21 21:48:53 -0500

    #1661: Updated RR identifier SQL to order RRs in dateCreated order

--- src/backend/src/prisma/migrations/20240129020615_rr_identifier/migration.sql
+++ src/backend/src/prisma/migrations/20240129020615_rr_identifier/migration.sql
@@ -4,6 +4,18 @@
   - A unique constraint covering the columns `[identifier]` on the table `Reimbursement_Request` will be added. If there are existing duplicate values, this will fail.
 
 */
+-- Select all old RRs that has no identifier in dateCreated ascending order
+CREATE TEMP TABLE cte_table AS (
+	WITH CTE AS (
+		SELECT 
+			"reimbursementRequestId",
+			row_number() OVER (ORDER BY "dateCreated" ASC) AS new_identifier
+		FROM 
+			"Reimbursement_Request"
+	)
+	SELECT * FROM CTE
+);
+
 -- AlterTable
 ALTER TABLE "Reimbursement_Request" ADD COLUMN     "identifier" SERIAL NOT NULL;
 
@@ -11,16 +23,10 @@ ALTER TABLE "Reimbursement_Request" ADD COLUMN     "identifier" SERIAL NOT NULL;
 CREATE UNIQUE INDEX "Reimbursement_Request_identifier_key" ON "Reimbursement_Request"("identifier");
 
 -- Assign Identifier to all old Reimbursement Requests based on their dateCreated order
-WITH CTE AS (
-    SELECT 
-        "reimbursementRequestId",
-        ROW_NUMBER() OVER (ORDER BY "dateCreated" ASC) AS new_identifier
-    FROM 
-        "Reimbursement_Request"
-    WHERE
-        "identifier" IS NULL
-)
 UPDATE "Reimbursement_Request" AS rr
-SET "identifier" = cte.new_identifier
-FROM CTE
-WHERE rr."reimbursementRequestId" = CTE."reimbursementRequestId";
+SET "identifier" = "cte_table".new_identifier
+FROM cte_table
+WHERE rr."reimbursementRequestId" = "cte_table"."reimbursementRequestId";
+
+-- Remove the temporary table
+DROP TABLE cte_table;
