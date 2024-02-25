22ea826352ee1792dfb197aedb1c14ca5de7fc5d
Vishy K <visisht@gmail.com>
2024-02-25 17:49:06 -0500

    #2060 added test file and fixed naming

--- src/backend/src/controllers/design-review.controllers.ts
+++ src/backend/src/controllers/design-review.controllers.ts
@@ -1,1 +1,15 @@
-export default class DesignReviewController {}
+import { NextFunction, Request, Response } from 'express';
+import DesignReviewService from '../services/design-review.services';
+
+export default class DesignReviewController {
+  static async getSingleUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { designReviewId } = req.params;
+      const requestedDesignReview = await DesignReviewService.getSingleUser(designReviewId);
+
+      res.status(200).json(requestedDesignReview);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+}
