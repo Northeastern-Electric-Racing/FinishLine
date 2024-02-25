35f2c1d2886d3e06e16a562ae954d03ec6386997
Vishy K <visisht@gmail.com>
2024-02-25 17:39:17 -0500

    #2060: created get single design review endpoint

--- src/backend/src/routes/design-review.routes.ts
+++ src/backend/src/routes/design-review.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import DesignReviewController from '../controllers/design-review.controllers';
 
 const designReviewRouter = express.Router();
 
+designReviewRouter.get('/:DesignReviewId', DesignReviewController.getSingleUser);
 export default designReviewRouter;
--- src/backend/src/services/design-review.services.ts
+++ src/backend/src/services/design-review.services.ts
@@ -1,1 +1,17 @@
-export default class DesignReviewService {}
+import { Design_Review } from '@prisma/client';
+import prisma from '../prisma/prisma';
+import { NotFoundException } from '../utils/errors.utils';
+
+export default class DesignReviewService {
+  /**
+   * Gets the design review with the specified id
+   * @param designReviewId the id of the design reivew that's returned
+   * @returns the design review with the specified id
+   * @throws if the given design review id doesn't exist
+   */
+  static async getSingleUser(designReviewId: string): Promise<Design_Review> {
+    const requestedDesignReview = await prisma.design_Review.findUnique({ where: { designReviewId } });
+    if (!requestedDesignReview) throw new NotFoundException('Design Review', designReviewId);
+    return requestedDesignReview;
+  }
+}
