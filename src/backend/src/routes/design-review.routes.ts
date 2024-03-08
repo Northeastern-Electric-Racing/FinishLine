624f4d9b98c85450116adda17e0523a0e223008e
walker-sean <walker.se@northeastern.edu>
2024-03-08 17:36:45 -0500

    #2060 endpoint and tests work

--- src/backend/src/routes/design-review.routes.ts
+++ src/backend/src/routes/design-review.routes.ts
@@ -4,5 +4,6 @@ import DesignReviewController from '../controllers/design-review.controllers';
 const designReviewRouter = express.Router();
 
 designReviewRouter.delete('/:designReviewId/delete', DesignReviewController.deleteDesignReview);
+designReviewRouter.get('/:designReviewId', DesignReviewController.getSingleDesignReview);
 
 export default designReviewRouter;
