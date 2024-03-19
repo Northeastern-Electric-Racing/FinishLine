c364073c06ff7393e609b0ae76b9e4abce2a12d6
rogerhuang <haoming5674@gmail.com>
2024-03-18 21:10:45 -0400

    #2141 - further fixes

--- src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
+++ src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
@@ -1,21 +1,17 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import VendorsTable from './FinanceConfig/VendorsTable';
 import AccountCodesTable from './FinanceConfig/AccountCodesTable';
 
 const AdminToolsFinanceConfig: React.FC = () => {
   return (
-    <div>
-      <Typography variant="h4">Finance Config</Typography>
-      <br />
-      <Grid container spacing="3%">
-        <Grid item direction="column" xs={12} md={6}>
-          <VendorsTable />
-        </Grid>
-        <Grid item direction="column" alignSelf="right" xs={12} md={6}>
-          <AccountCodesTable />
-        </Grid>
+    <Grid container spacing="3%" paddingLeft="5px">
+      <Grid item direction="column" xs={12} md={6}>
+        <VendorsTable />
       </Grid>
-    </div>
+      <Grid item direction="column" alignSelf="right" xs={12} md={6}>
+        <AccountCodesTable />
+      </Grid>
+    </Grid>
   );
 };
 
