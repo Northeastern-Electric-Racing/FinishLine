890e301cbab4b829d2ac33d5f8832c180b6c8acd
martin0he <martin.hema.mh@gmail.com>
2024-03-19 00:34:50 -0400

    #2141: minor tweaks

--- src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
+++ src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
@@ -1,17 +1,22 @@
-import { Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import VendorsTable from './FinanceConfig/VendorsTable';
 import AccountCodesTable from './FinanceConfig/AccountCodesTable';
 
 const AdminToolsFinanceConfig: React.FC = () => {
   return (
-    <Grid container spacing="3%" paddingLeft="5px">
-      <Grid item direction="column" xs={12} md={6}>
-        <VendorsTable />
+    <Box padding="5px">
+      <Typography marginBottom="15px" variant="h5">
+        Finance Config
+      </Typography>
+      <Grid container spacing="3%">
+        <Grid item direction="column" xs={12} md={6}>
+          <VendorsTable />
+        </Grid>
+        <Grid item direction="column" alignSelf="right" xs={12} md={6}>
+          <AccountCodesTable />
+        </Grid>
       </Grid>
-      <Grid item direction="column" alignSelf="right" xs={12} md={6}>
-        <AccountCodesTable />
-      </Grid>
-    </Grid>
+    </Box>
   );
 };
 
