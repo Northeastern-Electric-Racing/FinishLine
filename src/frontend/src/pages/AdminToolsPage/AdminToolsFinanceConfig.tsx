6217901249547d7c62b086f36137028ff305b2b5
kennethaldridge <kenneth.r.aldridge@gmail.com>
2024-03-20 21:22:58 -0400

    #2243 changed Role Management, Team Management, Links Config, Bills of Materials Config, Finance Config, Slack Upcoming Deadlines, Design Review Attendee Info  headings in the Admin Tools page to the NER Red

--- src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
+++ src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
@@ -1,11 +1,11 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, useTheme } from '@mui/material';
 import VendorsTable from './FinanceConfig/VendorsTable';
 import AccountCodesTable from './FinanceConfig/AccountCodesTable';
 
 const AdminToolsFinanceConfig: React.FC = () => {
   return (
     <Box padding="5px">
-      <Typography variant="h5" gutterBottom borderBottom={1} color="red" borderColor={'white'}>
+      <Typography variant="h5" gutterBottom borderBottom={1} color="#ef4345" borderColor={'white'}>
         Finance Config
       </Typography>
       <Grid container spacing="3%">
