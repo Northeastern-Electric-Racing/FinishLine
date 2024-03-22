482a7bd600484654a120bb0bb734ac0af0369f67
Sharon Yang <sharonyyang0@gmail.com>
2024-03-21 20:58:05 -0400

    #2139 - show only mondays; resizing is buggy again

--- src/frontend/src/components/GanttChart/GanttChart.tsx
+++ src/frontend/src/components/GanttChart/GanttChart.tsx
@@ -1,11 +1,11 @@
-import { addDays, differenceInDays, eachDayOfInterval, getDate, isMonday, isWeekend } from 'date-fns';
+import { addDays, differenceInDays, eachDayOfInterval, isMonday } from 'date-fns';
 import { ComponentProps, DragEvent, MouseEvent, useEffect, useState } from 'react';
 import useMeasure from 'react-use-measure';
 import { Date_Event, EventChange, applyChangesToEvents } from './event';
 import { dateFormatMonthDate, dateToString } from './date.utils';
 import useId from '@mui/material/utils/useId';
 import dayjs from 'dayjs';
-import { Box, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { purple } from '@mui/material/colors';
 
 interface GanttChartProps {
@@ -15,7 +15,7 @@ interface GanttChartProps {
 }
 
 export function GanttChart({ start, end, data }: GanttChartProps) {
-  const days = eachDayOfInterval({ start, end });
+  const days = eachDayOfInterval({ start, end }).filter((day) => isMonday(day));
 
   const [eventChanges, setEventChanges] = useState<EventChange[]>([]);
   const createChange = (change: EventChange) => {
@@ -28,41 +28,34 @@ export function GanttChart({ start, end, data }: GanttChartProps) {
   const displayEvents = applyChangesToEvents(data, eventChanges);
 
   return (
-    <div>
+    <Box>
       <Box sx={{ overflow: 'scroll', padding: 1, paddingBottom: 2 }}>
         {/* Calendar/timeline */}
         <Box
           sx={{
             display: 'grid',
-            gap: '0.25rem',
+            gap: '0.75rem',
             gridTemplateRows: `repeat(1, minmax(0, 1fr))`,
             gridTemplateColumns: `repeat(${days.length}, minmax(auto, 1fr))`
           }}
         >
           {days.map((day) => {
-            const showDate = isMonday(day);
-            const showMonth = getDate(day) <= 7; // only show month name once
-            const weekend = isWeekend(day);
-            const dateDisplay = showDate ? (showMonth ? dayjs(day).format('MMM D') : dayjs(day).format('D')) : '';
             return (
               <Box
                 key={day.toISOString()}
                 sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: weekend ? '#E0E0E0' : '#424242',
+                  backgroundColor: '#424242',
                   borderRadius: '0.25rem',
                   color: 'white',
-                  fontSize: '0.75rem',
                   lineHeight: '1rem',
                   textAlign: 'center',
-                  height: '2.25rem',
-                  minWidth: '1.56rem',
-                  maxWidth: '1.56rem'
+                  height: '3.25rem',
+                  minWidth: '2.25rem',
+                  maxWidth: '2.25rem'
                 }}
               >
-                {dateDisplay}
+                <Typography fontWeight="bold">{dayjs(day).format('MMM')}</Typography>
+                <Typography fontWeight="bold">{dayjs(day).format('D')}</Typography>
               </Box>
             );
           })}
@@ -103,7 +96,7 @@ export function GanttChart({ start, end, data }: GanttChartProps) {
           );
         })}
       </div>
-    </div>
+    </Box>
   );
 }
 
@@ -113,8 +106,9 @@ function Event({
   createChange,
   ...props
 }: { days: Date[]; event: Date_Event; createChange: (change: EventChange) => void } & ComponentProps<'div'>) {
-  const startCol = days.findIndex((day) => dateToString(day) === dateToString(event.start)) + 1;
-  const endCol = days.findIndex((day) => dateToString(day) === dateToString(event.end)) + 2;
+  const startCol =
+    days.findIndex((day) => dateToString(day) === dateToString(dayjs(event.start).add(1, 'day').toDate())) + 1;
+  const endCol = days.findIndex((day) => dateToString(day) === dateToString(dayjs(event.end).add(1, 'day').toDate())) + 2;
 
   const id = useId() || 'id'; // id for creating event changes
   const [measureRef, bounds] = useMeasure();
@@ -172,7 +166,8 @@ function Event({
     e.preventDefault();
   };
   const onDrop = (day: Date) => {
-    const days = differenceInDays(day, event.start);
+    // makes sure that days is a multiple of 7
+    const days = Math.round(differenceInDays(day, event.start) / 7) * 7;
     createChange({ id, eventId: event.id, type: 'shift-by-days', days });
   };
 
@@ -182,7 +177,7 @@ function Event({
         sx={{
           width: '100%',
           display: 'grid',
-          gap: '0.25rem',
+          gap: '0.75rem',
           gridTemplateRows: `repeat(1, minmax(0, 1fr))`,
           gridTemplateColumns: `repeat(${days.length}, minmax(auto, 1fr))`,
           position: 'absolute',
@@ -199,8 +194,9 @@ function Event({
               onDrop={(e) => onDrop(day)}
               sx={{
                 borderRadius: '0.25rem',
-                height: '2.25rem',
-                padding: '.78rem',
+                height: '2.75rem',
+                minWidth: '2.25rem',
+                maxWidth: '2.25rem',
                 backgroundColor: `rgba(37, 99, 235, 0.1)`
               }}
             />
@@ -209,9 +205,9 @@ function Event({
       <Box
         sx={{
           display: 'grid',
-          gap: '0.25rem',
+          gap: '0.75rem',
           gridTemplateRows: `repeat(1, minmax(0, 1fr))`,
-          gridTemplateColumns: `repeat(${days.length}, minmax(1.56rem, 1fr))`,
+          gridTemplateColumns: `repeat(${days.length}, minmax(2.25rem, 1fr))`,
           width: '100%'
         }}
       >
@@ -221,7 +217,7 @@ function Event({
           style={{
             gridColumnStart: startCol,
             gridColumnEnd: endCol,
-            height: '2.25rem',
+            height: '2.75rem',
             width: width === 0 ? `unset` : `${width}px`,
             border: `1px solid ${isResizing ? 'rgb(37 99 235)' : theme.palette.divider}`,
             borderRadius: '0.25rem',
--- src/frontend/src/pages/GanttPage/GanttChart.tsx
+++ src/frontend/src/pages/GanttPage/GanttChart.tsx
@@ -16,12 +16,12 @@ interface GanttPageProps {
 
 const GanttChart: React.FC<GanttPageProps> = ({ ganttTasks, onExpanderClick }) => {
   const data = [
-    { id: '1', start: new Date('2023-12-25'), end: new Date('2024-01-05'), title: 'First event' },
-    { id: '2', start: new Date('2023-12-27'), end: new Date('2024-01-31'), title: 'Second event' }
+    { id: '1', start: new Date('2023-12-25'), end: new Date('2024-02-05'), title: 'First event' },
+    { id: '2', start: new Date('2024-01-01'), end: new Date('2024-02-19'), title: 'Second event' }
   ];
   return ganttTasks.length > 0 ? (
     <>
-      <NewGanttChart start={new Date('2023-12-25')} end={new Date('2024-12-5')} data={data} />
+      <NewGanttChart start={new Date('2023-12-25')} end={new Date('2024-12-2')} data={data} />
 
       <div style={{ marginBottom: 100, backgroundColor: 'red' }} />
       <Gantt
