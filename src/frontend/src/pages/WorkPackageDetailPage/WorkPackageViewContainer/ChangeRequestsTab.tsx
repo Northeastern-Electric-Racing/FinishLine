17a8ae32f15de3784a8036bae9efb5ea9234cda2
aarav <saarav.as19@gmail.com>
2024-03-27 18:53:14 -0400

    2105 Resolved all comments

--- src/frontend/src/pages/WorkPackageDetailPage/WorkPackageViewContainer/ChangeRequestsTab.tsx
+++ src/frontend/src/pages/WorkPackageDetailPage/WorkPackageViewContainer/ChangeRequestsTab.tsx
@@ -10,7 +10,6 @@ interface ChangeRequestsTabProps {
 
 const ChangeRequestsTab: React.FC<ChangeRequestsTabProps> = ({ workPackage }) => {
   const { data: changeRequests, isError: crIsError, isLoading: crIsLoading, error: crError } = useAllChangeRequests();
-  const currentDate = new Date();
 
   if (crIsLoading || !changeRequests) return <LoadingIndicator />;
   if (crIsError) return <ErrorPage message={crError?.message} />;
@@ -23,13 +22,7 @@ const ChangeRequestsTab: React.FC<ChangeRequestsTabProps> = ({ workPackage }) =>
 
   const approvedChangeRequests = changeRequests
     ? changeRequests
-        .filter(
-          (cr: ChangeRequest) =>
-            cr.dateReviewed &&
-            cr.accepted &&
-            currentDate.getTime() - cr.dateReviewed.getTime() <= 1000 * 60 * 60 * 24 * 5 &&
-            projectWbsPipe(cr.wbsNum) === projectWbsPipe(workPackage.wbsNum)
-        )
+        .filter((cr: ChangeRequest) => cr.accepted && projectWbsPipe(cr.wbsNum) === projectWbsPipe(workPackage.wbsNum))
         .sort((a, b) => (a.dateReviewed && b.dateReviewed ? b.dateReviewed.getTime() - a.dateReviewed.getTime() : 0))
     : [];
 
