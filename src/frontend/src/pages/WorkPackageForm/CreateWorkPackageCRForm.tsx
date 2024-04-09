016c8215f6759e443fde25bdb2e2a55268e0b93a
aaryan1203 <aaryanjain1203@gmail.com>
2024-04-08 22:48:21 -0400

    #1896 created onSubmit

--- src/frontend/src/pages/WorkPackageForm/CreateWorkPackageCRForm.tsx
+++ src/frontend/src/pages/WorkPackageForm/CreateWorkPackageCRForm.tsx
@@ -1,4 +1,4 @@
-import { validateWBS } from 'shared';
+import { ChangeRequestType, validateWBS } from 'shared';
 import WorkPackageForm from './WorkPackageForm';
 import { useHistory } from 'react-router-dom';
 import { routes } from '../../utils/routes';
@@ -6,8 +6,8 @@ import { useQuery } from '../../hooks/utils.hooks';
 import { WorkPackageApiInputs } from '../../apis/work-packages.api';
 import { WPFormType, startDateTester } from '../../utils/form';
 import * as yup from 'yup';
-import { getCurrentUser } from '../../../../backend/src/utils/auth.utils';
 import { useCreateStandardChangeRequest } from '../../hooks/change-requests.hooks';
+import { useCurrentUser } from '../../hooks/users.hooks';
 
 const CreateWorkPackageCRForm: React.FC = () => {
   const query = useQuery();
@@ -16,19 +16,20 @@ const CreateWorkPackageCRForm: React.FC = () => {
 
   if (!wbsNum) throw new Error('WBS number not included in request.');
 
-  const {mutateAsync} = useCreateStandardChangeRequest();
+  const { mutateAsync } = useCreateStandardChangeRequest();
 
+  const currentUser = useCurrentUser();
   const onSubmit = (payload: WorkPackageApiInputs) => {
     const crPayload = {
-      submitter: getCurrentUser(),
-      carNumber: 1, // harcoded example
-      projectNumber: 2, // harcoded example
-      workPackageNumber: 3, // harcoded example
-      type: CR_Type.ISSUE, // harcoded example
-      what: payload.name, // harcoded example
-      why: { type: Scope_CR_Why_Type; explain: string }[], 
-      proposedSolutions: [], // harcoded example
-      ...payload  
+      submitter: currentUser,
+      carNumber: wbsNum.substring(0, 1),
+      projectNumber: wbsNum.substring(2, 3),
+      workPackageNumber: wbsNum.substring(4, 5),
+      type: ChangeRequestType.Issue,
+      what: payload.name,
+      why: { type: 'DESIGN', explain: 'explination' },
+      proposedSolutions: [],
+      ...payload
     };
 
     await mutateAsync(crPayload);
