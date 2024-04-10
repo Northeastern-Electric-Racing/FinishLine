cea047bd122b3cf78a203188c009ae39a474e3bb
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2024-04-10 19:12:18 -0400

    Update ColumnHeader.tsx

--- src/frontend/src/pages/FinancePage/FinanceComponents/ColumnHeader.tsx
+++ src/frontend/src/pages/FinancePage/FinanceComponents/ColumnHeader.tsx
@@ -39,7 +39,7 @@ const ColumnHeader = ({
         align={leftAlign ? 'left' : 'center'}
         sx={{ fontSize: '16px', fontWeight: 600 }}
         sortDirection={orderBy === id ? (isAscendingOrder ? 'asc' : 'desc') : false}
-        style={{ paddingLeft: '42px' }}
+        style={{ paddingLeft: '24px', paddingRight: '0px' }}
       >
         <TableSortLabel
           active={orderBy === id}
