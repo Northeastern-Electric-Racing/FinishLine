c61b4471c877515689348f80e4b5575e940c9204
harish <harivarada1@gmail.com>
2024-04-13 13:09:49 -0400

    #2150 prettier

--- src/frontend/src/pages/HomePage/Home.tsx
+++ src/frontend/src/pages/HomePage/Home.tsx
@@ -12,42 +12,19 @@ import { useCurrentUser, useSingleUserSettings } from '../../hooks/users.hooks';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import ErrorPage from '../ErrorPage';
 import PageLayout from '../../components/PageLayout';
-import { NERButton } from '../../components/NERButton';
-import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
-import { useState } from 'react';
 
-export type ButtonInfo = {
-  count: string;
-  onClick: () => void;
-  disabled?: boolean;
-  dividerTop?: boolean;
-};
 const Home = () => {
-  const [counter, setCounter] = useState(0);
   const user = useCurrentUser();
   const { isLoading, isError, error, data: userSettingsData } = useSingleUserSettings(user.userId);
 
   if (isLoading || !userSettingsData) return <LoadingIndicator />;
   if (isError) return <ErrorPage error={error} message={error.message} />;
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    const updatedCounter = counter + 1
-    setCounter(updatedCounter);
-  };
-
   return (
     <PageLayout title="Home" hidePageTitle>
       <Typography variant="h3" marginLeft="auto" sx={{ marginTop: 2, textAlign: 'center', pt: 3, padding: 0 }}>
         Welcome, {user.firstName}!
       </Typography>
-      <NERButton
-        endIcon={<ArrowDropDown style={{ fontSize: 28 }} />}
-        variant="contained"
-        id="reimbursement-request-actions-dropdown"
-        onClick={handleClick}
-      >
-        {counter}
-      </NERButton>
       <OverdueWorkPackageAlerts />
       <UsefulLinks />
       <UpcomingDeadlines />
