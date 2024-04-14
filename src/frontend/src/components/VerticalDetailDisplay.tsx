8edb851541971bc50fcc688353bb59faaa9839d0
wanke20 <122842952+wanke20@users.noreply.github.com>
2024-04-14 01:10:14 -0400

    finance fixes

--- src/frontend/src/components/VerticalDetailDisplay.tsx
+++ src/frontend/src/components/VerticalDetailDisplay.tsx
@@ -25,21 +25,8 @@ const VerticalDetailDisplay: React.FC<VerticalDetailDisplayProps> = ({ label, co
       borderRadius={'10px'}
       justifyContent={'center'}
       boxShadow={1}
-      sx={{
-        '&::-webkit-scrollbar': {
-          height: '0.55rem' // Adjust the the thickness of the scrollbar
-        },
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: '#EF4345', //FinishLine 'red' color
-          borderRadius: '10px' //make the scrollbar rounded
-        },
-        '&::-webkit-scrollbar-thumb:hover': {
-          backgroundColor: '#b0191a' // Change to a darker shade of red on hover
-        },
-        ...boxStyle
-      }}
     >
-      <Typography textOverflow={'ellipsis'} textAlign={'center'} fontSize={50}>
+      <Typography textOverflow={'ellipsis'} textAlign={'center'} fontSize={300 / content.length}>
         {content}
       </Typography>
       <Typography textAlign={'center'} fontWeight={'bold'} marginBottom={'5px'}>
