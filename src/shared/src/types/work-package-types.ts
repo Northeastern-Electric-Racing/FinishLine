b14db0a739f216cbb6d703621d99ffa064b08561
harish <harivarada1@gmail.com>
2024-04-17 21:43:35 -0400

    #2150 prettier

--- src/shared/src/types/work-package-types.ts
+++ src/shared/src/types/work-package-types.ts
@@ -48,5 +48,5 @@ export interface BlockedByCreateArgs {
   blockedByInfoId: string;
   stage?: WorkPackageStage;
   name: string;
-  workPackageTemplateId: string
+  workPackageTemplateId: string;
 }
