0b3056700e3ac7716b336355a1937f7918e786f5
Bradford Derby <derby.b@northeastern.edu>
2024-04-19 16:25:09 -0400

    #1901 transform date util function

--- src/backend/src/utils/utils.ts
+++ src/backend/src/utils/utils.ts
@@ -23,3 +23,9 @@ export const validateInputs = (req: Request, res: Response, next: Function): Res
   }
   next();
 };
+
+export const transformDate = (date: Date) => {
+  const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : (date.getMonth() + 1).toString();
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate().toString();
+  return `${date.getFullYear().toString()}-${month}-${day}`;
+};
