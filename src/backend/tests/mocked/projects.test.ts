85c4defa8300c5087ea17c8eeb297162ee8b4846
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2024-04-19 23:31:04 -0400

    #2122 - typescript

--- src/backend/tests/mocked/projects.test.ts
+++ src/backend/tests/mocked/projects.test.ts
@@ -903,7 +903,8 @@ describe('Projects', () => {
     });
 
     test('Deleting assembly works', async () => {
-      vi.spyOn(prisma.assembly, 'findUnique').mockResolvedValue({ ...prismaAssembly1, materials: [] });
+      const assembly = { ...prismaAssembly1, materials: [] };
+      vi.spyOn(prisma.assembly, 'findUnique').mockResolvedValue(assembly);
       vi.spyOn(prisma.assembly, 'update').mockResolvedValue({ ...prismaAssembly1, dateDeleted: new Date() });
       const deletedAssembly = await ProjectsService.deleteAssembly('New Assembly', batman);
       expect(deletedAssembly.name).toBe('New Assembly');
