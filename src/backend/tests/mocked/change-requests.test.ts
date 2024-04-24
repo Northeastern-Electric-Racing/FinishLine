65cd610f35a7e52247d5969d69754f66b8397f45
Bradford Derby <derby.b@northeastern.edu>
2024-04-24 16:53:45 -0400

    #1901 prettier and lint

--- src/backend/tests/mocked/change-requests.test.ts
+++ src/backend/tests/mocked/change-requests.test.ts
@@ -120,7 +120,11 @@ describe('Change Requests', () => {
       vi.spyOn(prisma.change_Request, 'findUnique').mockResolvedValue(changeRequest);
       await expect(() =>
         ChangeRequestsService.reviewChangeRequest(batman, crId, reviewNotes, accepted, '1')
-      ).rejects.toThrow(new AccessDeniedException(`User ${batman.userId} is not allowed to review their own change request submitted by User ${changeRequest.submitterId}`);
+      ).rejects.toThrow(
+        new AccessDeniedException(
+          `User ${batman.userId} is not allowed to review their own change request submitted by User ${changeRequest.submitterId}`
+        )
+      );
       expect(prisma.change_Request.findUnique).toHaveBeenCalledTimes(1);
     });
 
