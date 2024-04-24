6df56de34a74b02782a25767ae500d813ba985fc
Aaryan Jain <76594216+Aaryan1203@users.noreply.github.com>
2024-04-23 22:06:47 -0400

    prettier

--- src/frontend/src/utils/work-package.utils.ts
+++ src/frontend/src/utils/work-package.utils.ts
@@ -1,13 +1,16 @@
 import { WbsElement, wbsPipe } from "shared";
 import { WPFormType } from "./form";
 
-export const getTitleFromFormType = (formType: WPFormType, wbsElement: WbsElement): string => {
-    switch (formType) {
-      case WPFormType.CREATEWITHCR:
-        return `Create Change Request - New Work Package - ${wbsElement.name}`;
-      case WPFormType.CREATE:
-        return `New Work Package - ${wbsElement.name}`;
-      default:
-        return `${wbsPipe(wbsElement.wbsNum)} - ${wbsElement.name}`;
-    }
-  };
+export const getTitleFromFormType = (
+  formType: WPFormType,
+  wbsElement: WbsElement
+): string => {
+  switch (formType) {
+    case WPFormType.CREATEWITHCR:
+      return `Create Change Request - New Work Package - ${wbsElement.name}`;
+    case WPFormType.CREATE:
+      return `New Work Package - ${wbsElement.name}`;
+    default:
+      return `${wbsPipe(wbsElement.wbsNum)} - ${wbsElement.name}`;
+  }
+};
