8bebccb755117d81f13a6ea55466e5e0cd3950b1
martin0he <martin.hema.mh@gmail.com>
2024-04-26 00:18:07 -0400

    #2277: fixed comparison

--- src/frontend/src/pages/ChangeRequestDetailPage/CompareFields.tsx
+++ src/frontend/src/pages/ChangeRequestDetailPage/CompareFields.tsx
@@ -35,57 +35,27 @@ const CompareFields: React.FC<CompareFieldsProps> = ({ first, second, isProposed
 
   const compareArrays = (arr1: DescriptionBullet[], arr2: DescriptionBullet[]): boolean => {
     if (arr1.length !== arr2.length) return false;
+
     for (let i = 0; i < arr1.length; i++) {
       const bullet1 = arr1[i];
       const bullet2 = arr2[i];
-      if (bullet1.id !== bullet2.id) {
+      if (bullet1.detail !== bullet2.detail) {
         return false;
       }
     }
     return true;
   };
 
-  const isDescriptionBullet = (input: any): input is DescriptionBullet =>
-    typeof input === 'object' &&
-    input !== null &&
-    typeof input.id === 'string' &&
-    typeof input.detail === 'string' &&
-    typeof input.dateAdded === 'string' &&
-    (typeof input.dateDeleted === 'undefined' || typeof input.dateDeleted === 'string');
-
-  if (first.field === second.field && first.content === second.content) {
+  if (
+    first.field === second.field &&
+    (first.content === second.content ||
+      compareArrays(first.content as DescriptionBullet[], second.content as DescriptionBullet[]))
+  ) {
     return (
       <Typography>
-        {first.field}: {first.content}
+        {first.field}: {renderContent(first.content)}
       </Typography>
     );
-  } else if (isDescriptionBullet(first.content) && isDescriptionBullet(second.content)) {
-    const areEqual = compareArrays(first.content as DescriptionBullet[], second.content as DescriptionBullet[]);
-
-    if (areEqual) {
-      return (
-        <Typography>
-          {first.field}: {renderContent(first.content)}
-        </Typography>
-      );
-    } else {
-      return (
-        <Box sx={{ backgroundColor: '#8a4e4e', borderRadius: '5px', mb: '3px' }}>
-          <Box
-            sx={{ backgroundColor: '#ba5050', borderRadius: '5px', width: 'fit-content' }}
-            component="span"
-            display="inline"
-          >
-            <Typography fontWeight="bold" padding="3px" display="inline">
-              {first.field}:
-            </Typography>
-          </Box>
-          <Box component="span" display="inline">
-            {renderContent(first.content)}
-          </Box>
-        </Box>
-      );
-    }
   } else if (!isProposed) {
     return (
       <Box sx={{ backgroundColor: '#8a4e4e', borderRadius: '5px', mb: '3px' }}>
