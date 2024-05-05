7106a7388ea4a55051be6c658404862d73cae24b
martin0he <martin.hema.mh@gmail.com>
2024-05-05 17:17:22 +0200

    #1254: correct migration done

--- src/backend/src/prisma/migrations/20240207185652_renamed_lead_manager_wbselement/migration.sql
+++ src/backend/src/prisma/migrations/20240207185652_renamed_lead_manager_wbselement/migration.sql
@@ -1,13 +1,11 @@
 -- Rename the columns
-ALTER TABLE "WBS_Element"
-  RENAME COLUMN "projectLeadId" TO "leadId",
-  RENAME COLUMN "projectManagerId" TO "managerId";
+ALTER TABLE "WBS_Element" RENAME COLUMN "projectLeadId" TO "leadId";
+ALTER TABLE "WBS_Element" RENAME COLUMN "projectManagerId" TO "managerId";
 
 -- Drop existing foreign key constraints
 ALTER TABLE "WBS_Element" DROP CONSTRAINT "WBS_Element_projectLeadId_fkey";
 ALTER TABLE "WBS_Element" DROP CONSTRAINT "WBS_Element_projectManagerId_fkey";
 
 -- Add new foreign key constraints
-ALTER TABLE "WBS_Element"
-  ADD CONSTRAINT "WBS_Element_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT "WBS_Element_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "WBS_Element" ADD CONSTRAINT "WBS_Element_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "WBS_Element" ADD CONSTRAINT "WBS_Element_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
