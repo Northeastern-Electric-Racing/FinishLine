1f442f59a818a419451472e8a601138d4bf14165
martin0he <martin.hema.mh@gmail.com>
2024-05-05 17:02:40 +0200

    #1254: changed migration

--- src/backend/src/prisma/migrations/20240207185652_renamed_lead_manager_wbselement/migration.sql
+++ src/backend/src/prisma/migrations/20240207185652_renamed_lead_manager_wbselement/migration.sql
@@ -1,24 +1,13 @@
-/*
-  Warnings:
+-- Rename the columns
+ALTER TABLE "WBS_Element"
+  RENAME COLUMN "projectLeadId" TO "leadId",
+  RENAME COLUMN "projectManagerId" TO "managerId";
 
-  - You are about to drop the column `projectLeadId` on the `WBS_Element` table. All the data in the column will be lost.
-  - You are about to drop the column `projectManagerId` on the `WBS_Element` table. All the data in the column will be lost.
-
-*/
--- DropForeignKey
+-- Drop existing foreign key constraints
 ALTER TABLE "WBS_Element" DROP CONSTRAINT "WBS_Element_projectLeadId_fkey";
-
--- DropForeignKey
 ALTER TABLE "WBS_Element" DROP CONSTRAINT "WBS_Element_projectManagerId_fkey";
 
--- AlterTable
-ALTER TABLE "WBS_Element" DROP COLUMN "projectLeadId",
-DROP COLUMN "projectManagerId",
-ADD COLUMN     "leadId" INTEGER,
-ADD COLUMN     "managerId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "WBS_Element" ADD CONSTRAINT "WBS_Element_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "WBS_Element" ADD CONSTRAINT "WBS_Element_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
+-- Add new foreign key constraints
+ALTER TABLE "WBS_Element"
+  ADD CONSTRAINT "WBS_Element_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT "WBS_Element_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
