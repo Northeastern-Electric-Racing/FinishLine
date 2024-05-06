b8e35b92e903c89daa4da85d8034329e609d871b
ryan howe <ryan.wm.howe@gmail.com>
2024-05-06 17:14:20 -0400

    pretty

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/TaskList/TaskListDataGrid.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/TaskList/TaskListDataGrid.tsx
@@ -243,7 +243,7 @@ const TaskListDataGrid: React.FC<TaskListDataGridProps> = ({
             icon={<DescriptionIcon fontSize="small" />}
             label="notes"
             onClick={() => {
-              console.log("hitting this button");
+              console.log('hitting this button');
               setSelectedTask(params.row.task);
               setModalShow(true);
             }}
