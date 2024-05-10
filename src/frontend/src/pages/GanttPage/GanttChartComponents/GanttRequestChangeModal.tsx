63279b13dc27f88668c13d2e5059ab7931267ad5
aaryan1203 <aaryanjain1203@gmail.com>
2024-05-10 18:47:05 -0400

    #2382 form submits with correct proposed changes

--- src/frontend/src/pages/GanttPage/GanttChartComponents/GanttRequestChangeModal.tsx
+++ src/frontend/src/pages/GanttPage/GanttChartComponents/GanttRequestChangeModal.tsx
@@ -1,12 +1,13 @@
 import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material';
 import { RequestEventChange } from '../../../utils/gantt.utils';
-import { ChangeRequestReason, ChangeRequestType, validateWBS } from 'shared';
-import { useState } from 'react';
+import { ChangeRequestReason, ChangeRequestType, WorkPackage, validateWBS } from 'shared';
+import { useEffect, useState } from 'react';
 import NERModal from '../../../components/NERModal';
 import dayjs from 'dayjs';
 import { useCreateStandardChangeRequest } from '../../../hooks/change-requests.hooks';
 import LoadingIndicator from '../../../components/LoadingIndicator';
 import ErrorPage from '../../ErrorPage';
+import { getSingleWorkPackage } from '../../../apis/work-packages.api';
 
 interface GanttRequestChangeProps {
   change: RequestEventChange;
@@ -16,8 +17,24 @@ export const GanttRequestChange: React.FC<GanttRequestChangeProps> = ({ change }
   const [reasonForChange, setReasonForChange] = useState<ChangeRequestReason>();
   const [explanationForChange, setExplanationForChange] = useState('');
   const [showModal, setShowModal] = useState(true);
-
+  const [workPackage, setWorkPackage] = useState<WorkPackage>();
   const { isLoading, isError, error, mutateAsync } = useCreateStandardChangeRequest();
+
+  useEffect(() => {
+    const fetchWorkPackage = async () => {
+      try {
+        const wp = await getSingleWorkPackage(validateWBS(change.eventId));
+        setWorkPackage(wp.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchWorkPackage();
+  });
+
+  if (!workPackage || !showModal) {
+    return null;
+  }
 
   const handleReasonChange = (event: SelectChangeEvent<ChangeRequestReason>) => {
     setReasonForChange(event.target.value as ChangeRequestReason);
@@ -30,6 +47,10 @@ export const GanttRequestChange: React.FC<GanttRequestChangeProps> = ({ change }
   if (isLoading) return <LoadingIndicator />;
   if (isError) return <ErrorPage message={error?.message} />;
 
+  const changeInTimeline = (startDate: Date, endDate: Date) => {
+    return `${dayjs(startDate).format('MMMM D, YYYY')} - ${dayjs(endDate).format('MMMM D, YYYY')}`;
+  };
+
   const handleSubmit = async () => {
     if (!reasonForChange) {
       return;
@@ -38,7 +59,10 @@ export const GanttRequestChange: React.FC<GanttRequestChangeProps> = ({ change }
     const payload = {
       wbsNum: validateWBS(change.eventId),
       type: ChangeRequestType.Issue,
-      what: `Timeline change request for ${change.name}`,
+      what: `Move timeline From: ${changeInTimeline(change.prevStart, change.prevEnd)} To: ${changeInTimeline(
+        change.newStart,
+        change.newEnd
+      )} - ${dayjs(change.newEnd).format('MMMM D, YYYY')}`,
       why: [
         {
           explain: explanationForChange,
@@ -50,9 +74,9 @@ export const GanttRequestChange: React.FC<GanttRequestChangeProps> = ({ change }
         name: change.name,
         duration: (change.newEnd.getTime() - change.newStart.getTime()) / (1000 * 60 * 60 * 24), // is there a better way to do this
         startDate: change.newStart.toLocaleDateString(),
-        blockedBy: [],
-        expectedActivities: [],
-        deliverables: []
+        blockedBy: workPackage.blockedBy,
+        expectedActivities: workPackage.expectedActivities.map((expectedActivity) => expectedActivity.detail),
+        deliverables: workPackage.deliverables.map((deliverable) => deliverable.detail)
       }
     };
     console.log('payload: ', payload);
@@ -62,10 +86,6 @@ export const GanttRequestChange: React.FC<GanttRequestChangeProps> = ({ change }
     console.log('Explanation:', explanationForChange);
     setShowModal(false);
   };
-
-  if (!showModal) {
-    return null;
-  }
 
   return (
     <NERModal
@@ -78,11 +98,9 @@ export const GanttRequestChange: React.FC<GanttRequestChangeProps> = ({ change }
       <Box sx={{ width: '450px' }}>
         <Typography sx={{ mb: 0.5, fontSize: '1.2em' }}>{change.name} Timeline changed</Typography>
         <Typography sx={{ fontSize: '1em', mb: 0.5 }}>
-          {`From: ${dayjs(change.prevStart).format('MMMM D, YYYY')} - ${dayjs(change.prevEnd).format('MMMM D, YYYY')}`}
+          {`From: ${changeInTimeline(change.prevStart, change.prevEnd)}`}
         </Typography>
-        <Typography sx={{ fontSize: '1em' }}>
-          {`To: ${dayjs(change.newStart).format('MMMM D, YYYY')} - ${dayjs(change.newEnd).format('MMMM D, YYYY')}`}
-        </Typography>
+        <Typography sx={{ fontSize: '1em' }}>{`To: ${changeInTimeline(change.newStart, change.newEnd)}`}</Typography>
       </Box>
       <Box sx={{ padding: '0 15px 0 15px', mt: 2 }}>
         <FormControl fullWidth>
