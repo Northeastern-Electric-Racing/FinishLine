a1d74c2cd286c2c70371958a40365102ed030c1b
Peyton-McKee <mckee.p@northeastern.edu>
2024-05-23 23:33:15 -0400

    Remove Interactive Docker Command

--- system-tests/run.sh
+++ system-tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Must be called via yarn test:e2e from inside the root of Finishline directory
-docker run -it -v $PWD/system-tests:/e2e -w /e2e cypress/included:12.8.1
+docker run -v $PWD/system-tests:/e2e -w /e2e cypress/included:12.8.1
