d203b362a68f084c981dbf1c4b7eb446cf9656b6
Peyton-McKee <mckee.p@northeastern.edu>
2024-05-26 15:24:23 -0400

    Redeploy

--- src/backend/src/utils/datetime.utils.ts
+++ src/backend/src/utils/datetime.utils.ts
@@ -3,6 +3,11 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
+/**
+ * Transforms a date into a string in the format 'YYYY-MM-DD'
+ * @param date The date to transform
+ * @returns the date as a string in the format 'YYYY-MM-DD'
+ */
 export const transformDate = (date: Date) => {
   const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : (date.getMonth() + 1).toString();
   const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate().toString();
