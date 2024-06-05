ba9019b5955b39cbe7ad95dffd5124e7bd069d0d
aaryan1203 <aaryanjain1203@gmail.com>
2024-06-04 21:25:49 -0400

    #2633 fixed id

--- src/backend/tests/test-data/organizations.test-data.ts
+++ src/backend/tests/test-data/organizations.test-data.ts
@@ -2,7 +2,7 @@ import { LinkCreateArgs } from 'shared';
 
 export const testLink1: LinkCreateArgs[] = [
   {
-    linkId: '11',
+    linkId: '-1',
     linkTypeName: 'example link type',
     url: 'https://example.com/link1'
   }
