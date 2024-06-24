879aaf152bfa061559cd990503120eec471d1c51
aarav <saarav.as19@gmail.com>
2024-06-23 23:49:15 -0400

    #2674 Fixed reimbursement request bug

--- src/frontend/src/pages/FinancePage/RefundsSection.tsx
+++ src/frontend/src/pages/FinancePage/RefundsSection.tsx
@@ -16,7 +16,7 @@ import { useState } from 'react';
 import { useAllReimbursements, useCurrentUserReimbursements } from '../../hooks/finance.hooks';
 import ErrorPage from '../ErrorPage';
 import LoadingIndicator from '../../components/LoadingIndicator';
-import { Reimbursement, ReimbursementRequest, isAdmin } from 'shared';
+import { Reimbursement, ReimbursementRequest, ReimbursementStatusType, isAdmin } from 'shared';
 import { useCurrentUser } from '../../hooks/users.hooks';
 import { centsToDollar, datePipe, fullNamePipe } from '../../utils/pipes';
 import NERProgressBar from '../../components/NERProgressBar';
@@ -122,7 +122,7 @@ const Refunds = ({ userReimbursementRequests, allReimbursementRequests }: Refund
 
   const displayedReimbursements = allReimbursements && tabValue === 1 ? allReimbursements : userReimbursements;
   const displayedReimbursementRequests =
-    allReimbursementRequests && tabValue === 1 ? allReimbursementRequests : userReimbursementRequests;
+      (allReimbursementRequests && tabValue === 1 ? allReimbursementRequests : userReimbursementRequests).filter((request: ReimbursementRequest) => request.reimbursementStatuses[0].type !== ReimbursementStatusType.DENIED);
 
   const rows = displayedReimbursements.map(getRefundRowData).sort(getComparator(order, orderBy));
 
