95d6c34f1b4c15b075ea3e05cd29fcbd4f0bf5ef
aaryan1203 <aaryanjain1203@gmail.com>
2024-07-19 21:51:49 -0400

    #2699 created models for FAQs and Milestone

---
+++ src/backend/src/prisma/migrations/20240720015130_created_models_for_faq_and_milestone/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "FrequentlyAskedQuestion" (
+    "faqId" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "userCreatedId" TEXT NOT NULL,
+    "userDeletedId" TEXT,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dateDeleted" TIMESTAMP(3),
+
+    CONSTRAINT "FrequentlyAskedQuestion_pkey" PRIMARY KEY ("faqId")
+);
+
+-- CreateTable
+CREATE TABLE "Milestone" (
+    "milestoneId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "dateOfEvent" TIMESTAMP(3) NOT NULL,
+    "description" TEXT,
+    "userCreatedId" TEXT NOT NULL,
+    "userDeletedId" TEXT,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dateDeleted" TIMESTAMP(3),
+
+    CONSTRAINT "Milestone_pkey" PRIMARY KEY ("milestoneId")
+);
+
+-- AddForeignKey
+ALTER TABLE "FrequentlyAskedQuestion" ADD CONSTRAINT "FrequentlyAskedQuestion_userCreatedId_fkey" FOREIGN KEY ("userCreatedId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FrequentlyAskedQuestion" ADD CONSTRAINT "FrequentlyAskedQuestion_userDeletedId_fkey" FOREIGN KEY ("userDeletedId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Milestone" ADD CONSTRAINT "Milestone_userCreatedId_fkey" FOREIGN KEY ("userCreatedId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Milestone" ADD CONSTRAINT "Milestone_userDeletedId_fkey" FOREIGN KEY ("userDeletedId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
