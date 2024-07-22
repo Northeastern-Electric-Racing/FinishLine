c0335c37436c333ed7ced0fb2b9e7767d68a516d
aaryan1203 <aaryanjain1203@gmail.com>
2024-07-22 15:03:29 -0400

    #2709 fixed import routes

--- src/frontend/src/pages/HomePage/GuestHomePage.tsx
+++ src/frontend/src/pages/HomePage/GuestHomePage.tsx
@@ -8,28 +8,29 @@ interface GuestHomePageProps {
   user: AuthenticatedUser;
 }
 
-const GuestHomePage = ({ user }: GuestHomePageProps) => (
-  <PageLayout title="Home" hidePageTitle>
-    <Typography variant="h3" textAlign="center" sx={{ mt: 2, pt: 3 }}>
-      {user ? `Welcome, ${user.firstName}!` : 'Welcome, Guest!'}
-    </Typography>
-    <Box sx={{ display: 'flex', mt: 4 }}>
-      <Box sx={{ display: 'flex', padding: '50px', gap: 5 }}>
-        <ImageWithButton
-          title="Interested in applying"
-          imageSrc={`/Apply.png`}
-          buttonText="Learn More"
-          onClick={() => console.log('Apply Now clicked')}
-        />
-        <ImageWithButton
-          title="Explore Our Work as a Guest"
-          imageSrc={`/Guest.png`}
-          buttonText="FinishLine"
-          onClick={() => console.log('FinishLine clicked')}
-        />
+const GuestHomePage = ({ user }: GuestHomePageProps) => {
+  return (
+    <PageLayout title="Home" hidePageTitle>
+      <Typography variant="h3" textAlign="center" sx={{ mt: 2, pt: 3 }}>
+        {user ? `Welcome, ${user.firstName}!` : 'Welcome, Guest!'}
+      </Typography>
+      <Box sx={{ display: 'flex', mt: 4 }}>
+        <Box sx={{ display: 'flex', padding: '50px', gap: 5 }}>
+          <ImageWithButton
+            title="Interested in applying"
+            imageSrc={`/Apply.png`}
+            buttonText="Learn More"
+            onClick={() => console.log('Apply Now clicked')}
+          />
+          <ImageWithButton
+            title="Explore Our Work as a Guest"
+            imageSrc={`/Guest.png`}
+            buttonText="FinishLine"
+            onClick={() => console.log('Explore button clicked')}
+          />
+        </Box>
       </Box>
-    </Box>
-  </PageLayout>
-);
-
+    </PageLayout>
+  );
+};
 export default GuestHomePage;
