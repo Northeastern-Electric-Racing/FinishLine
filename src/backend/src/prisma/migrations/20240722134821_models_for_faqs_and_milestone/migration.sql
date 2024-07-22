056d832b4d24be30cdcd16eadd5ab13703f168d1
aaryan1203 <aaryanjain1203@gmail.com>
2024-07-22 09:49:30 -0400

    #2699 linked models to organization

--- src/backend/src/prisma/migrations/20240722134821_models_for_faqs_and_milestone/migration.sql
+++ src/backend/src/prisma/migrations/20240722134821_models_for_faqs_and_milestone/migration.sql
@@ -7,6 +7,7 @@ CREATE TABLE "FrequentlyAskedQuestion" (
     "userDeletedId" TEXT,
     "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "dateDeleted" TIMESTAMP(3),
+    "organizationId" TEXT NOT NULL,
 
     CONSTRAINT "FrequentlyAskedQuestion_pkey" PRIMARY KEY ("frequentlyAskedQuestionId")
 );
@@ -16,11 +17,12 @@ CREATE TABLE "Milestone" (
     "milestoneId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "dateOfEvent" TIMESTAMP(3) NOT NULL,
-    "description" TEXT,
+    "description" TEXT NOT NULL,
     "userCreatedId" TEXT NOT NULL,
     "userDeletedId" TEXT,
     "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "dateDeleted" TIMESTAMP(3),
+    "organizationId" TEXT NOT NULL,
 
     CONSTRAINT "Milestone_pkey" PRIMARY KEY ("milestoneId")
 );
@@ -32,7 +34,13 @@ ALTER TABLE "FrequentlyAskedQuestion" ADD CONSTRAINT "FrequentlyAskedQuestion_us
 ALTER TABLE "FrequentlyAskedQuestion" ADD CONSTRAINT "FrequentlyAskedQuestion_userDeletedId_fkey" FOREIGN KEY ("userDeletedId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "FrequentlyAskedQuestion" ADD CONSTRAINT "FrequentlyAskedQuestion_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("organizationId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Milestone" ADD CONSTRAINT "Milestone_userCreatedId_fkey" FOREIGN KEY ("userCreatedId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Milestone" ADD CONSTRAINT "Milestone_userDeletedId_fkey" FOREIGN KEY ("userDeletedId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Milestone" ADD CONSTRAINT "Milestone_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("organizationId") ON DELETE RESTRICT ON UPDATE CASCADE;
