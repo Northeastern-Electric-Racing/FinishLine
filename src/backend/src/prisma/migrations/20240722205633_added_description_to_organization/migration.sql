e57c357c471b284b1ccb09d425048694339664d4
aaryan1203 <aaryanjain1203@gmail.com>
2024-07-23 11:19:46 -0400

    #2721 combined both migrations into one

--- src/backend/src/prisma/migrations/20240722205633_added_description_to_organization/migration.sql
+++ src/backend/src/prisma/migrations/20240722205633_added_description_to_organization/migration.sql
@@ -2,3 +2,5 @@
 ALTER TABLE "Organization" ADD COLUMN "description" TEXT DEFAULT 'Default description';
 
 ALTER TABLE "Organization" ALTER COLUMN "description" DROP DEFAULT;
+
+ALTER TABLE "Organization" ALTER COLUMN "description" SET NOT NULL;
