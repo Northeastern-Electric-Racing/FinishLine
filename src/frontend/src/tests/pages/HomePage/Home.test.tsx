635346cc6d654d2a2df16d599c184757c89a7709
aaryan1203 <aaryanjain1203@gmail.com>
2024-07-29 11:08:09 -0400

    #2709 fixed home test

--- src/frontend/src/tests/pages/HomePage/Home.test.tsx
+++ src/frontend/src/tests/pages/HomePage/Home.test.tsx
@@ -46,7 +46,7 @@ const renderComponent = () => {
   const RouterWrapper = routerWrapperBuilder({ path: routes.HOME, route: routes.HOME });
   return render(
     <RouterWrapper>
-      <Home onMemberHomePage={true} setOnMemberHomePage={() => {}} />
+      <Home />
     </RouterWrapper>
   );
 };
