0d4a5f580f653651c2c2435e5e8911779646cac4
caiodasilva2005 <caiodasilva05@outlook.com>
2024-09-22 13:58:24 -0400

    #2846-separated into different pages

--- src/frontend/src/pages/HomePage/MemberHomePage.tsx
+++ src/frontend/src/pages/HomePage/MemberHomePage.tsx
@@ -4,21 +4,17 @@
  */
 
 import { Typography } from '@mui/material';
-import OverdueWorkPackageAlerts from './components/OverdueWorkPackageAlerts';
-import UsefulLinks from './components/UsefulLinks';
-import WorkPackagesByTimelineStatus from './components/WorkPackagesByTimelineStatus';
-import UpcomingDeadlines from './components/UpcomingDeadlines';
 import { useSingleUserSettings } from '../../hooks/users.hooks';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import ErrorPage from '../ErrorPage';
 import PageLayout from '../../components/PageLayout';
 import { AuthenticatedUser } from 'shared';
 
-interface MemberHomePageProps {
+interface MemeberHomePageProps {
   user: AuthenticatedUser;
 }
 
-const MemberHomePage = ({ user }: MemberHomePageProps) => {
+const MemberHomePage = ({ user }: MemeberHomePageProps) => {
   const { isLoading, isError, error, data: userSettingsData } = useSingleUserSettings(user.userId);
 
   if (isLoading || !userSettingsData) return <LoadingIndicator />;
@@ -29,10 +25,6 @@ const MemberHomePage = ({ user }: MemberHomePageProps) => {
       <Typography variant="h3" marginLeft="auto" sx={{ marginTop: 2, textAlign: 'center', pt: 3, padding: 0 }}>
         Welcome, {user.firstName}!
       </Typography>
-      <OverdueWorkPackageAlerts />
-      <UsefulLinks />
-      <UpcomingDeadlines />
-      <WorkPackagesByTimelineStatus />
     </PageLayout>
   );
 };
