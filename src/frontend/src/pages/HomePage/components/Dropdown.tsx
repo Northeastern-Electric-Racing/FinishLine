b465e85e8e0e95b5059c35bbd5ce59b699b08a2f
dreifusjack <dreifus.j@northeastern.edu>
2024-09-28 11:10:15 -0400

    Icon change for dropdown

--- src/frontend/src/pages/HomePage/components/Dropdown.tsx
+++ src/frontend/src/pages/HomePage/components/Dropdown.tsx
@@ -1,6 +1,7 @@
 import { Box, Accordion, AccordionSummary, Typography, AccordionDetails } from '@mui/material';
 
-import { KeyboardArrowDown } from '@mui/icons-material';
+import { ChevronRight } from '@mui/icons-material';
+import React, { useState } from 'react';
 
 interface DropdownProps {
   title: string;
@@ -8,17 +9,27 @@ interface DropdownProps {
 }
 
 const Dropdown = ({ title, description }: DropdownProps) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <Box marginBottom={2}>
       <Accordion
+        expanded={expanded}
+        onChange={() => setExpanded(!expanded)}
         sx={{
           backgroundColor: '#ef4244'
         }}
       >
         <AccordionSummary
-          expandIcon={<KeyboardArrowDown />}
           sx={{ flexDirection: 'row-reverse', borderBottomLeftRadius: '100px', borderBottomRightRadius: '100px' }}
         >
+          <ChevronRight
+            sx={{
+              transition: 'transform 0.3s ease',
+              transform: expanded ? 'rotate(90deg)' : 'rotate(0deg)',
+              fontSize: 30
+            }}
+          />
           <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>{title}</Typography>
         </AccordionSummary>
         <AccordionDetails sx={{ backgroundColor: 'white', borderBottomLeftRadius: '4px', borderBottomRightRadius: '4px' }}>
