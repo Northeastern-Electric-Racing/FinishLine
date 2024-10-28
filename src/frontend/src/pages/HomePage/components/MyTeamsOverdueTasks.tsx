4e5909756c007ba81b853fa4c0dc8bc800b30d7d
caiodasilva2005 <caiodasilva05@outlook.com>
2024-10-27 20:25:09 -0400

    #2805-fixed format

--- src/frontend/src/pages/HomePage/components/MyTeamsOverdueTasks.tsx
+++ src/frontend/src/pages/HomePage/components/MyTeamsOverdueTasks.tsx
@@ -18,7 +18,7 @@ interface IndividualTaskCardsProps {
   setOverdueTasks: (overdueTasks: Set<Task>) => void;
 }
 
-const IndividualTaskCard: React.FC<IndividualTaskCardsProps> = ({ member, overdueTasks, setOverdueTasks }) => {
+const IndividualTaskCards: React.FC<IndividualTaskCardsProps> = ({ member, overdueTasks, setOverdueTasks }) => {
   const { data: tasks, isLoading, isError, error } = useUserTasks(member.userId);
 
   if (isLoading || !tasks) return <LoadingIndicator />;
@@ -39,21 +39,13 @@ const MyTeamsOverdueTasks: React.FC<MyTeamsOverdueTasksProps> = ({ user }) => {
   const [overdueTasks, setOverdueTasks] = useState<Set<Task>>(new Set());
   const teamsAsHead = user.teamsAsHead ?? [];
   const teamsAsLead = user.teamsAsLead ?? [];
-  const teamsAsLeadership = teamsAsHead.concat(teamsAsLead);
+  const teamsAsLeadership = [...teamsAsHead, ...teamsAsLead];
   // converting to set for no duplicate members
   const allMembers = new Set(teamsAsLeadership.map((team) => team.members).flat());
-
-  const handleOverdueTasks = (overdueTasks: Set<Task>) => {
-    setOverdueTasks((prevOverdueTasks) => {
-      const allOverdueTasks = new Set([...prevOverdueTasks, ...overdueTasks]);
-      return allOverdueTasks;
-    });
-  };
-
   return (
     <ScrollablePageBlock title={`My Team's Overdue Tasks (${overdueTasks.size})`}>
       {[...allMembers].map((member) => (
-        <IndividualTaskCard member={member} overdueTasks={overdueTasks} setOverdueTasks={handleOverdueTasks} />
+        <IndividualTaskCards member={member} overdueTasks={overdueTasks} setOverdueTasks={setOverdueTasks} />
       ))}
     </ScrollablePageBlock>
   );
