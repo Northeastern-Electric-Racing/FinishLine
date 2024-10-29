c9e1af9141bd63cbe21b21110629f7c54c011a89
caiodasilva2005 <caiodasilva05@outlook.com>
2024-10-29 14:15:16 -0400

    #2805-refactored team's overdue tasks section

--- src/frontend/src/pages/HomePage/components/MyTeamsOverdueTasks.tsx
+++ src/frontend/src/pages/HomePage/components/MyTeamsOverdueTasks.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ScrollablePageBlock from './ScrollablePageBlock';
 import { AuthenticatedUser, Task, User } from 'shared';
-import { useUserTasks } from '../../../hooks/users.hooks';
+import { useManyUserTasks, useUserTasks } from '../../../hooks/users.hooks';
 import LoadingIndicator from '../../../components/LoadingIndicator';
 import ErrorPage from '../../ErrorPage';
 import TeamTaskCard from './TeamTaskCard';
@@ -12,41 +12,25 @@ interface MyTeamsOverdueTasksProps {
   user: AuthenticatedUser;
 }
 
-interface IndividualTaskCardsProps {
-  member: User;
-  overdueTasks: Set<Task>;
-  setOverdueTasks: (overdueTasks: Set<Task>) => void;
-}
-
-const IndividualTaskCards: React.FC<IndividualTaskCardsProps> = ({ member, overdueTasks, setOverdueTasks }) => {
-  const { data: tasks, isLoading, isError, error } = useUserTasks(member.userId);
-
-  if (isLoading || !tasks) return <LoadingIndicator />;
-  if (isError) return <ErrorPage message={error.message} />;
-
-  const individualOverdueTasks = new Set(tasks.filter((task) => daysOverdue(new Date(task.deadline)) > 0));
-  setOverdueTasks(individualOverdueTasks);
-  return (
-    <Stack spacing={2}>
-      {[...individualOverdueTasks.difference(overdueTasks)].map((task, index) => (
-        <TeamTaskCard task={task} taskNumber={index + 1} />
-      ))}
-    </Stack>
-  );
-};
-
 const MyTeamsOverdueTasks: React.FC<MyTeamsOverdueTasksProps> = ({ user }) => {
-  const [overdueTasks, setOverdueTasks] = useState<Set<Task>>(new Set());
   const teamsAsHead = user.teamsAsHead ?? [];
   const teamsAsLead = user.teamsAsLead ?? [];
   const teamsAsLeadership = [...teamsAsHead, ...teamsAsLead];
   // converting to set for no duplicate members
   const allMembers = new Set(teamsAsLeadership.map((team) => team.members).flat());
+  const { data: tasks, isLoading, isError, error } = useManyUserTasks([...allMembers].map((member) => member.userId));
+
+  if (isLoading || !tasks) return <LoadingIndicator />;
+  if (isError) return <ErrorPage message={error.message} />;
+
+  const overdueTasks = new Set(tasks.filter((task) => daysOverdue(new Date(task.deadline)) > 0));
   return (
     <ScrollablePageBlock title={`My Team's Overdue Tasks (${overdueTasks.size})`}>
-      {[...allMembers].map((member) => (
-        <IndividualTaskCards member={member} overdueTasks={overdueTasks} setOverdueTasks={setOverdueTasks} />
-      ))}
+      <Stack spacing={2}>
+        {[...overdueTasks].map((task, index) => (
+          <TeamTaskCard task={task} taskNumber={index + 1} />
+        ))}
+      </Stack>
     </ScrollablePageBlock>
   );
 };
