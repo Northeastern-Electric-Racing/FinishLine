0d8ea76a292e33773c6f77359576a5e21a6002d1
Aryan0102 <aryansumit0102@gmail.com>
2024-11-12 20:55:36 -0500

    t

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -243,7 +243,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Chris Li', color: '#5d42f5' },
     { name: 'Kenneth Aldridge', color: '#E70D0D' },
     { name: 'Nishanth Chinnadurai', color: '#8E6B9F' },
-    { name: 'Aryan Gupta', color: '#5a4094'}
+    { name: 'Aryan Gupta', color: '#5a4094' }
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
