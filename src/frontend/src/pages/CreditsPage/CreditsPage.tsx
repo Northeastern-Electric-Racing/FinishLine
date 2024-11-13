a12cf19aa902a84eb67aa1ab75cb7be2d46efbce
rohan0sharma <sharma.rohan4@northeastern.edu>
2024-11-12 20:46:52 -0500

    #2955 Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -242,7 +242,7 @@ const CreditsPage: React.FC = () => {
     { name: 'Alex Dong', color: '#5d42f5' },
     { name: 'Chris Li', color: '#5d42f5' },
     { name: 'Kenneth Aldridge', color: '#E70D0D' },
-    { name: 'Nishanth Chinnadurai', color: '#8E6B9F' }
+    { name: 'Nishanth Chinnadurai', color: '#8E6B9F' },
     { name: 'Rohan Sharma', color: '#E3BB4D' }
   ];
 
