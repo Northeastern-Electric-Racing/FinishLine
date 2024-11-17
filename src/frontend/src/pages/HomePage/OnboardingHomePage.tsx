631eb641150ae6ae0fbcbc885efd8cf0fae053ec
Wilson Wei <2004wwei@gmail.com>
2024-11-17 14:12:04 -0500

    #2984 Created QuestionsBlock.tsx and added it to OnboardingHomePage.tsx

--- src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
+++ src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
@@ -1,10 +1,11 @@
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import PageLayout from '../../components/PageLayout';
 import { useCurrentOrganization } from '../../hooks/organizations.hooks';
 import { useEffect } from 'react';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import ErrorPage from '../ErrorPage';
 import { useHomePageContext } from '../../app/HomePageContext';
+import QuestionsBlock from './components/QuestionsBlock';
 
 const OnboardingHomePage = () => {
   const { data: organization, isError, error, isLoading } = useCurrentOrganization();
@@ -19,9 +20,27 @@ const OnboardingHomePage = () => {
   if (!organization || isLoading) return <LoadingIndicator />;
   if (isError) return <ErrorPage message={error?.message} />;
 
+  const gridStyle: React.CSSProperties = {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end'
+  };
+
   return (
     <PageLayout title="Home" hidePageTitle>
       <Typography sx={{ fontSize: '3em', mt: 2, ml: 2 }}>Welcome to the Northeastern Electric Racing Team</Typography>
+      <Grid container direction="column" sx={gridStyle}>
+        {/* Placeholder */}
+        <Grid item>
+          <QuestionsBlock />
+        </Grid>
+        {/* Placeholder */}
+        <Grid item>
+          <QuestionsBlock />
+        </Grid>
+        <Grid item>
+          <QuestionsBlock />
+        </Grid>
+      </Grid>
     </PageLayout>
   );
 };
---
+++ src/frontend/src/pages/HomePage/components/QuestionsBlock.tsx
@@ -0,0 +1,30 @@
+/*
+ * This file is part of NER's FinishLine and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ *
+ */
+
+import { ListItem, useTheme } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { Box, Card, List } from '@mui/material';
+import React from 'react';
+
+const QuestionsBlock: React.FC = () => {
+  const theme = useTheme();
+
+  return (
+    <Card sx={{ my: 2, background: theme.palette.background.paper }}>
+      <Box sx={{ margin: 2 }}>
+        <Typography variant="h5">Questions?</Typography>
+        <Typography sx={{ mt: 1, mb: -1 }}>Feel free to contact:</Typography>
+        <List sx={{ listStyleType: 'disc', pl: 2 }}>
+          <ListItem sx={{ display: 'list-item', padding: 0 }}>President - Catherine Kennedy (email)</ListItem>
+          <ListItem sx={{ display: 'list-item', padding: 0 }}>Chief Mechanical Engineer - Sofia Varner (email)</ListItem>
+          <ListItem sx={{ display: 'list-item', padding: 0 }}>Chief Software Engineer - Reid Chandler (email)</ListItem>
+        </List>
+      </Box>
+    </Card>
+  );
+};
+
+export default QuestionsBlock;
