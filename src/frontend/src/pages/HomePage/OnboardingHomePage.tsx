b48765791b2079ba05775eab80442f75400c66e6
aaryan1203 <aaryanjain1203@gmail.com>
2024-11-17 14:20:33 -0500

    removed useful links and questions block

--- src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
+++ src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
@@ -75,30 +75,6 @@ const OnboardingHomePage = () => {
                 <Typography>Onboarding</Typography>
               </Box>
             </Grid>
-            {/* This will be replaced with the 'Useful Links' block*/}
-            <Grid item>
-              <Box
-                sx={{
-                  backgroundColor: 'gray',
-                  height: '20vh',
-                  borderRadius: '10px'
-                }}
-              >
-                <Typography>Useful Links</Typography>
-              </Box>
-            </Grid>
-            {/* This will be replaced with the 'Questions' block*/}
-            <Grid item>
-              <Box
-                sx={{
-                  backgroundColor: 'gray',
-                  height: '20vh',
-                  borderRadius: '10px'
-                }}
-              >
-                <Typography>Questions</Typography>
-              </Box>
-            </Grid>
           </Grid>
         </Grid>
       </Grid>
