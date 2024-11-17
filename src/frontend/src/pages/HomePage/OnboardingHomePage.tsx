00748fe83b906a158251df027e07163f1fb59fde
Wilson Wei <2004wwei@gmail.com>
2024-11-16 22:12:41 -0500

    Updated QuestionsSection.tsx and added it to and updated OnboardingHomePage.tsx

--- src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
+++ src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
@@ -1,10 +1,11 @@
-import { Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import PageLayout from '../../components/PageLayout';
 import { useCurrentOrganization } from '../../hooks/organizations.hooks';
 import { useEffect } from 'react';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import ErrorPage from '../ErrorPage';
 import { useHomePageContext } from '../../app/HomePageContext';
+import QuestionsSection from './components/QuestionsSection';
 
 const OnboardingHomePage = () => {
   const { data: organization, isError, error, isLoading } = useCurrentOrganization();
@@ -19,9 +20,27 @@ const OnboardingHomePage = () => {
   if (!organization || isLoading) return <LoadingIndicator />;
   if (isError) return <ErrorPage message={error?.message} />;
 
+  const gridStyle: React.CSSProperties = {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end'
+  };
+
   return (
     <PageLayout title="Home" hidePageTitle>
       <Typography sx={{ fontSize: '3em', mt: 2, ml: 2 }}>Welcome to the Northeastern Electric Racing Team</Typography>
+      <Grid container xs={6} md={12} direction="column" sx={gridStyle}>
+        <Grid item>
+          {/* Placeholder */}
+          <QuestionsSection></QuestionsSection>
+        </Grid>
+        <Grid item>
+          {/* Placeholder */}
+          <QuestionsSection></QuestionsSection>
+        </Grid>
+        <Grid item>
+          <QuestionsSection></QuestionsSection>
+        </Grid>
+      </Grid>
     </PageLayout>
   );
 };
--- src/frontend/src/pages/HomePage/components/QuestionsSection.tsx
+++ src/frontend/src/pages/HomePage/components/QuestionsSection.tsx
@@ -6,7 +6,7 @@
 
 import { ListItem, useTheme } from '@mui/material';
 import Typography from '@mui/material/Typography';
-import { Box, Card, Grid, List } from '@mui/material';
+import { Box, Card, List } from '@mui/material';
 import React from 'react';
 
 const QuestionsSection: React.FC = () => {
@@ -14,16 +14,14 @@ const QuestionsSection: React.FC = () => {
 
   return (
     <Card sx={{ my: 2, background: theme.palette.background.paper }}>
-      <Box sx={{ mt: 2, ml: 2 }}>
+      <Box sx={{ margin: 2 }}>
         <Typography variant="h5">Questions?</Typography>
-        <Grid item xs={6} md={6}>
-          <Typography sx={{ mt: 1, mb: -1 }}>Feel free to contact:</Typography>
-          <List sx={{ listStyleType: 'disc', pl: 2 }}>
-            <ListItem sx={{ display: 'list-item', padding: 0 }}>President - Catherine Kennedy (email)</ListItem>
-            <ListItem sx={{ display: 'list-item', padding: 0 }}>Chief Mechanical Engineer - Sofia Varner (email)</ListItem>
-            <ListItem sx={{ display: 'list-item', padding: 0 }}>Chief Software Engineer - Reid Chandler (email)</ListItem>
-          </List>
-        </Grid>
+        <Typography sx={{ mt: 1, mb: -1 }}>Feel free to contact:</Typography>
+        <List sx={{ listStyleType: 'disc', pl: 2 }}>
+          <ListItem sx={{ display: 'list-item', padding: 0 }}>President - Catherine Kennedy (email)</ListItem>
+          <ListItem sx={{ display: 'list-item', padding: 0 }}>Chief Mechanical Engineer - Sofia Varner (email)</ListItem>
+          <ListItem sx={{ display: 'list-item', padding: 0 }}>Chief Software Engineer - Reid Chandler (email)</ListItem>
+        </List>
       </Box>
     </Card>
   );
