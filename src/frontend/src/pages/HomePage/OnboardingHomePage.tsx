999bc76693786221db25e18303f8e58f12251248
BewThanin <kongkiatsophon.t@northeastern.edu>
2024-11-17 01:07:56 -0500

    2983 created onboarding block with recommended changes.

--- src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
+++ src/frontend/src/pages/HomePage/OnboardingHomePage.tsx
@@ -6,7 +6,6 @@ import LoadingIndicator from '../../components/LoadingIndicator';
 import ErrorPage from '../ErrorPage';
 import { useHomePageContext } from '../../app/HomePageContext';
 import { Box } from '@mui/system';
-import OnboardingBlock from './OnboardingBlock';
 
 const OnboardingHomePage = () => {
   const { data: organization, isError, error, isLoading } = useCurrentOrganization();
@@ -24,19 +23,68 @@ const OnboardingHomePage = () => {
   return (
     <PageLayout title="Home" hidePageTitle>
       <Typography sx={{ fontSize: '3em', mt: 2, ml: 2 }}>Welcome to the Northeastern Electric Racing Team</Typography>
-      <Grid item xs={12} md={4}>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            alignItems: 'flex-start'
-          }}
-        >
-          <OnboardingBlock
-            title="Onboarding"
-            text="Thank you for applying to Northeastern Electric Racing! After reviewing your application, we are very excited to officially welcome you to our team."
-          />
-        </Box>
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-end'
+        }}
+      >
+        <Grid item xs={12} md={4}>
+          <Box
+            sx={{
+              backgroundColor: '#272727',
+              borderRadius: '8px',
+              padding: 2,
+              width: '400px'
+            }}
+          >
+            <Typography variant="h6" sx={{ marginBottom: 1 }}>
+              Onboarding
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#CCCCCC', lineHeight: 1.5, marginBottom: 5 }}>
+              Thank you for applying to Northeastern Electric Racing! After reviewing your application, we are very excited
+              to officially welcome you to our team.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box
+            sx={{
+              backgroundColor: '#272727',
+              borderRadius: '8px',
+              padding: 2,
+              width: '400px'
+            }}
+          >
+            <Typography variant="h6" sx={{ marginBottom: 1 }}>
+              Useful Links
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#CCCCCC', lineHeight: 1.5 }}>
+              Useful Links placeholder.
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <Box
+            sx={{
+              backgroundColor: '#272727',
+              borderRadius: '8px',
+              padding: 2,
+              width: '400px'
+            }}
+          >
+            <Typography variant="h6" sx={{ marginBottom: 1 }}>
+              Questions
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#CCCCCC', lineHeight: 1.5 }}>
+              Questions placeholder.
+            </Typography>
+          </Box>
+        </Grid>
       </Grid>
     </PageLayout>
   );
