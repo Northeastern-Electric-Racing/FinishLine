a962dcf25e743fbcfda21f703534b894a7777ace
AmberFri <friar.a@northeastern.edu>
2024-11-19 21:11:16 -0500

    #2964 pt6

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -255,7 +255,6 @@ const CreditsPage: React.FC = () => {
     { name: 'Thanin Kongkiatsophon', color: '#15B392' },
     { name: 'Aryan Gupta', color: '#5a4094' },
     { name: 'Lisa Wan', color: '#CCCCFF' },
-    { name: 'Amber Friar', color: '#5BCEFA' },
     { name: 'Aidan Wong', color: '#4284f5' },
   ];
 
