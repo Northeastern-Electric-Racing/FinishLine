016a1c8289af92da0acafc16b09b0d139952b407
gcooper <gcooper740@gmail.com>
2024-11-19 20:32:33 -0500

    #2776: Completed pie chart component and added sample pie chart to stats page

--- src/frontend/src/pages/StatisticsPage/StatisticsPage.tsx
+++ src/frontend/src/pages/StatisticsPage/StatisticsPage.tsx
@@ -5,11 +5,31 @@
 
 import { Box } from '@mui/material';
 import PageLayout from '../../components/PageLayout';
+import PieChart from '../../components/StatsPieChart';
 
 const StatisticsPage: React.FC = () => {
   return (
     <PageLayout title="Statistics">
-      {/* Add your frontend components here to check them */}
+      {
+        <PieChart
+          xAxisData={[
+            'test1',
+            'test2',
+            'test3',
+            'test4',
+            'test5',
+            'test6',
+            'test7',
+            'test8',
+            'test9',
+            'test10',
+            'test11',
+            'test12'
+          ]}
+          yAxisData={[10, 20, 5, 35, 15, 25, 10, 20, 5, 10, 12, 20]}
+          graphTitle="Pie Chart Test"
+        />
+      }
       <Box></Box>
     </PageLayout>
   );
