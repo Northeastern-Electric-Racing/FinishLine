265f61ab0e01fa9018447c9f1c9ebe7b4f301f09
Shrey Agarwal <shreyagarwal7924@gmail.com>
2024-11-19 21:09:48 -0500

    Resolved merge conflicts

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -255,8 +255,8 @@ const CreditsPage: React.FC = () => {
     { name: 'Thanin Kongkiatsophon', color: '#15B392' },
     { name: 'Aryan Gupta', color: '#5a4094' },
     { name: 'Lisa Wan', color: '#CCCCFF' },
-    { name: 'Aidan Wong', color: '#4284f5' },
-    { name: 'Shrey Agarwal', color: '#ADD8E6' }
+    { name: 'Shrey Agarwal', color: '#800080' },
+    { name: 'Aidan Wong', color: '#4284f5' }
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
