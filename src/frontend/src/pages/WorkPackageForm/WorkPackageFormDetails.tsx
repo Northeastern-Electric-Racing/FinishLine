63a46975eb84872a3b0a4e2a4991963a654e4d37
Lisa W <wan.lis@northeastern.edu>
2024-11-19 21:24:28 -0500

    #2684: Changed the Start Date Request for a Work Package Form from YYYY-MM-DD to MM-DD-YYYY

--- src/frontend/src/pages/WorkPackageForm/WorkPackageFormDetails.tsx
+++ src/frontend/src/pages/WorkPackageForm/WorkPackageFormDetails.tsx
@@ -93,7 +93,7 @@ const WorkPackageFormDetails: React.FC<Props> = ({
         </Grid>
         <Grid item xs={12} md={4}>
           <FormControl fullWidth sx={{ overflow: 'hidden' }}>
-            <FormLabel sx={{ whiteSpace: 'noWrap' }}>Start Date (YYYY-MM-DD)</FormLabel>
+            <FormLabel sx={{ whiteSpace: 'noWrap' }}>Start Date (MM-DD-YYYY)</FormLabel>
             <Controller
               name="startDate"
               control={control}
