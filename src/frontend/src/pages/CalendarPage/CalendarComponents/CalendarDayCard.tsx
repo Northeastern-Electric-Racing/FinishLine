edf5611e31fa7f86cfcc18a386d6b513b7a2da3c
morrill-tw <morrill.tw@gmail.com>
2024-11-25 12:44:24 -0500

    #2595: Added a faint border around the current day to the design review calendar

--- src/frontend/src/pages/CalendarPage/CalendarComponents/CalendarDayCard.tsx
+++ src/frontend/src/pages/CalendarPage/CalendarComponents/CalendarDayCard.tsx
@@ -178,6 +178,9 @@ const CalendarDayCard: React.FC<CalendarDayCardProps> = ({ cardDate, events, tea
     );
   };
 
+  const today = new Date().toDateString();
+  const isCurrentDay = cardDate.toDateString() === today;
+
   return (
     <>
       <DesignReviewCreateModal
@@ -188,7 +191,8 @@ const CalendarDayCard: React.FC<CalendarDayCardProps> = ({ cardDate, events, tea
         teamTypes={teamTypes}
         defaultDate={cardDate}
       />
-      <Card sx={{ borderRadius: 2, width: { xs: '95%', md: '80%' }, height: { xs: '10vh', sm: '15vh' } }}>
+      <Card sx={{ borderRadius: 2, width: { xs: '95%', md: '80%' }, height: { xs: '10vh', sm: '15vh' }, 
+      border: isCurrentDay ? '2px solid gray' : 'none', boxShadow: isCurrentDay ? '0 0 10px rgba(255, 255, 255, 0.5)' : 'none', }}>
         <CardContent sx={{ padding: 0 }}>
           <DayCardTitle />
           {events.length < 3 ? (
