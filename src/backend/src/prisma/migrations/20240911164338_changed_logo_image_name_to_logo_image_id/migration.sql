e59e07b1a2186c7f512eae289dfc499be4ccfdf2
caiodasilva2005 <caiodasilva05@outlook.com>
2024-12-12 10:17:32 -0500

    #3060-removed unnneeded migration

--- src/backend/src/prisma/migrations/20240911164338_changed_logo_image_name_to_logo_image_id/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `logoImage` on the `Organization` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Organization" DROP COLUMN "logoImage",
-ADD COLUMN     "logoImageId" TEXT;
