b4774800512cb6530861e0ba68de9765dcb7a23f
caiodasilva2005 <caiodasilva05@outlook.com>
2024-12-14 14:53:45 -0500

    #2817-linting

--- src/frontend/src/pages/HomePage/components/OverdueWorkPackageView.tsx
+++ src/frontend/src/pages/HomePage/components/OverdueWorkPackageView.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Stack, Typography, useTheme } from '@mui/material';
+import { Box, Card, CardContent, Typography, useTheme } from '@mui/material';
 import WorkPackageCard from './WorkPackageCard';
 import { WorkPackage } from 'shared';
 import EmptyPageBlockDisplay from './EmptyPageBlockDisplay';
