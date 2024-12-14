343ae916624b5cb1fc69ea42d0301e3de21c13a7
dreifusjack <dreifus.j@northeastern.edu>
2024-12-14 16:47:07 -0500

    #3001 set onboardingText hook and api

--- src/frontend/src/apis/organizations.api.ts
+++ src/frontend/src/apis/organizations.api.ts
@@ -1,6 +1,7 @@
 import axios from '../utils/axios';
 import { Organization } from 'shared';
 import { apiUrls } from '../utils/urls';
+import { OnboardingTextPayload } from '../hooks/organizations.hooks';
 
 /**
  * Create a design review
@@ -9,5 +10,15 @@ import { apiUrls } from '../utils/urls';
 export const getCurrentOrganization = async () => {
   return axios.get<Organization>(apiUrls.currentOrganization(), {
     transformResponse: (data) => JSON.parse(data)
+  });
+};
+
+/**
+ * Sets onboarding text field
+ * @param payload all info needed to create a design review
+ */
+export const setOnboardingText = (payload: OnboardingTextPayload) => {
+  return axios.post(apiUrls.organizationSetOnboardingText(), {
+    ...payload
   });
 };
--- src/frontend/src/components/OnboardingBlock.tsx
+++ src/frontend/src/components/OnboardingBlock.tsx
@@ -11,6 +11,7 @@ const OnboardingBlock: React.FC<OnboardingBlockProps> = ({ organization, isAdmin
   const theme = useTheme();
   const handleEdit = () => {
     console.log('clicked');
+    // use NER MODEL component and call the hook here
   };
 
   return (
@@ -27,7 +28,9 @@ const OnboardingBlock: React.FC<OnboardingBlockProps> = ({ organization, isAdmin
           <Typography variant="h5" ml={2} pt={2}>
             Onboarding
           </Typography>
-          {isAdmin && <EditIcon sx={{ marginRight: '15px', marginTop: '20px' }} onClick={handleEdit}></EditIcon>}
+          {isAdmin && (
+            <EditIcon sx={{ marginRight: '15px', marginTop: '20px', cursor: 'pointer' }} onClick={handleEdit}></EditIcon>
+          )}
         </Box>
         <Typography sx={{ mt: 1, mb: -1, ml: 2, fontSize: { xs: 16, sm: 16, md: 18 }, marginRight: '15px' }}>
           {organization.onboardingText}
