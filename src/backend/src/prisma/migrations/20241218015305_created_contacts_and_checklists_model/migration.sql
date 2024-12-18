bb8cde5654520ffedb9a99bdda71e5a835890513
aaryan1203 <aaryanjain1203@gmail.com>
2024-12-17 20:53:51 -0500

    #3008 combined migration files

--- src/backend/src/prisma/migrations/20241218015305_created_contacts_and_checklists_model/migration.sql
+++ src/backend/src/prisma/migrations/20241218015305_created_contacts_and_checklists_model/migration.sql
@@ -1,3 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `contacts` on the `Organization` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Organization" DROP COLUMN "contacts";
+
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "completedOnboarding" BOOLEAN NOT NULL DEFAULT true;
 
@@ -20,10 +29,23 @@ CREATE TABLE "Checklist" (
 );
 
 -- CreateTable
+CREATE TABLE "Contact" (
+    "contactId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("contactId")
+);
+
+-- CreateTable
 CREATE TABLE "_checkedChecklists" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_organizationId_userId_title_key" ON "Contact"("organizationId", "userId", "title");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_checkedChecklists_AB_unique" ON "_checkedChecklists"("A", "B");
@@ -48,6 +70,12 @@ ALTER TABLE "Checklist" ADD CONSTRAINT "Checklist_userDeletedId_fkey" FOREIGN KE
 
 -- AddForeignKey
 ALTER TABLE "Checklist" ADD CONSTRAINT "Checklist_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("organizationId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("organizationId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_checkedChecklists" ADD CONSTRAINT "_checkedChecklists_A_fkey" FOREIGN KEY ("A") REFERENCES "Checklist"("checklistId") ON DELETE CASCADE ON UPDATE CASCADE;
