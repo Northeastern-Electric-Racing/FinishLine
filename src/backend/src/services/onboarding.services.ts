fb0f3a25bb16c1678d1096d6d21337af26bf944d
caiodasilva2005 <caiodasilva05@outlook.com>
2024-12-21 18:48:31 -0500

    #2795-removed log

--- src/backend/src/services/onboarding.services.ts
+++ src/backend/src/services/onboarding.services.ts
@@ -4,7 +4,6 @@ import { downloadImageFile } from '../utils/google-integration.utils';
 export default class OnboardingServices {
   static async downloadImage(fileId: string) {
     const fileData = await downloadImageFile(fileId);
-    console.log('FILE DATA RECEIVED');
 
     if (!fileData) throw new NotFoundException('Image File', fileId);
     return fileData;
