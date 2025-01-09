cb75fc4636166db1ac36689d01e09a64dd7ef3c9
aaryan1203 <aaryanjain1203@gmail.com>
2025-01-09 17:39:14 -0500

    #3110 tests and prettier

--- src/backend/tests/unmocked/onboarding.test.ts
+++ src/backend/tests/unmocked/onboarding.test.ts
@@ -88,7 +88,7 @@ describe('Onboarding tests', () => {
     });
   });
 
-  describe('Get Users TeamType Checklists', () => {
+  describe('Get Users Checklists', () => {
     it('Fails if user does not exists', async () => {
       await expect(async () => await OnboardingServices.getUsersChecklists('1', organization)).rejects.toThrow(
         new NotFoundException('User', '1')
@@ -99,18 +99,15 @@ describe('Onboarding tests', () => {
       const batman = await createTestUser(batmanAppAdmin, orgId);
       const teamType1 = await createTestTeamType('teamtype1', organization);
       const teamType2 = await createTestTeamType('teamtype2', organization);
-      const team1 = await createTestTeam('team1', organization, teamType1.teamTypeId, batman.userId);
-      await prisma.user.update({
-        where: { userId: batman.userId },
-        data: { teamsAsMember: { connect: { teamId: team1.teamId } } }
-      });
       const checklist1 = await createTestChecklist(batman, orgId, 'Checklist 1', teamType1.teamTypeId);
       await createTestChecklist(batman, orgId, 'Checklist 2', teamType2.teamTypeId);
-      const checklist3 = await createTestChecklist(batman, orgId, 'Checklist 3', undefined, team1.teamId);
+      await prisma.user.update({
+        where: { userId: batman.userId },
+        data: { onboardingTeamTypes: { connect: { teamTypeId: teamType1.teamTypeId } } }
+      });
       const teamTypeChecklists = await OnboardingServices.getUsersChecklists(batman.userId, organization);
-      expect(teamTypeChecklists.length).toEqual(2);
+      expect(teamTypeChecklists.length).toEqual(1);
       expect(teamTypeChecklists[0].checklistId).toEqual(checklist1.checklistId);
-      expect(teamTypeChecklists[1].checklistId).toEqual(checklist3.checklistId);
     });
   });
 
--- src/frontend/src/apis/team-types.api.ts
+++ src/frontend/src/apis/team-types.api.ts
@@ -22,7 +22,7 @@ export const setTeamType = (id: string, teamTypeId: string) => {
 
 export const toggleOnboardingUser = (teamTypeId: string) => {
   return axios.post<TeamType>(apiUrls.toggleOnboardingUser(teamTypeId));
-}
+};
 
 export const createTeamType = (payload: CreateTeamTypePayload) => {
   return axios.post<TeamType>(apiUrls.teamTypesCreate(), payload);
