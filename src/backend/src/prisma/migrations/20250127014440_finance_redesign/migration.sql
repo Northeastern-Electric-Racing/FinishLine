853b19b3e303b86cc9db381ddc57b5894fae703a
wavehassman <wavehassman@gmail.com>
2025-01-26 20:50:30 -0500

    #3128: optional fields and renaming

--- src/backend/src/prisma/migrations/20250127014440_finance_redesign/migration.sql
+++ src/backend/src/prisma/migrations/20250127014440_finance_redesign/migration.sql
@@ -2,11 +2,11 @@
 ALTER TABLE "Material" ADD COLUMN     "reimbursementRequestId" TEXT NOT NULL DEFAULT '';
 
 -- AlterTable
-ALTER TABLE "Vendor" ADD COLUMN     "addedByUserId" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "discountCode" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "notes" TEXT NOT NULL DEFAULT '',
+ALTER TABLE "Vendor" ADD COLUMN     "addedByUserId" TEXT,
+ADD COLUMN     "discountCode" TEXT,
+ADD COLUMN     "notes" TEXT,
 ADD COLUMN     "password" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "twoFactorContactId" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "twoFactorContactId" TEXT,
 ADD COLUMN     "username" TEXT NOT NULL DEFAULT '';
 
 -- CreateTable
@@ -21,7 +21,7 @@ CREATE TABLE "Sponsor" (
     "sponsorTierId" TEXT NOT NULL,
     "sponsorValue" INTEGER NOT NULL,
     "joinDate" TIMESTAMP(3) NOT NULL,
-    "discountCode" TEXT NOT NULL,
+    "discountCode" TEXT,
     "activeYears" INTEGER[],
     "taxExempt" BOOLEAN NOT NULL,
 
@@ -33,7 +33,7 @@ CREATE TABLE "Sponsor_Task" (
     "sponsorTaskId" TEXT NOT NULL,
     "dueDate" TIMESTAMP(3) NOT NULL,
     "notifyDate" TIMESTAMP(3),
-    "assignToUserId" TEXT,
+    "assigneeUserId" TEXT,
     "notes" TEXT NOT NULL,
     "sponsorId" TEXT NOT NULL,
 
@@ -45,7 +45,7 @@ CREATE TABLE "Sponsor_Tier" (
     "sponsorTierId" TEXT NOT NULL,
     "organizationId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "color" VARCHAR(7) NOT NULL,
+    "colorHexCode" TEXT NOT NULL,
 
     CONSTRAINT "Sponsor_Tier_pkey" PRIMARY KEY ("sponsorTierId")
 );
@@ -66,10 +66,10 @@ CREATE UNIQUE INDEX "_ProjectToReimbursement_Request_AB_unique" ON "_ProjectToRe
 CREATE INDEX "_ProjectToReimbursement_Request_B_index" ON "_ProjectToReimbursement_Request"("B");
 
 -- AddForeignKey
-ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_twoFactorContactId_fkey" FOREIGN KEY ("twoFactorContactId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_twoFactorContactId_fkey" FOREIGN KEY ("twoFactorContactId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_addedByUserId_fkey" FOREIGN KEY ("addedByUserId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_addedByUserId_fkey" FOREIGN KEY ("addedByUserId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Sponsor" ADD CONSTRAINT "Sponsor_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("organizationId") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -78,7 +78,7 @@ ALTER TABLE "Sponsor" ADD CONSTRAINT "Sponsor_organizationId_fkey" FOREIGN KEY (
 ALTER TABLE "Sponsor" ADD CONSTRAINT "Sponsor_sponsorTierId_fkey" FOREIGN KEY ("sponsorTierId") REFERENCES "Sponsor_Tier"("sponsorTierId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Sponsor_Task" ADD CONSTRAINT "Sponsor_Task_assignToUserId_fkey" FOREIGN KEY ("assignToUserId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Sponsor_Task" ADD CONSTRAINT "Sponsor_Task_assigneeUserId_fkey" FOREIGN KEY ("assigneeUserId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Sponsor_Task" ADD CONSTRAINT "Sponsor_Task_sponsorId_fkey" FOREIGN KEY ("sponsorId") REFERENCES "Sponsor"("sponsorId") ON DELETE RESTRICT ON UPDATE CASCADE;
