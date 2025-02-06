5a7360276b40d8958ace921d88782183d794613e
nickyokaitis <yokaitisn@gmail.com>
2025-02-06 14:30:59 -0500

    #3154: reimbursements page changes

--- src/frontend/src/pages/FinancePage/ReimbursmentRequests.tsx
+++ src/frontend/src/pages/FinancePage/ReimbursmentRequests.tsx
@@ -1,7 +1,27 @@
-import { Box } from '@mui/system';
+import { Box, Typography, IconButton } from '@mui/material';
+import { SearchBar } from '../../components/SearchBar';
+import { useState } from 'react';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import ReimbursementRequestTable from './ReimbursementRequestsSection';
 
 const ReimbursementRequests: React.FC = () => {
-  return <Box>Reimbursement Requests</Box>;
+  const [searchText, setSearchText] = useState<string>('');
+  return (
+    <Box sx={{ padding: '5px', display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
+        <Typography variant="h3">Reimbursement Requests</Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ width: '300px' }}>
+          <SearchBar placeholder="Search" searchText={searchText} setSearchText={setSearchText} />
+        </Box>
+        <IconButton color="primary" aria-label="filter">
+          <FilterListIcon />
+          <Typography variant="button">Filters</Typography>
+        </IconButton>
+      </Box>
+      <ReimbursementRequestTable userReimbursementRequests={[]} allReimbursementRequests={[]} />
+    </Box>
+  );
 };
 
 export default ReimbursementRequests;
