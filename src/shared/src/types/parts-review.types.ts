16a019f0a36cc8194d59424dba92001eec3cb55c
Aryan0102 <aryansumit0102@gmail.com>
2025-02-06 21:53:46 -0500

    #3169: Fixed prettier finally

--- src/shared/src/types/parts-review.types.ts
+++ src/shared/src/types/parts-review.types.ts
@@ -1,12 +1,12 @@
-import { Organization } from "./user-types";
+import { Organization } from './user-types';
 
 export interface PartTagReviewType {
-    partTagId: string;
-    name: string;
-    colorHexCode: string;
-    dateCreated: Date;
-    dateDeleted?: Date;
-    parts: String[];
-    organization?: Organization;
-    organizationId: string;
+  partTagId: string;
+  name: string;
+  colorHexCode: string;
+  dateCreated: Date;
+  dateDeleted?: Date;
+  parts: String[];
+  organization?: Organization;
+  organizationId: string;
 }
