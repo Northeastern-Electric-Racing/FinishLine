f4b1226341ee44f2a0323d7107b9016fa0f0af4c
Wilson Wei <2004wwei@gmail.com>
2025-02-07 15:52:56 -0500

    Merge with develop

--- src/frontend/src/pages/FinancePage/ReimbursementRequestDetailPage/ReimbursementRequestDetails.tsx
+++ src/frontend/src/pages/FinancePage/ReimbursementRequestDetailPage/ReimbursementRequestDetails.tsx
@@ -12,8 +12,8 @@ const ReimbursementRequestDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { data: reimbursementRequest, isError, error, isLoading } = useSingleReimbursementRequest(id);
 
-  if (isError) return <ErrorPage error={error} />;
   if (!reimbursementRequest || isLoading) return <LoadingIndicator />;
+  if (isError) return <ErrorPage error={error} />;
 
   return <ReimbursementRequestDetailsView reimbursementRequest={reimbursementRequest} />;
 };
