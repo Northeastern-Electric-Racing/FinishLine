0805ba42788c69a434df254e6b74a0aec3691518
Peyton-McKee <mckee.p@northeastern.edu>
2025-02-12 12:13:28 -0500

    Add Error For Title

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/TaskList/TaskFormModal.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/TaskList/TaskFormModal.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Autocomplete, FormControl, FormLabel, Grid, MenuItem, TextField } from '@mui/material';
+import { Autocomplete, FormControl, FormHelperText, FormLabel, Grid, MenuItem, TextField } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import { Controller, useForm } from 'react-hook-form';
 import { countWords, isGuest, isUnderWordCount, Task, TaskPriority, TeamPreview } from 'shared';
@@ -106,6 +106,7 @@ const TaskFormModal: React.FC<TaskFormModalProps> = ({ task, onSubmit, modalShow
                   />
                 )}
               />
+              <FormHelperText error={!!errors.title}>{errors.title?.message}</FormHelperText>
             </FormControl>
           </Grid>
           <Grid item xs={12} md={5}>
