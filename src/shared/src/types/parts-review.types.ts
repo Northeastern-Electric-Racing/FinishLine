8a2a8c2db60886927c3c58e9c6412220bd3e8a13
Chris Pyle <cpyle@unicode.org>
2025-02-22 18:33:01 -0500

    #3236 prettier changes

--- src/shared/src/types/parts-review.types.ts
+++ src/shared/src/types/parts-review.types.ts
@@ -1,7 +1,7 @@
 export interface PartReviewCommonMistake {
-    id: string;
-    title: string;
-    description: string;
-    starred: Boolean;
-    userCreatedId: string;
+  id: string;
+  title: string;
+  description: string;
+  starred: Boolean;
+  userCreatedId: string;
 }
