520302979d5ef16355fefb5f17be2cc659cfa325
jessegarcia1 <garcia.jesse1@northeastern.edu>
2025-02-21 21:07:46 -0500

    edited finance types

--- src/shared/src/types/finance-types.ts
+++ src/shared/src/types/finance-types.ts
@@ -1,17 +1,11 @@
-import { Organization } from './user-types';
 import { User } from './user-types';
 
 export interface Sponsor {
   sponsorId: string;
   name: string;
-  organizationId: string;
-  organization: Organization;
-  dateCreated: Date;
-  dateDeleted?: Date;
   activeStatus: boolean;
   vendorContact: string;
   tier: SponsorTier;
-  sponsorTierId: string;
   sponsorValue: number;
   joinDate: Date;
   discountCode?: string;
@@ -22,8 +16,6 @@ export interface Sponsor {
 
 export interface SponsorTier {
   sponsorTierId: string;
-  organization: Organization;
-  organizationId: string;
   name: string;
   colorHexCode: string;
   sponsors: Sponsor[];
@@ -34,8 +26,6 @@ export interface Sponsor_Task {
   dueDate: Date;
   notifyDate?: Date;
   assignee?: User;
-  assigneeUserId?: string;
   notes: string;
   sponsor: Sponsor;
-  sponsorId: string;
 }
