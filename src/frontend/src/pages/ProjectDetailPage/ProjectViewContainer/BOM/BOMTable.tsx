ee07644672a71ec7c1c9573b575e645f020360f9
Eric Yao <ericyaohan@yahoo.com>
2025-02-23 18:42:16 -0500

    changed so that can be dropped when dragged on a material

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOM/BOMTable.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOM/BOMTable.tsx
@@ -138,7 +138,6 @@ const BOMTable: React.FC<BOMTableProps> = ({ setHideColumn, assignMaterial, colu
               handleDragStart(materialId);
             },
             onDrop: (event: React.DragEvent) => {
-              if (event.currentTarget.className.includes('super-app-theme--material')) return;
               const rowIndex = parseInt(event.currentTarget.getAttribute('data-rowindex') || '0');
               const materials = rows.concat(materialsWithAssemblies.filter(isAssemblyOpen));
               const { assemblyId } = materials[rowIndex];
