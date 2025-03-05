6063f5b91e63274b3ca5e7b7c624b2aca39ba1ac
wavehassman <wavehassman@gmail.com>
2025-03-05 17:15:45 -0500

    revert changes

--- src/backend/src/prisma-query-args/index-code.query-args.ts
+++ src/backend/src/prisma-query-args/index-code.query-args.ts
@@ -5,11 +5,6 @@ export type IndexCodeQueryArgs = ReturnType<typeof getIndexCodeQueryArgs>;
 export const getIndexCodeQueryArgs = () =>
   Prisma.validator<Prisma.Index_CodeDefaultArgs>()({
     include: {
-      userCreated: {
-        include: {
-          organizations: true,
-          roles: true
-        }
-      }
+      userCreated: true
     }
   });
