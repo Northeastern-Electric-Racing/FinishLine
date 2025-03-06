fe352894128c4ac3c7662f6e2c06030c3a4a5222
Chris Pyle <cpyle@unicode.org>
2025-03-06 18:37:26 -0500

    #3284 part models shared types, query args, and transformers

--- src/backend/src/transformers/part-review.transformer.ts
+++ src/backend/src/transformers/part-review.transformer.ts
@@ -1,6 +1,93 @@
-import { PartReviewCommonMistake } from '@prisma/client';
-import { PartReviewCommonMistake as SharedCommonMistake } from 'shared';
+import { PartReviewCommonMistake, Prisma } from '@prisma/client';
+import {
+  PartReviewCommonMistake as SharedCommonMistake,
+  Part as SharedPart,
+  Review_Status,
+  PartSubmission as sharedSubmission,
+  PartReviewRequest as sharedReviewRequest,
+  PartReview,
+  PartPreview
+} from 'shared';
+import {
+  PartQueryArgs,
+  PartReviewQueryArgs,
+  PartReviewRequestQueryArgs,
+  PartSubmissionQueryArgs
+} from '../prisma-query-args/part-review.query-args';
+import { userTransformer } from './user.transformer';
 
 export const partsReviewCommonMistakeTransformer = (commonMistake: PartReviewCommonMistake): SharedCommonMistake => {
   return commonMistake;
+};
+
+export const partTransformer = (part: Prisma.PartGetPayload<PartQueryArgs>): SharedPart => {
+  return {
+    ...partPreviewTransformer(part),
+    submissions: part.submissions.map(submissionTransformer)
+  };
+};
+
+export const partPreviewTransformer = (part: Prisma.PartGetPayload<PartQueryArgs>): PartPreview => {
+  return {
+    partId: part.partId,
+    index: part.index,
+    commonName: part.commonName,
+    description: part.description ?? undefined,
+    previewImageLink: part.previewImageLink ?? undefined,
+    status: part.status as Review_Status,
+    tags: part.tags,
+    projectId: part.projectId,
+    assignees: part.assignees.map(userTransformer),
+    reviewers: part.submissions
+      .map((submission) =>
+        submission.reviewRequests
+          .map((reviewReq) => reviewReq.reviewerRequested)
+          .flat()
+          .concat(submission.reviews.map((review) => review.userCreated).flat())
+      )
+      .flat()
+      .map(userTransformer),
+    userCreated: userTransformer(part.userCreated),
+    createdAt: part.createdAt
+  };
+};
+
+export const submissionTransformer = (
+  submission: Prisma.PartSubmissionGetPayload<PartSubmissionQueryArgs>
+): sharedSubmission => {
+  return {
+    partSubmissionId: submission.partSubmissionId,
+    fileIds: submission.fileIds,
+    name: submission.name,
+    notes: submission.notes ?? undefined,
+    partId: submission.partId,
+    userCreated: userTransformer(submission.userCreated),
+    reviewRequests: submission.reviewRequests.map(partReviewRequestTransformer),
+    reviews: submission.reviews.map(partReviewTransformer)
+  };
+};
+
+export const partReviewRequestTransformer = (
+  reviewRequest: Prisma.PartReviewRequestGetPayload<PartReviewRequestQueryArgs>
+): sharedReviewRequest => {
+  return {
+    partReviewRequestId: reviewRequest.partReviewRequestId,
+    submissionId: reviewRequest.submissionId,
+    requester: userTransformer(reviewRequest.requester),
+    reviewerRequested: userTransformer(reviewRequest.reviewerRequested),
+    createdAt: reviewRequest.createdAt
+  };
+};
+
+export const partReviewTransformer = (review: Prisma.PartReviewGetPayload<PartReviewQueryArgs>): PartReview => {
+  return {
+    partReviewId: review.partReviewId,
+    fileIds: review.fileIds,
+    notes: review.notes ?? undefined,
+    submissionId: review.submissionId,
+    popUps: review.popUps,
+    completedAt: review.completedAt ?? undefined,
+    createdAt: review.createdAt,
+    userCreated: userTransformer(review.userCreated)
+  };
 };
--- src/shared/src/types/part-review.types.ts
+++ src/shared/src/types/part-review.types.ts
@@ -1,3 +1,71 @@
+import { User } from './user-types';
+
+export enum Review_Status {
+  IN_PROGRESS = 'IN_PROGRESS',
+  READY_FOR_REVIEW = 'READY_FOR_REVIEW',
+  IN_REVIEW = 'IN_REVIEW',
+  REVIEWED = 'REVIEWED',
+  APPROVED = 'APPROVED'
+}
+
+export interface PartPreview {
+  partId: string;
+  index: number;
+  commonName: string;
+  description?: string;
+  previewImageLink?: string;
+  status: Review_Status;
+  tags: PartTag[];
+  projectId: string;
+  assignees: User[];
+  reviewers: User[];
+  createdAt: Date;
+  userCreated: User;
+}
+
+export interface Part extends PartPreview {
+  submissions: PartSubmission[];
+}
+
+export interface PartSubmission {
+  partSubmissionId: string;
+  fileIds: string[];
+  name: string;
+  notes?: string;
+  partId: string;
+  userCreated: User;
+  reviewRequests: PartReviewRequest[];
+  reviews: PartReview[];
+}
+
+export interface PartReviewRequest {
+  partReviewRequestId: string;
+  submissionId: string;
+  requester: User;
+  reviewerRequested: User;
+  createdAt: Date;
+}
+
+export interface PartReview {
+  partReviewId: string;
+  fileIds: string[];
+  notes?: string;
+  submissionId: string;
+  popUps: Part_Review_Popup[];
+  completedAt?: Date;
+  createdAt: Date;
+  userCreated: User;
+}
+
+export interface Part_Review_Popup {
+  partReviewPopupId: string;
+  xCoord: number;
+  yCoord: number;
+  title: string;
+  description: string;
+  reviewId: string;
+}
+
 export interface PartReviewCommonMistake {
   partReviewCommonMistakeId: string;
   title: string;
