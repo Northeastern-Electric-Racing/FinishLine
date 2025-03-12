e67c9cbb91cf21e59112c9f059ace8898d895508
Kaung Mo <126790548+mokaung@users.noreply.github.com>
2025-03-12 19:28:41 -0400

    #3165 - fixing small changes

--- src/backend/src/prisma/seed-data/parts.seed.ts
+++ src/backend/src/prisma/seed-data/parts.seed.ts
@@ -1,4 +1,5 @@
-/* This file is part of NER's FinishLine and licensed under GNU AGPLv3.
+/* 
+ * This file is part of NER's FinishLine and licensed under GNU AGPLv3.
  * See the LICENSE file in the repository root folder for details.
  */
 
