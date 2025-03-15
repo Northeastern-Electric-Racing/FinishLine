2452cca4d57d3e91134264b0537d8ff8f920da20
dreifusjack <dreifus.j@northeastern.edu>
2025-03-15 19:54:36 -0400

    #3279 simplified using ... operator

--- src/backend/src/transformers/finance.transformer.ts
+++ src/backend/src/transformers/finance.transformer.ts
@@ -5,26 +5,17 @@ import { userTransformer } from './user.transformer';
 
 export const sponsorTransformer = (sponsor: Prisma.SponsorGetPayload<SponsorQueryArgs>): Sponsor => {
   return {
-    sponsorId: sponsor.sponsorId,
-    name: sponsor.name,
-    activeStatus: sponsor.activeStatus,
-    vendorContact: sponsor.vendorContact,
+    ...sponsor,
     tierId: sponsor.sponsorTierId,
-    sponsorValue: sponsor.sponsorValue,
-    joinDate: sponsor.joinDate,
     discountCode: sponsor.discountCode ?? undefined,
-    activeYears: sponsor.activeYears,
-    taxExempt: sponsor.taxExempt,
     sponsorTasks: sponsor.sponsorTasks.map(sponsorTaskTranformer)
   };
 };
 
 export const sponsorTaskTranformer = (sponsorTask: Prisma.Sponsor_TaskGetPayload<SponsorTaskQueryArgs>): SponsorTask => {
   return {
-    sponsorTaskId: sponsorTask.sponsorTaskId,
-    dueDate: sponsorTask.dueDate,
+    ...sponsorTask,
     notifyDate: sponsorTask.notifyDate ?? undefined,
-    assignee: sponsorTask.assignee ? userTransformer(sponsorTask.assignee) : undefined,
-    notes: sponsorTask.notes
+    assignee: sponsorTask.assignee ? userTransformer(sponsorTask.assignee) : undefined
   };
 };
