da832f57f3844fb8c84d3341fec1e058840008ee
alexpark0 <park.ale@northeastern.edu>
2025-03-25 17:11:46 -0400

    #3316: create review controller

--- src/backend/src/controllers/part-review.controllers.ts
+++ src/backend/src/controllers/part-review.controllers.ts
@@ -159,4 +159,14 @@ export default class PartReviewController {
       next(error);
     }
   }
+
+  static async createPartReview(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { submission } = req.body;
+      const review = await PartReviewService.createPartReview(submission, req.currentUser, req.organization.organizationId);
+      res.status(200).json(review);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
 }
