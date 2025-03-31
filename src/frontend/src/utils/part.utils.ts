54a7c1e2eddc7aeacb70fcc730f3aee76e92f183
Ciel Bellerose <kcbellerose@icloud.com>
2025-03-30 20:01:41 -0400

    #3339 util part creation and submission history

--- src/frontend/src/utils/part.utils.ts
+++ src/frontend/src/utils/part.utils.ts
@@ -1,30 +1,39 @@
-import { Part, Review_Status } from 'shared';
+import { Part, PartReviewRequest, PartSubmission, Review_Status, User } from 'shared';
 
-// return tuples of [date, data] for each of the
-// different actions that need to be processed
 type HistoryEntry = [Date, string];
 
 /* [01/01/24] - PROJ_PartName_0000-00A was created. */
-// [Date] - [PartPreview.commonName] was created
-export const getPartCreationHistory = (part: Part): HistoryEntry[] => {
-  return [[new Date(part.createdAt), `${part.commonName} was created`]];
+export const getPartCreationHistory = (createdAt: Date, name: String): HistoryEntry[] => {
+  return [[new Date(createdAt), `${name} was created`]];
 };
 
 /* [01/01/24] - Joseph Aoun uploaded Submission #1 for PROJ_PartName_0000-00A. */
-// [Date] - [User] uploaded [PartSubmission.name] for [Part Project Name]
 /* [01/01/24] - Joseph Aoun uploaded Submission #2 */
-export const getSubmissionHistory = (part: Part): HistoryEntry[] => {
-  return; // all uploads as tuples
+export const getSubmissionHistory = (user: User, submissions: PartSubmission[], name: String): HistoryEntry[] => {
+  if (submissions.length === 0) return [];
+
+  const firstSubmission = [...submissions]
+    .sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+    .at(0);
+
+  return submissions.map((sub) => {
+    const isFirstSubmission = firstSubmission && sub.createdAt === firstSubmission.createdAt;
+    const message = isFirstSubmission ? `${user} uploaded ${sub.name} for ${name}` : `${user} uploaded ${sub.name}`;
+
+    return [new Date(sub.createdAt), message];
+  });
 };
 
 /* [01/01/24] - Joseph Aoun requested a review from Jacob Brown and George Miller. */
 // [Date] - [User] requested a review from [PartReviewRequest.reviewerRequested] and [PartReviewRequest.reviewerRequested]
 // Two different PartReviewRequests on the same date from the same User should be combined
-
 /* [01/03/24] - Joseph Aoun re-requested a review from George Miller. */
-// [Date] -
-export const getReviewRequestHistory = (part: Part): HistoryEntry[] => {
-  return;
+export const getReviewRequestHistory = (user: User, reviewRequests: PartReviewRequest[]): HistoryEntry[] => {
+  if(reviewRequests.length === 0) return [];
+
+  reviewRequests.forEach((req) => {});
+
+  return [[new Date(reviewRequests[0].createdAt), `?`]]; // repeat for all review requests and combine if on the same date
 };
 
 /* [01/01/24] - George Miller began reviewing Submission #1 */
@@ -32,25 +41,31 @@ export const getReviewRequestHistory = (part: Part): HistoryEntry[] => {
 
 /* [01/03/24] - George Miller reviewed Submission #2(added comments) */
 // [Date] -  [User/PartReviewRequest.reviewerRequested] reviewed [PartSubmission.name](added comments)
-// Check for 'added commends' by checking PartReview.notes?
 
 /* [01/01/24] - George Miller reviewed Submission #1 (in Submission #1 Review)*/
 // [Date] - [User/PartReviewRequest.reviewerRequested] reviewed [PartSubmission.name] (in [PartReview.submissionId?])
-export const getReviewHistory = (part: Part): HistoryEntry[] => {
-  Review_Status;
-  return;
-};
 
 /* [01/05/24] - George Miller approved Submission #3 */
 // [Date] - [User/PartReviewRequest.reviewerRequested] approved [PartSubmission.name]
 // Check for PartPreview.status.APPROVED
 
+export const getReviewHistory = (reviewer: User, submissions: PartSubmission[]): HistoryEntry[] => {
+  if (submissions.length === 0) return [];
+
+  submissions.forEach(sub => {
+    // figure out the status using submission id? NOT PartPreview.status
+    // using completed at (check if approved),
+    // using notes? ("added comments")
+  });
+  return [[new Date(submissions[0].createdAt), `?`]]; // repeat for all submissions
+};
+
 export const completePartHistory = (part: Part): HistoryEntry[] => {
   const history = [
-    ...getPartCreationHistory(part),
-    ...getSubmissionHistory(part),
-    ...getReviewRequestHistory(part),
-    ...getReviewHistory(part)
+    ...getPartCreationHistory(part.createdAt, part.commonName),
+    ...getSubmissionHistory(part.userCreated, part.submissions, part.commonName),
+    ...getReviewRequestHistory(part.userCreated, part.reviewRequests),
+    ...getReviewHistory(part.userCreated, part.submissions)
   ];
   return history.sort((a, b) => a[0].getTime() - b[0].getTime());
 };
