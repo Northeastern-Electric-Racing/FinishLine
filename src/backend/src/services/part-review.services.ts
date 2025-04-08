cbbfb5de3e5045492fc25d0a7fea2a31e63d0283
nate <41275256+nateci@users.noreply.github.com>
2025-04-08 14:59:50 -0400

    Update part-review.services.ts

--- src/backend/src/services/part-review.services.ts
+++ src/backend/src/services/part-review.services.ts
@@ -23,9 +23,18 @@ import {
 } from '../utils/errors.utils';
 import prisma from '../prisma/prisma';
 import { getFaqQueryArgs } from '../prisma-query-args/faq.query-args';
-import { getPartQueryArgs, getPartReviewQueryArgs, getPartReviewRequestQueryArgs } from '../prisma-query-args/part-review.query-args';
+import {
+  getPartQueryArgs,
+  getPartReviewQueryArgs,
+  getPartReviewRequestQueryArgs
+} from '../prisma-query-args/part-review.query-args';
 import { faqTransformer } from '../transformers/faq.transformer';
-import { partReviewRequestTransformer, partsReviewCommonMistakeTransformer, partTransformer, partPreviewTransformer } from '../transformers/part-review.transformer';
+import {
+  partReviewRequestTransformer,
+  partsReviewCommonMistakeTransformer,
+  partTransformer,
+  partPreviewTransformer
+} from '../transformers/part-review.transformer';
 import { isUserPartOfTeams } from '../utils/teams.utils';
 import { uploadFile } from '../utils/google-integration.utils';
 import ProjectsService from './projects.services';
