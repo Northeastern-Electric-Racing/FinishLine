ce554053c0fbab82b66b7c99a1030314e89d6354
Chris DiCono <dicono.c@northeastern.edu>
2025-04-09 02:39:30 -0400

    #3341 adjusted title size

--- src/frontend/src/pages/FinancePage/CompaniesAndSponsors.tsx
+++ src/frontend/src/pages/FinancePage/CompaniesAndSponsors.tsx
@@ -5,28 +5,34 @@ import { useState } from 'react';
 import PageLayout from '../../components/PageLayout';
 import NERTabs from '../../components/Tabs';
 import { routes } from '../../utils/routes';
+import { Typography } from '@mui/material';
 
 const CompaniesAndSponsors: React.FC = () => {
   const [tabIndex, setTabIndex] = useState<number>(0);
 
   return (
-    <PageLayout
-      title="Companies and Sponsoring Vendors"
-      tabs={
-        <NERTabs
-          setTab={setTabIndex}
-          tabsLabels={[
-            { tabUrlValue: 'companies', tabName: 'Companies' },
-            { tabUrlValue: 'sponsoring-vendors', tabName: 'Sponsoring Vendors' }
-          ]}
-          baseUrl={routes.COMPANIES_SPONSORS}
-          defaultTab="companies"
-          id="companies-and-sponsors-tabs"
-        />
-      }
-    >
-      {tabIndex === 0 ? <Companies /> : <SponsoringVendors />}
-    </PageLayout>
+    <Box>
+      <Typography variant="h3" sx={{ mt: 1, fontSize: { xs: '1.4rem', sm: '1.75rem', md: '3rem' } }}>
+        Companies and Sponsoring Vendors
+      </Typography>
+      <PageLayout
+        title=" "
+        tabs={
+          <NERTabs
+            setTab={setTabIndex}
+            tabsLabels={[
+              { tabUrlValue: 'companies', tabName: 'Companies' },
+              { tabUrlValue: 'sponsoring-vendors', tabName: 'Sponsoring Vendors' }
+            ]}
+            baseUrl={routes.COMPANIES_SPONSORS}
+            defaultTab="companies"
+            id="companies-and-sponsors-tabs"
+          />
+        }
+      >
+        {tabIndex === 0 ? <Companies /> : <SponsoringVendors />}
+      </PageLayout>
+    </Box>
   );
 };
 
--- src/frontend/src/pages/FinancePage/FinanceDashboard.tsx
+++ src/frontend/src/pages/FinancePage/FinanceDashboard.tsx
@@ -9,44 +9,50 @@ import { useState } from 'react';
 import PageLayout from '../../components/PageLayout';
 import NERTabs from '../../components/Tabs';
 import { routes } from '../../utils/routes';
+import { Typography } from '@mui/material';
 
 const FinanceDashboard: React.FC = () => {
   const [tabIndex, setTabIndex] = useState<number>(0);
 
   return (
-    <PageLayout
-      title="Finance Budget Overview"
-      tabs={
-        <NERTabs
-          setTab={setTabIndex}
-          tabsLabels={[
-            { tabUrlValue: 'all', tabName: 'all' },
-            { tabUrlValue: 'electrical', tabName: 'Electrical' },
-            { tabUrlValue: 'mechanical', tabName: 'Mechanical' },
-            { tabUrlValue: 'software', tabName: 'Software' },
-            { tabUrlValue: 'business', tabName: 'Business' },
-            { tabUrlValue: 'categories', tabName: 'Categories' }
-          ]}
-          baseUrl={routes.FINANCE_DASHBOARD}
-          defaultTab="electrical"
-          id="finance-dashboard-tabs"
-        />
-      }
-    >
-      {tabIndex === 0 ? (
-        <All />
-      ) : tabIndex === 1 ? (
-        <Electrical />
-      ) : tabIndex === 2 ? (
-        <Mechanical />
-      ) : tabIndex === 3 ? (
-        <Software />
-      ) : tabIndex === 4 ? (
-        <Business />
-      ) : (
-        <Categories />
-      )}
-    </PageLayout>
+    <Box>
+      <Typography variant="h3" sx={{ mt: 1, fontSize: { xs: '1.4rem', sm: '1.75rem', md: '3rem' } }}>
+        Finance Budget Overview
+      </Typography>
+      <PageLayout
+        title=" "
+        tabs={
+          <NERTabs
+            setTab={setTabIndex}
+            tabsLabels={[
+              { tabUrlValue: 'all', tabName: 'all' },
+              { tabUrlValue: 'electrical', tabName: 'Electrical' },
+              { tabUrlValue: 'mechanical', tabName: 'Mechanical' },
+              { tabUrlValue: 'software', tabName: 'Software' },
+              { tabUrlValue: 'business', tabName: 'Business' },
+              { tabUrlValue: 'categories', tabName: 'Categories' }
+            ]}
+            baseUrl={routes.FINANCE_DASHBOARD}
+            defaultTab="all"
+            id="finance-dashboard-tabs"
+          />
+        }
+      >
+        {tabIndex === 0 ? (
+          <All />
+        ) : tabIndex === 1 ? (
+          <Electrical />
+        ) : tabIndex === 2 ? (
+          <Mechanical />
+        ) : tabIndex === 3 ? (
+          <Software />
+        ) : tabIndex === 4 ? (
+          <Business />
+        ) : (
+          <Categories />
+        )}
+      </PageLayout>
+    </Box>
   );
 };
 
