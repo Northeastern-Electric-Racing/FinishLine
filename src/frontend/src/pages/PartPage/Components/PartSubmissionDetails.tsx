b75cb0a64fcb7dd6a4ff5ac2f6d546854c620eb7
gaurirajesh <rajesh.ga@northeastern.edu>
2025-04-09 19:08:05 -0400

    #3338: fixed alignment of component

--- src/frontend/src/pages/PartPage/Components/PartSubmissionDetails.tsx
+++ src/frontend/src/pages/PartPage/Components/PartSubmissionDetails.tsx
@@ -8,19 +8,19 @@ interface PartSubmissionProps {
 
 const PartSubmissionDetails = ({ submission }: PartSubmissionProps) => {
   return (
-    <Stack spacing={'4%'} alignItems="center" width="100%">
+    <Stack spacing={'4%'} alignItems="left" width="100%">
       <Typography sx={{ fontWeight: 'normal' }} variant="h5">
-        Details for ${submission.name}:
+        Details for {submission.name}:
       </Typography>
 
       <Typography variant="body1">
-        Uploader: ${submission.userCreated.firstName} {submission.userCreated.lastName}
+        Uploader: {submission.userCreated.firstName} {submission.userCreated.lastName}
       </Typography>
 
       <Typography variant="body1">Uploader Notes: {submission.notes || 'There are no notes.'}</Typography>
 
       <Typography variant="body1">
-        Reviewer Notes:
+        Reviewer Notes:{' '}
         {submission.reviews.length !== 0
           ? submission.reviews.map((review) => review.notes).join('\n')
           : 'There are no notes.'}
