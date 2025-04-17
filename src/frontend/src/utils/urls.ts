309823f6d6c5821f8cbad0e611e79fc01a73b171
Griffin Cooper <72273901+gcooper407@users.noreply.github.com>
2025-04-16 22:06:24 -0400

    #3369: NOTE added fixes to bugs causing issues on feature branch (endpoint naming scheme, transformer logic mishandling)

--- src/frontend/src/utils/urls.ts
+++ src/frontend/src/utils/urls.ts
@@ -44,7 +44,7 @@ const projectsEditLinkTypes = (linkTypeName: string) => `${projects()}/link-type
 /**************** Part Review Endpoints ********************/
 const parts = () => `${API_URL}/parts`;
 const partsByProject = (wbsNum: string) => `${parts()}/byProject/${wbsNum}`;
-const partByIndex = (wbsNum: string, partIndex: number) => `${parts()}/${wbsNum}/${partIndex}`;
+const partByIndex = (wbsNum: string, partIndex: number) => `${parts()}/bypart/${wbsNum}/${partIndex}`;
 const partsCreate = () => `${parts()}/create`;
 const partsUploadPreviewImage = (partId: string) => `${parts()}/${partId}/upload-preview`;
 const partsEdit = (partId: string) => `${parts()}/${partId}/update`;
