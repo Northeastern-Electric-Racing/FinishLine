5a3b46de5d47ed80e538b108674f67ed61fe2081
wavehassman <wavehassman@gmail.com>
2025-04-22 11:33:41 -0400

    #3381 removed unused import

--- src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
+++ src/frontend/src/pages/AdminToolsPage/AdminToolsFinanceConfig.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import VendorsTable from './FinanceConfig/VendorsTable';
 import AccountCodesTable from './FinanceConfig/AccountCodesTable';
 
