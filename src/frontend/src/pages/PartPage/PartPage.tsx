dd474f2c9c239a54b605c0f253131ef4f454df9f
Chris Pyle <cpyle@unicode.org>
2025-04-24 15:08:39 -0400

    #3426 breadcrumbs and url resolution

--- src/frontend/src/pages/PartPage/PartPage.tsx
+++ src/frontend/src/pages/PartPage/PartPage.tsx
@@ -1,94 +1,55 @@
 import { Box, Typography, Grid, Breadcrumbs } from '@mui/material';
-import { RoleEnum } from 'shared';
+import { RoleEnum, validateWBS, wbsPipe } from 'shared';
 import PartSubmissionDetails, { partReviewExample1, partReviewExample2 } from './Components/PartSubmissionDetails';
+import PageLayout from '../../components/PageLayout';
+import { routes } from '../../utils/routes';
+import { useParams } from 'react-router-dom';
+import { usePartsFromProject, useSinglePart } from '../../hooks/part-review.hooks';
+import { useSingleProject } from '../../hooks/projects.hooks';
+import LoadingIndicator from '../../components/LoadingIndicator';
+import ErrorPage from '../ErrorPage';
 
 const PartPage: React.FC = () => {
+  interface ParamTypes {
+    wbsNum: string;
+    indexNum: string;
+  }
+  const { wbsNum, indexNum } = useParams<ParamTypes>();
+
+  const {
+    data: project,
+    isLoading: projectLoading,
+    isError: projectIsError,
+    error: projectError
+  } = useSingleProject(validateWBS(wbsNum));
+
+  const {
+    data: part,
+    isLoading: partLoading,
+    isError: partIsError,
+    error: partError
+  } = useSinglePart(wbsNum, parseInt(indexNum));
+
+  if (projectLoading || !project || partLoading || !part) return <LoadingIndicator />;
+  if (projectIsError) return <ErrorPage message={projectError?.message} />;
+  if (partIsError) return <ErrorPage message={partError?.message} />;
+
+  const pageTitle = `${project.abbreviation ?? project.name}_${part.commonName}_${part.index.toString().padStart(5, '0')}`;
+
   return (
-    <Box padding={4}>
-      {/* This is where the breadcrumbs (series of links) will go */}
-      <Breadcrumbs sx={{ mb: 2 }}></Breadcrumbs>
-
-      {/* Need to query for the part title */}
-      <Typography variant="h4" fontWeight="bold" mb={3}>
-        [PROJ_PartName_PartNum]
-      </Typography>
-
-      <Grid container spacing={3}>
-        {/* The code below will be replaced by the part preview */}
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              backgroundColor: 'black',
-              height: '75vh',
-              width: '50%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              border: '2px solid white'
-            }}
-          >
-            <Typography color="white">No submission yet.</Typography>
-          </Box>
-        </Grid>
-
-        {/* The code below will be replaced by the Overview, Details, and History components */}
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={2} direction="column">
-            <Grid item>
-              <Box
-                sx={{
-                  backgroundColor: 'gray',
-                  height: '24vh',
-                  width: '50%',
-                  borderRadius: 2,
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  mb: 2
-                }}
-              >
-                <Typography>Overview</Typography>
-              </Box>
-              <Box
-                sx={{
-                  backgroundColor: 'gray',
-                  height: '24vh',
-                  width: '50%',
-                  borderRadius: 2,
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  mb: 2
-                }}
-              ></Box>
-              <Box>
-                <PartSubmissionDetails
-                  submission={{
-                    partSubmissionId: '1',
-                    userCreated: {
-                      userId: '123',
-                      email: 'john.doe@example.com',
-                      emailId: 'john.doe@example.com',
-                      role: RoleEnum.MEMBER,
-                      permissions: [],
-                      firstName: 'John',
-                      lastName: 'Doe'
-                    },
-                    notes: 'This is a test note.',
-                    reviews: [partReviewExample1, partReviewExample2],
-                    fileIds: [],
-                    name: 'Test Part Submission',
-                    partId: '456',
-                    createdAt: new Date()
-                  }}
-                />
-              </Box>
-              <Typography>History</Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Box>
+    <PageLayout
+      title={pageTitle}
+      previousPages={[
+        { name: 'Projects', route: routes.PROJECTS },
+        { name: `${wbsPipe(project.wbsNum)} - ${project.name}`, route: `${routes.PROJECTS}/${wbsNum}` },
+        { name: 'Files', route: `${routes.PROJECTS}/${wbsNum}/parts-review` }
+      ]}
+    >
+      <Box>
+        <Typography>Part: {part.commonName}</Typography>
+        <Breadcrumbs sx={{ mb: 2 }}></Breadcrumbs>
+      </Box>
+    </PageLayout>
   );
 };
 
