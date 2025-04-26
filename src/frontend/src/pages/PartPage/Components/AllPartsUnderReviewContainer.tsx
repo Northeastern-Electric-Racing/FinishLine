5325830bc7f44710df172a37390a1a1f639e4b8c
gaurirajesh <rajesh.ga@northeastern.edu>
2025-04-26 17:14:42 -0400

    #3370 - corrections
    added loading, error, removed checking user

--- src/frontend/src/pages/PartPage/Components/AllPartsUnderReviewContainer.tsx
+++ src/frontend/src/pages/PartPage/Components/AllPartsUnderReviewContainer.tsx
@@ -1,38 +1,38 @@
 import { Box } from '@mui/system';
-import { isHead, isLead, Project, Review_Status, wbsPipe } from 'shared';
+import { Project, Review_Status, wbsPipe } from 'shared';
 import { Grid, Typography } from '@mui/material';
-import { useCurrentUser } from '../../../hooks/users.hooks';
 import { usePartsFromProject } from '../../../hooks/part-review.hooks';
+import LoadingIndicator from '../../../components/LoadingIndicator';
+import EmptyPageBlockDisplay from '../../HomePage/components/EmptyPageBlockDisplay';
+import React from 'react';
+import ErrorPage from '../../ErrorPage';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 
 interface AllPartsUnderReview {
   project: Project;
 }
 
 const AllPartsUnderReviewContainer: React.FC<AllPartsUnderReview> = ({ project }) => {
-  // check if current user is head or lead for this project
-  const currentUser = useCurrentUser();
   const { wbsNum } = project;
   const { data, isLoading, isError, error } = usePartsFromProject(wbsPipe(wbsNum));
 
-  if (isLoading || isError || error) {
-    return null;
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+  if (isError) {
+    return <ErrorPage message={error.message} />;
   }
 
-  // all heads for this project
-  const projectHeads = project.teams.map((team) => team.head);
-  // all leads for this project
-  const projectLeads = project.teams
-    .map((team) => team.leads)
-    .reduce((accumLeads, currTeamLeads) => accumLeads.concat(currTeamLeads));
-
-  const isUserHead = isHead(currentUser.role) && projectHeads.includes(currentUser);
-  const isUserLead = isLead(currentUser.role) && projectLeads.includes(currentUser);
-
-  // filter out parts in project that have not been approved
   const parts = data?.filter((part) => part.status !== Review_Status.APPROVED) || [];
-  // show list only if head/lead of current project
-  if (!(isUserHead || isUserLead) || parts.length === 0) {
-    return null;
+
+  if (parts.length === 0) {
+    return (
+      <EmptyPageBlockDisplay
+        icon={<CheckCircleOutlineOutlinedIcon sx={{ fontSize: 70 }} />}
+        heading={`You're all caught up!`}
+        message={'There are no in progress parts on this project!'}
+      />
+    );
   }
   return (
     <Grid item xs={12}>
