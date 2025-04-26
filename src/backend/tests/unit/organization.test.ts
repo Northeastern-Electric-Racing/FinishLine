6088ffeb159eda10899b29826f69a542636375c2
gaurirajesh <rajesh.ga@northeastern.edu>
2025-04-26 00:59:19 -0400

    #3409 - added unit tests

--- src/backend/tests/unit/organization.test.ts
+++ src/backend/tests/unit/organization.test.ts
@@ -330,4 +330,32 @@ describe('Organization Tests', () => {
       expect(updatedOrganization.slackWorkspaceId).toBe('1234');
     });
   });
+
+  describe('Get Part Review Guide Link', () => {
+    it('Fails if an organization does not exist', async () => {
+      await expect(async () => await OrganizationsService.getPartReviewGuideLink('1')).rejects.toThrow(
+        new NotFoundException('Organization', '1')
+      );
+    });
+
+    it('Succeeds and gets the part review guide link', async () => {
+      const testBatman = await createTestUser(batmanAppAdmin, orgId);
+      await OrganizationsService.setPartReviewGuideLink(testBatman, orgId, 'newlink');
+      const guideLink = await OrganizationsService.getPartReviewGuideLink(orgId);
+
+      expect(guideLink).not.toBeNull();
+      expect(guideLink).toBe('newlink');
+    });
+  });
+
+  describe('Set Part Review Guide Link', () => {
+    it('Succeeds and updates part review guide link', async () => {
+      const testBatman = await createTestUser(batmanAppAdmin, orgId);
+
+      const updatedOrganization = await OrganizationsService.setPartReviewGuideLink(testBatman, orgId, 'newlink');
+
+      expect(updatedOrganization).not.toBeNull();
+      expect(updatedOrganization.partReviewGuideLink).toBe('newlink');
+    });
+  });
 });
