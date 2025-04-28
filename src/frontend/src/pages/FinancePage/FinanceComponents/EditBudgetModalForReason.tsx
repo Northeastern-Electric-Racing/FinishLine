893098e1dafebe356e66f44c827eeeaf7a53acd5
Sarah Taylor <150694563+staysgt@users.noreply.github.com>
2025-04-28 19:33:25 -0400

    #3329 - modal stays fixed size

--- src/frontend/src/pages/FinancePage/FinanceComponents/EditBudgetModalForReason.tsx
+++ src/frontend/src/pages/FinancePage/FinanceComponents/EditBudgetModalForReason.tsx
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import NERFormModal from '../../../components/NERFormModal';
-import { FormControl, FormLabel, MenuItem, Select, Typography } from '@mui/material';
+import { Box, FormControl, FormLabel, MenuItem, Select, Typography } from '@mui/material';
 import ReactHookTextField from '../../../components/ReactHookTextField';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import { useGetAllIndexCodes, useGetAllOtherProductReason } from '../../../hooks/finance.hooks';
@@ -104,96 +104,98 @@ export const EditBudgetModalForReason: React.FC<EditBudgetModalForReasonProps> =
       submitText="Save"
       showCloseButton
     >
-      <FormControl fullWidth>
-        <FormLabel sx={{ alignSelf: 'start' }}>Category</FormLabel>
-        <Controller
-          control={control}
-          name={'category'}
-          render={({ field: { onChange, value } }) => (
-            <Select
-              displayEmpty
-              value={value !== undefined ? value : ''}
-              onChange={onChange}
-              renderValue={(selected) => {
-                const selectedReason = otherReasons.find((r) => r.otherProductReasonId === selected);
-                return selectedReason ? (
-                  selectedReason.name
-                ) : (
-                  <Typography sx={{ color: 'gray' }}>Select category to allocate to</Typography>
-                );
-              }}
-              sx={{ height: 56, width: '100%', textAlign: 'left' }}
-              fullWidth
-              MenuProps={{
-                anchorOrigin: {
-                  vertical: 'bottom',
-                  horizontal: 'right'
-                },
-                transformOrigin: {
-                  vertical: 'top',
-                  horizontal: 'right'
-                }
-              }}
-            >
-              {Object.values(otherReasons).map((reason) => (
-                <MenuItem key={reason.otherProductReasonId} value={reason.otherProductReasonId}>
-                  {reason.name}
-                </MenuItem>
-              ))}
-            </Select>
-          )}
-        />
-      </FormControl>
+      <Box display="flex" flexDirection={'column'} minWidth={400}>
+        <FormControl fullWidth>
+          <FormLabel sx={{ alignSelf: 'start' }}>Category</FormLabel>
+          <Controller
+            control={control}
+            name={'category'}
+            render={({ field: { onChange, value } }) => (
+              <Select
+                displayEmpty
+                value={value !== undefined ? value : ''}
+                onChange={onChange}
+                renderValue={(selected) => {
+                  const selectedReason = otherReasons.find((r) => r.otherProductReasonId === selected);
+                  return selectedReason ? (
+                    selectedReason.name
+                  ) : (
+                    <Typography sx={{ color: 'gray' }}>Select category to allocate to</Typography>
+                  );
+                }}
+                sx={{ height: 56, width: '100%', textAlign: 'left' }}
+                fullWidth
+                MenuProps={{
+                  anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'right'
+                  },
+                  transformOrigin: {
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }
+                }}
+              >
+                {Object.values(otherReasons).map((reason) => (
+                  <MenuItem key={reason.otherProductReasonId} value={reason.otherProductReasonId}>
+                    {reason.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+          />
+        </FormControl>
 
-      <FormControl fullWidth>
-        <FormLabel sx={{ alignSelf: 'start' }}>Account</FormLabel>
-        <Controller
-          control={control}
-          name={'updatedIndexCode'}
-          render={({ field: { onChange, value } }) => (
-            <Select
-              displayEmpty
-              value={value !== undefined ? value : ''}
-              onChange={onChange}
-              renderValue={(selected) => {
-                const code = indexCodes.find((c) => c.indexCodeId === selected);
-                return code ? code.name : <Typography sx={{ color: 'gray' }}>Select account</Typography>;
-              }}
-              sx={{ height: 56, width: '100%', textAlign: 'left' }}
-              fullWidth
-              MenuProps={{
-                anchorOrigin: {
-                  vertical: 'bottom',
-                  horizontal: 'right'
-                },
-                transformOrigin: {
-                  vertical: 'top',
-                  horizontal: 'right'
-                }
-              }}
-            >
-              {Object.values(indexCodes).map((code) => (
-                <MenuItem key={code.indexCodeId} value={code.indexCodeId}>
-                  {code.name}
-                </MenuItem>
-              ))}
-            </Select>
-          )}
-        />
-      </FormControl>
+        <FormControl fullWidth>
+          <FormLabel sx={{ alignSelf: 'start' }}>Account</FormLabel>
+          <Controller
+            control={control}
+            name={'updatedIndexCode'}
+            render={({ field: { onChange, value } }) => (
+              <Select
+                displayEmpty
+                value={value !== undefined ? value : ''}
+                onChange={onChange}
+                renderValue={(selected) => {
+                  const code = indexCodes.find((c) => c.indexCodeId === selected);
+                  return code ? code.name : <Typography sx={{ color: 'gray' }}>Select account</Typography>;
+                }}
+                sx={{ height: 56, width: '100%', textAlign: 'left' }}
+                fullWidth
+                MenuProps={{
+                  anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'right'
+                  },
+                  transformOrigin: {
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }
+                }}
+              >
+                {Object.values(indexCodes).map((code) => (
+                  <MenuItem key={code.indexCodeId} value={code.indexCodeId}>
+                    {code.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+          />
+        </FormControl>
 
-      <FormControl>
-        <FormLabel>Amount</FormLabel>
-        <ReactHookTextField
-          placeholder={'New Amount'}
-          name="updatedBudget"
-          type="number"
-          control={control}
-          sx={{ width: 1 }}
-          startAdornment={<AttachMoneyIcon />}
-          errorMessage={errors.updatedBudget}
-        />
-      </FormControl>
+        <FormControl>
+          <FormLabel>Amount</FormLabel>
+          <ReactHookTextField
+            placeholder={'New Amount'}
+            name="updatedBudget"
+            type="number"
+            control={control}
+            sx={{ width: 1 }}
+            startAdornment={<AttachMoneyIcon />}
+            errorMessage={errors.updatedBudget}
+          />
+        </FormControl>
+      </Box>
     </NERFormModal>
   );
 };
