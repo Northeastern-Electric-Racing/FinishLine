034d2b199ee2f8153c10e34c0b4a7676a05b9fba
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-05-01 14:20:27 -0400

    table modifications

--- src/frontend/src/pages/FinancePage/ReimbursementRequestDetailPage/ReimbursementProductsView.tsx
+++ src/frontend/src/pages/FinancePage/ReimbursementRequestDetailPage/ReimbursementProductsView.tsx
@@ -15,24 +15,40 @@ const ReimbursementProductsView: React.FC<ReimbursementRequestProductsViewProps>
     keys.push(key);
   }
 
+  const allKeysAreSame = keys.length === 0 || keys.every((key) => key === keys[0]);
+
   return (
     <>
       <Typography variant="h5">Products</Typography>
-      <Table component={Paper}>
+      <Table sx={{ maxWidth: 500 }}>
         <TableHead>
           <TableRow>
-            <TableCell>Project or Expense Category</TableCell>
-            <TableCell>Products</TableCell>
+            <TableCell>
+              <Typography variant="h6">Item</Typography>
+            </TableCell>
+            {!allKeysAreSame && (
+              <TableCell>
+                <Typography variant="h6">Project</Typography>
+              </TableCell>
+            )}
+            <TableCell>
+              <Typography variant="h6">Cost</Typography>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {keys.map((key) => {
             return (
               <TableRow key={key}>
-                <TableCell>{displayEnum(key)}</TableCell>
                 <TableCell>
                   {uniqueWbsElementsWithProducts.get(key)?.map((product) => {
-                    return <Chip label={`${product.name} $${centsToDollar(product.cost)}`} />;
+                    return product.name;
+                  })}
+                </TableCell>
+                {!allKeysAreSame && <TableCell>{displayEnum(key)}</TableCell>}
+                <TableCell>
+                  {uniqueWbsElementsWithProducts.get(key)?.map((product) => {
+                    return `$${centsToDollar(product.cost)}`;
                   })}
                 </TableCell>
               </TableRow>
