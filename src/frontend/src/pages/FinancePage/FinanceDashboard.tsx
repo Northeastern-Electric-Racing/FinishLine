4ee4f88dbba46a68f9c7902e53d8ae2b7838b24d
Sarah Taylor <150694563+staysgt@users.noreply.github.com>
2025-05-02 20:57:06 -0400

    #3328 - edit budget for project modal

--- src/frontend/src/pages/FinancePage/FinanceDashboard.tsx
+++ src/frontend/src/pages/FinancePage/FinanceDashboard.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+import { NERButton } from '../../components/NERButton';
+import { EditBudgetModalForProject } from './FinanceComponents/EditBudgetModalProject';
 import { Box } from '@mui/system';
 
 const FinanceDashboard: React.FC = () => {
-  return <Box>Finance Dashboard</Box>;
+  const [showModal, setShowModal] = useState(false);
+  // return <Box>Finance Dashboard</Box>;
+  return (
+    <Box>
+      <NERButton onClick={() => setShowModal(true)}> Edit Budget</NERButton>
+      <EditBudgetModalForProject showModal={showModal} handleClose={() => setShowModal(false)} />
+    </Box>
+  );
 };
 
 export default FinanceDashboard;
