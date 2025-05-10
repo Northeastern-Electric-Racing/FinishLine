946a92c4cdf13936bd132195d3e489f62f3b5475
wavehassman <wavehassman@gmail.com>
2025-05-09 23:06:34 -0400

    #3440 requested changes

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/ProjectDetails.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/ProjectDetails.tsx
@@ -5,7 +5,7 @@
 
 import { Construction, Work } from '@mui/icons-material';
 import ScheduleIcon from '@mui/icons-material/Schedule';
-import { Project, WorkPackage, wbsPipe } from 'shared';
+import { Project, wbsPipe } from 'shared';
 import { datePipe, dollarsPipe, fullNamePipe, weeksPipe } from '../../../utils/pipes';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -45,207 +45,103 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
     return <LoadingIndicator />;
   }
 
-  const isEmpty =
+  const emptyRRData =
     rrData.pendingLeadership === 0 &&
     rrData.pendingFinance === 0 &&
     rrData.submittedToSabo === 0 &&
     rrData.reimbursed === 0 &&
     rrData.available === 0;
 
-  if (isEmpty) {
-    return (
-      <Grid container display="flex" flexDirection="row" sx={{ mt: '10px' }}>
-        <Grid item sm={12} md={6} sx={{ mb: 2 }}>
-          <Typography
-            variant="h5"
-            sx={{
-              cursor: 'pointer',
-              mb: 1
-            }}
-          >
-            Details
-          </Typography>
+  const detailGridSize = emptyRRData ? { sm: 12, md: 6 } : { xs: 12, md: 3 };
+  const summaryGridSize = emptyRRData ? { sm: 12, md: 6 } : { xs: 12, md: 3 };
 
-          <Grid container spacing={2}>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <GroupIcon sx={{ mr: 2 }} />
-              <DetailDisplay
-                label={project.teams.length > 1 ? 'Teams' : 'Team'}
-                content={getProjectTeamsName(project)}
-                paddingRight={1}
-              />
-            </Grid>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <Construction sx={{ mr: 2 }} />
-              <DetailDisplay label="Project Lead" content={fullNamePipe(project.lead)} paddingRight={1} />
-            </Grid>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <ScheduleIcon sx={{ mr: 2 }} />
-              <DetailDisplay label="Start Date" content={datePipe(project.startDate) || 'n/a'} paddingRight={1} />
-            </Grid>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <Work sx={{ mr: 2 }} />
-              <DetailDisplay label="Project Manager" content={fullNamePipe(project.manager)} paddingRight={1} />
-            </Grid>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <ScheduleIcon sx={{ mr: 2 }} />
-              <DetailDisplay label="End Date" content={datePipe(project.endDate) || 'n/a'} paddingRight={1} />
-            </Grid>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <AttachMoneyIcon sx={{ mr: 2 }} />
-              <DetailDisplay label="Budget" content={dollarsPipe(project.budget)} paddingRight={1} />
-            </Grid>
-            <Grid item display="flex" alignItems="center" xs={12} sm={6}>
-              <ScheduleIcon sx={{ mr: 2 }} />
-              <DetailDisplay label="Duration" content={weeksPipe(project.duration)} paddingRight={1} />
-            </Grid>
-          </Grid>
-        </Grid>
+  const detailItems = [
+    {
+      icon: <GroupIcon sx={{ mr: emptyRRData ? 2 : 1 }} />,
+      label: project.teams.length > 1 ? 'Teams' : 'Team',
+      content: getProjectTeamsName(project)
+    },
+    {
+      icon: <Construction sx={{ mr: emptyRRData ? 2 : 1 }} />,
+      label: 'Project Lead',
+      content: fullNamePipe(project.lead)
+    },
+    {
+      icon: <Work sx={{ mr: emptyRRData ? 2 : 1 }} />,
+      label: 'Project Manager',
+      content: fullNamePipe(project.manager)
+    },
+    {
+      icon: <ScheduleIcon sx={{ mr: emptyRRData ? 2 : 1 }} />,
+      label: 'Start Date',
+      content: datePipe(project.startDate) || 'n/a'
+    },
+    {
+      icon: <ScheduleIcon sx={{ mr: emptyRRData ? 2 : 1 }} />,
+      label: 'Duration',
+      content: weeksPipe(project.duration)
+    },
+    {
+      icon: <ScheduleIcon sx={{ mr: emptyRRData ? 2 : 1 }} />,
+      label: 'End Date',
+      content: datePipe(project.endDate) || 'n/a'
+    },
+    ...(emptyRRData
+      ? [
+          {
+            icon: <AttachMoneyIcon sx={{ mr: 2 }} />,
+            label: 'Budget',
+            content: dollarsPipe(project.budget)
+          }
+        ]
+      : [])
+  ];
 
-        <Grid container display="flex" flexDirection="row" item sm={12} md={6} sx={{ mb: 2 }}>
-          <Grid item xs sx={{ mb: 2 }}>
-            <Typography
-              variant="h5"
-              sx={{
-                mb: 1,
-                cursor: 'pointer'
-              }}
-            >
-              Summary
-            </Typography>
-            <Typography>{project.summary}</Typography>
-          </Grid>
-
-          <Grid container item xs={12}>
-            <Grid item xs={12}>
-              <Typography
-                variant="h5"
-                sx={{
-                  mb: 1,
-                  cursor: 'pointer'
-                }}
-              >
-                Links
-              </Typography>
-            </Grid>
-            {project.links.map((link) => (
-              <Grid item xs={4} key={link.linkId}>
-                <LinkView link={link} />
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-        <Grid container item display="flex">
-          <Grid item xs={12}>
-            <Typography
-              variant="h5"
-              sx={{
-                mb: 1,
-                cursor: 'pointer'
-              }}
-            >
-              Work Packages
-            </Typography>
-          </Grid>
-          {project.workPackages.map((ele: WorkPackage) => (
-            <Grid item xs={12} key={wbsPipe(ele.wbsNum)} sx={{ mb: 0.5 }}>
-              <WorkPackageSummary workPackage={ele} />
+  return (
+    <Grid container spacing={2} sx={{ mt: '10px' }}>
+      <Grid item {...detailGridSize} sx={{ mb: 2 }}>
+        <Typography variant="h5" sx={{ cursor: 'pointer', mb: 1 }}>
+          Details
+        </Typography>
+        <Grid container spacing={2}>
+          {detailItems.map((item, index) => (
+            <Grid item display="flex" alignItems="center" xs={12} sm={emptyRRData ? 6 : 12} key={index}>
+              {item.icon}
+              <DetailDisplay label={item.label} content={item.content} paddingRight={1} />
             </Grid>
           ))}
         </Grid>
       </Grid>
-    );
-  }
-
-  return (
-    <Grid container spacing={2} sx={{ mt: '10px' }}>
-      <Grid item xs={12} md={3} sx={{ mb: 2 }}>
-        <Typography
-          variant="h5"
-          sx={{
-            cursor: 'pointer',
-            mb: 1
-          }}
-        >
-          Details
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item display="flex" alignItems="center" xs={12}>
-            <GroupIcon sx={{ mr: 1 }} />
-            <DetailDisplay
-              label={project.teams.length > 1 ? 'Teams' : 'Team'}
-              content={getProjectTeamsName(project)}
-              paddingRight={1}
-            />
-          </Grid>
-          <Grid item display="flex" alignItems="center" xs={12}>
-            <Construction sx={{ mr: 1 }} />
-            <DetailDisplay label="Project Lead" content={fullNamePipe(project.lead)} paddingRight={1} />
-          </Grid>
-          <Grid item display="flex" alignItems="center" xs={12}>
-            <Work sx={{ mr: 1 }} />
-            <DetailDisplay label="Project Manager" content={fullNamePipe(project.manager)} paddingRight={1} />
-          </Grid>
-          <Grid item display="flex" alignItems="center" xs={12}>
-            <ScheduleIcon sx={{ mr: 1 }} />
-            <DetailDisplay label="Start Date" content={datePipe(project.startDate) || 'n/a'} paddingRight={1} />
-          </Grid>
-          <Grid item display="flex" alignItems="center" xs={12}>
-            <ScheduleIcon sx={{ mr: 1 }} />
-            <DetailDisplay label="End Date" content={datePipe(project.endDate) || 'n/a'} paddingRight={1} />
-          </Grid>
-          <Grid item display="flex" alignItems="center" xs={12}>
-            <ScheduleIcon sx={{ mr: 1 }} />
-            <DetailDisplay label="Duration" content={weeksPipe(project.duration)} paddingRight={1} />
-          </Grid>
+      {!emptyRRData && (
+        <Grid item xs={12} md={3} sx={{ mb: 7, mr: 29 }}>
+          <Typography variant="h5" sx={{ cursor: 'pointer', mb: -4 }}>
+            Budget
+          </Typography>
+          <PieChart
+            totalBalance={rrData.totalBudget}
+            pendingLeadership={rrData.pendingLeadership}
+            pendingFinance={rrData.pendingFinance}
+            submittedToSABO={rrData.submittedToSabo}
+            reimbursed={rrData.reimbursed}
+            available={rrData.available}
+          />
         </Grid>
-      </Grid>
-      <Grid item xs={12} md={3} sx={{ mb: 7, mr: 29 }}>
-        <Typography
-          variant="h5"
-          sx={{
-            cursor: 'pointer',
-            mb: -4
-          }}
-        >
-          Budget
-        </Typography>
-        <PieChart
-          totalBalance={rrData.totalBudget}
-          pendingLeadership={rrData.pendingLeadership}
-          pendingFinance={rrData.pendingFinance}
-          submittedToSABO={rrData.submittedToSabo}
-          reimbursed={rrData.reimbursed}
-          available={rrData.available}
-        />
-      </Grid>
-      <Grid item xs={12} md={3} sx={{ mb: 2 }}>
+      )}
+      <Grid item {...summaryGridSize} sx={{ mb: emptyRRData ? 2 : 7 }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography
-              variant="h5"
-              sx={{
-                cursor: 'pointer',
-                mb: 1
-              }}
-            >
+            <Typography variant="h5" sx={{ cursor: 'pointer', mb: 1 }}>
               Summary
             </Typography>
             <Typography>{project.summary}</Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography
-              variant="h5"
-              sx={{
-                cursor: 'pointer',
-                mb: 1
-              }}
-            >
+            <Typography variant="h5" sx={{ cursor: 'pointer', mb: 1 }}>
               Links
             </Typography>
             <Grid container spacing={1}>
               {project.links.map((link) => (
-                <Grid item xs={12} key={link.linkId}>
+                <Grid item xs={emptyRRData ? 4 : 12} key={link.linkId}>
                   <LinkView link={link} />
                 </Grid>
               ))}
@@ -254,16 +150,10 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
         </Grid>
       </Grid>
       <Grid item xs={12}>
-        <Typography
-          variant="h5"
-          sx={{
-            mb: 2,
-            cursor: 'pointer'
-          }}
-        >
+        <Typography variant="h5" sx={{ mb: 2, cursor: 'pointer' }}>
           Work Packages
         </Typography>
-        {project.workPackages.map((ele: WorkPackage) => (
+        {project.workPackages.map((ele) => (
           <Grid item xs={12} key={wbsPipe(ele.wbsNum)} sx={{ mb: 0.5 }}>
             <WorkPackageSummary workPackage={ele} />
           </Grid>
