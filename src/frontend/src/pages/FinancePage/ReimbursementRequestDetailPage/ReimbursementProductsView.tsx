d00482764613e8b4af5c81ea03a5a035187f94de
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-05-10 01:46:27 -0400

    comment no longer needed

--- src/frontend/src/pages/FinancePage/ReimbursementRequestDetailPage/ReimbursementProductsView.tsx
+++ src/frontend/src/pages/FinancePage/ReimbursementRequestDetailPage/ReimbursementProductsView.tsx
@@ -17,8 +17,6 @@ const ReimbursementProductsView: React.FC<ReimbursementRequestProductsViewProps>
 
   const allKeysAreSame = keys.length === 0 || keys.every((key) => key === keys[0]);
 
-  // Placeholder until support for multiple refund sources is available
-  // Currently, does not seem to be the case, as cost is the only field
   const multipleRefundSources = reimbursementRequest.reimbursementProducts.some(
     (product) => product.refundSources.length > 1
   );
