50f29e47bd0bc6d27a210389b5edc772f57916ff
Sean Walker <walker.se@northeastern.edu>
2025-05-10 22:11:39 -0400

    #3452 follow up

--- src/backend/src/prisma/seed-data/teams.seed.ts
+++ src/backend/src/prisma/seed-data/teams.seed.ts
@@ -95,7 +95,7 @@ const financeTeam = (headId: string, organizationId: string): Prisma.TeamCreateA
   return {
     data: {
       teamName: 'financeTeam',
-      slackId: 'C06HR7WTTKM',
+      slackId: 'financerand',
       headId,
       organizationId,
       financeTeam: true
