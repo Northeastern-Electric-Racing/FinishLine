cfca82e2eed527cf1e5f06376eb21404cdae59b1
dreifusjack <dreifus.j@northeastern.edu>
2025-05-25 10:24:46 -0400

    #3443 schema change, date deleted on sponsor tasks

--- src/backend/src/prisma/migrations/20250402021534_finance_redesign/migration.sql
+++ src/backend/src/prisma/migrations/20250402021534_finance_redesign/migration.sql
@@ -257,3 +257,6 @@ ALTER TABLE "Reimbursement_Request_Comment" ADD CONSTRAINT "Reimbursement_Reques
 
 -- AlterTable
 ALTER TABLE "Reimbursement_Request_Comment" ADD COLUMN     "dateDeleted" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "Sponsor_Task" ADD COLUMN     "dateDeleted" TIMESTAMP(3);
