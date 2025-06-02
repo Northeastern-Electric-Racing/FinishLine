a679b7f566387be19e6fa23b62aaa7f1516eb11b
Chris Pyle <cpyle@unicode.org>
2025-06-02 09:44:11 +0200

    only in prod

--- src/backend/src/utils/slack.utils.ts
+++ src/backend/src/utils/slack.utils.ts
@@ -559,7 +559,7 @@ export const sendSlackPartReviewRequestNotif = async (
   partName: string,
   partLink: string
 ) => {
-  // if (process.env.NODE_ENV !== 'production') return; // don't send msgs unless in prod
+  if (process.env.NODE_ENV !== 'production') return; // don't send msgs unless in prod
 
   const msg = `Your review has been requested on part: ${partName} for project ${projectName}`;
   const link = `https://finishlinebyner.com${partLink}`;
@@ -573,7 +573,7 @@ export const sendSlackPartAssignmentNotif = async (
   partName: string,
   partLink: string
 ) => {
-  // if (process.env.NODE_ENV !== 'production') return; // don't send msgs unless in prod
+  if (process.env.NODE_ENV !== 'production') return; // don't send msgs unless in prod
 
   const msg = `You have been assigned to part: ${partName} on project ${projectName}`;
   const link = `https://finishlinebyner.com${partLink}`;
