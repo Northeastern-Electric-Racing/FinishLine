f654c6871668c67a92537a74de769362551f9c5f
Peyton-McKee <mckee.p@northeastern.edu>
2025-07-08 09:43:50 -0400

    fix diff page

--- src/frontend/src/pages/ChangeRequestDetailPage/DiffSection/DiffSection.tsx
+++ src/frontend/src/pages/ChangeRequestDetailPage/DiffSection/DiffSection.tsx
@@ -1,58 +1,27 @@
 import { Box } from '@mui/system';
 import InfoBlock from '../../../components/InfoBlock';
-import { StandardChangeRequest, isProject } from 'shared';
-import { displayEnum } from '../../../utils/pipes';
-import DiffSectionCreate from './DiffSectionCreate';
-import DiffSectionEdit from './DiffSectionEdit';
-import { projectProposedChangesToPreview, workPackageProposedChangesToPreview } from '../../../utils/diff-page.utils';
+import { isProjectWbs, StandardChangeRequest } from 'shared';
+import ProjectDiffSection from './ProjectDiffSection';
+import WorkPackageDiffSection from './WorkPackageDiffSection';
 import LoadingIndicator from '../../../components/LoadingIndicator';
-
 interface DiffSectionProps {
   changeRequest: StandardChangeRequest;
 }
 
-enum ChangeRequestAction {
-  CREATE_PROJECT = 'CREATE_PROJECT',
-  CREATE_WORK_PACKAGE = 'CREATE_WORK_PACKAGE',
-  EDIT_PROJECT = 'EDIT_PROJECT',
-  EDIT_WORK_PACKAGE = 'EDIT_WORK_PACKAGE'
-}
-
 const DiffSection: React.FC<DiffSectionProps> = ({ changeRequest }) => {
   const { wbsNum, projectProposedChanges, workPackageProposedChanges } = changeRequest;
-  const isOnProject = wbsNum ? isProject(wbsNum) : false;
-
-  const changeRequestAction: ChangeRequestAction =
-    projectProposedChanges && projectProposedChanges.carNumber !== undefined
-      ? ChangeRequestAction.CREATE_PROJECT
-      : isOnProject && workPackageProposedChanges
-        ? ChangeRequestAction.CREATE_WORK_PACKAGE
-        : isOnProject
-          ? ChangeRequestAction.EDIT_PROJECT
-          : ChangeRequestAction.EDIT_WORK_PACKAGE;
-
-  const projectProposedChangesPreview = projectProposedChangesToPreview(projectProposedChanges);
-  const originalProjectData = projectProposedChangesToPreview(changeRequest.originalProjectData);
-  const originalWorkPackageData = workPackageProposedChangesToPreview(changeRequest.originalWorkPackageData);
-  const workPackageProposedChangesPreview = workPackageProposedChangesToPreview(workPackageProposedChanges);
 
   return (
     <Box>
-      <InfoBlock title={`Proposed Changes - ${displayEnum(changeRequestAction)}`} />
-      {changeRequestAction === ChangeRequestAction.CREATE_PROJECT ||
-      changeRequestAction === ChangeRequestAction.CREATE_WORK_PACKAGE ? (
-        <DiffSectionCreate
-          projectProposedChanges={projectProposedChangesPreview}
-          workPackageProposedChanges={workPackageProposedChangesPreview}
-        />
-      ) : wbsNum ? (
-        <DiffSectionEdit
-          projectProposedChanges={projectProposedChangesPreview}
-          workPackageProposedChanges={workPackageProposedChangesPreview}
-          originalProjectData={originalProjectData}
-          originalWorkPackageData={originalWorkPackageData}
-          wbsNum={wbsNum}
-        />
+      <InfoBlock title={`Proposed Changes`} />
+      {wbsNum ? (
+        projectProposedChanges ? (
+          <ProjectDiffSection projectProposedChanges={projectProposedChanges} wbsNum={wbsNum} />
+        ) : workPackageProposedChanges ? (
+          <WorkPackageDiffSection workPackageProposedChanges={workPackageProposedChanges} wbsNum={wbsNum} />
+        ) : (
+          <></>
+        )
       ) : (
         <LoadingIndicator />
       )}
