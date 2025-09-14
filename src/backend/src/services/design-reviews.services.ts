18f15fb75a990489da077adc210cdea357fdf5e4
Chris Pyle <pyle.c@northeastern.edu>
2025-09-14 19:15:44 -0400

    add prisma to prod dependencies

--- src/backend/src/services/design-reviews.services.ts
+++ src/backend/src/services/design-reviews.services.ts
@@ -196,15 +196,14 @@ export default class DesignReviewsService {
     for (const memberUserSetting of memberUserSettings) {
       if (memberUserSetting.slackId) {
         try {
-          if (!project) {
-            throw new Error('Could not find work package project');
+          if (project) {
+            await sendSlackDesignReviewConfirmNotification(
+              memberUserSetting.slackId,
+              designReview.designReviewId,
+              designReview.wbsElement.name,
+              project.wbsElement.name
+            );
           }
-          await sendSlackDesignReviewConfirmNotification(
-            memberUserSetting.slackId,
-            designReview.designReviewId,
-            designReview.wbsElement.name,
-            project.wbsElement.name
-          );
         } catch (err: unknown) {
           if (err instanceof Error) {
             throw new HttpException(500, `Failed to send slack notification: ${err.message}`);
