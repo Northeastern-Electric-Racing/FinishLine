53e7047214e2ffa16762d1f789d472be7de1809f
sayegh.st@northeastern.edu <stephaniesayegh375@gmail.com>
2025-10-04 12:13:44 -0400

    hook fully setup

--- src/frontend/src/hooks/calendar.hooks.ts
+++ src/frontend/src/hooks/calendar.hooks.ts
@@ -1,1 +1,36 @@
-import { useState, useContext } from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { Shop } from 'shared';
+import { getShops, createShop} from '../apis/calendar.api';
+
+/**
+ * Get all shops for the current org
+ */
+export const useShops = () => {
+  return useQuery<Shop[], Error>(['calendar', 'shops'], async () => {
+    const { data } = await getShops();
+    return data;
+  });
+};
+
+/**
+ * Create a shop 
+ */
+type CreateShopPayload = { name: string; description: string };
+
+export const useCreateShop = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<Shop, Error, CreateShopPayload>(
+    ['calendar', 'shops', 'create'],
+    async ({ name, description }) => {
+      const { data } = await createShop({ name, description });
+      return data;
+    },
+    {
+      onSuccess: () => {
+        // refresh the table
+        queryClient.invalidateQueries(['calendar', 'shops']);
+      }
+    }
+  );
+};
