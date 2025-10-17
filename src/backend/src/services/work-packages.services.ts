0b3b8aebf8f980a5d88e206d9b84c83569339785
harish <harivarada1@gmail.com>
2025-10-16 20:11:52 -0400

    prettier

--- src/backend/src/services/work-packages.services.ts
+++ src/backend/src/services/work-packages.services.ts
@@ -43,7 +43,7 @@ dayjs.extend(utc);
 export default class WorkPackagesService {
   /**
    * Normalize an input date string to the user's local midnight converted to UTC
-   * @param input - The date string to normalize 
+   * @param input - The date string to normalize
    * @returns date object representing midnight UTC
    */
   private static toUtcMidnight(input: string): Date {
