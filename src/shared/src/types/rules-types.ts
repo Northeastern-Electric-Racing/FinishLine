a6f6939157d3c5136eb764eb4f37221ec8256cb3
Ciel Bellerose <kcbellerose@icloud.com>
2025-10-30 12:22:49 -0400

    #3692 initial shared/schema updates

--- src/shared/src/types/rules-types.ts
+++ src/shared/src/types/rules-types.ts
@@ -37,18 +37,8 @@ export interface Rule {
   ruleCode: string;
   ruleContent: string;
   imageFileIds: string[];
-  ruleset: {
-    rulesetId: string;
-    name: string;
-  };
-  parentRule?: {
-    ruleId: string;
-    ruleCode: string;
-  };
   subRuleIds: string[];
-  referencedRules: { ruleId: string; ruleCode: string }[];
-  referencedBy: { ruleId: string; ruleCode: string }[];
-  projects: ProjectRule[];
+  referencedRules: { ruleId: string }[];
 }
 
 export interface RuleStatusChange {
