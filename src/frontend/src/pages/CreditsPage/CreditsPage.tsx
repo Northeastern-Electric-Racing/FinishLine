30f9d4d448d0a5a7f445b8640615e894bbc62ac1
Tony Feng <tonywf15@gmail.com>
2025-11-04 16:53:20 -0500

    added credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -335,6 +335,21 @@ const CreditsPage: React.FC = () => {
         },
         filter: 'drop-shadow(0 0 4px rgba(190, 80, 255, 0.6))'
       }
+    },
+    {
+      name: 'Tony Feng',
+      color: 'transparent',
+      sx: {
+        textShadow: '0 0 5px #a7f3eaff, 0 0 10px #fb99e9ff, 0 0 30px #97eb97ff',
+        background: 'linear-gradient(90deg, white, violet, blue, red, white)',
+        '-webkit-background-clip': 'text',
+        backgroundSize: '800% 800%',
+        animation: 'gradient 30s infinite linear',
+        '@keyframes gradient': {
+          '0%': { backgroundPosition: '400% 200%' },
+          '100%': { backgroundPosition: '0% 200%' }
+        }
+      }
     }
   ];
 
