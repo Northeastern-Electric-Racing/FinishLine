06ed70e559ea3f178849958a067f2c2e79e77786
Anthony Basko <75228079+nunnyu@users.noreply.github.com>
2025-11-05 19:07:08 -0500

    Prettier linting fix

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -318,8 +318,8 @@ const CreditsPage: React.FC = () => {
     {
       name: 'Anthony Basko',
       color: '#c0aed0ff',
-      sx: { 
-        textShadow: '0px 0px 3px rgba(152, 119, 233, 1), 0px .5px rgba(53, 37, 66, 1)' 
+      sx: {
+        textShadow: '0px 0px 3px rgba(152, 119, 233, 1), 0px .5px rgba(53, 37, 66, 1)'
       }
     },
     {
