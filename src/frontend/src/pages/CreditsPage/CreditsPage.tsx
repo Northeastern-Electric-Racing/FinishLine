92ad1e8fed96ddb4cba7fbe4aabaff0e6bd2fb71
Edgar Buttner <buttneredgar@gmail.com>
2025-11-12 14:16:04 -0500

    #3727 Added my name to credits

--- src/frontend/src/pages/CreditsPage/CreditsPage.tsx
+++ src/frontend/src/pages/CreditsPage/CreditsPage.tsx
@@ -299,7 +299,8 @@ const CreditsPage: React.FC = () => {
           '100%': { backgroundPosition: '0% 200%' }
         }
       }
-    }
+    },
+    { name: 'Edgar Buttner', color: '#9E9764' }
   ];
 
   const snark = ['Add your name!', "Shouldn't you do it yourself?", 'Seriously', 'go', 'do', 'it'];
