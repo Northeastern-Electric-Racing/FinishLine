fea458082ea810337ed8db3fd8f9585e343d341b
Aryan0102 <aryansumit0102@gmail.com>
2025-11-17 16:20:19 -0500

    minor change

--- src/backend/tests/unit/rule.test.ts
+++ src/backend/tests/unit/rule.test.ts
@@ -940,3 +940,5 @@ describe('Get Team Rules in Ruleset Type Tests', () => {
     ).rejects.toThrow(new NotFoundException('Team', 'fake-team-id'));
   });
 });
+
+// Minor change to force test to run
