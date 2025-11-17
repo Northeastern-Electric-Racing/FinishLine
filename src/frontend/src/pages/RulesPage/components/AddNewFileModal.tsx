af4f9af0ade855b531bb3b3c03e0457aa030f8fd
Anthony Basko <75228079+nunnyu@users.noreply.github.com>
2025-11-17 18:51:16 -0500

    #3680 The modal now takes in input, and is a proper form modal.

--- src/frontend/src/pages/RulesPage/components/AddNewFileModal.tsx
+++ src/frontend/src/pages/RulesPage/components/AddNewFileModal.tsx
@@ -1,18 +1,43 @@
-import NERModal from '../../../components/NERModal';
+import NERFormModal from '../../../components/NERFormModal';
+import { useForm } from 'react-hook-form';
+import { TextField } from '@mui/material';
 
 interface AddNewFileModalProps {
   open: boolean;
   onHide: () => void;
-  onConfirm: (data: { file: File; name: string; car: string; isActive: boolean }) => Promise<void>;
+  onConfirm: (data: FormData) => Promise<void>;
+}
 
-  isActive?: boolean;
+interface FormData {
+  file: File;
+  name: string;
+  car: string;
+  isActive: boolean;
 }
 
 const AddNewFileModal: React.FC<AddNewFileModalProps> = ({ open, onHide, onConfirm }) => {
+  const { register, handleSubmit, reset } = useForm<FormData>({
+    defaultValues: {
+      name: '',
+      car: '',
+      isActive: false
+    }
+  });
+
   return (
-    <NERModal open={open} onHide={onHide} title="Add New File" hideFormButtons showCloseButton>
-      <text> basic modal visualization </text>
-    </NERModal>
+    <NERFormModal
+      open={open}
+      onHide={onHide}
+      title="Add New File"
+      hideFormButtons
+      showCloseButton
+      reset={reset}
+      handleUseFormSubmit={handleSubmit}
+      onFormSubmit={onConfirm}
+      formId={'add-new-file-form'}
+    >
+      <TextField required autoComplete="off" placeholder={'Name File'} {...register('name')} />
+    </NERFormModal>
   );
 };
 
