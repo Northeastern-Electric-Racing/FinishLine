49b114edb46d1343410ea051e06cbc7ec08b1758
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-12-12 21:51:36 -0500

    #minorupdate

--- src/frontend/src/utils/urls.ts
+++ src/frontend/src/utils/urls.ts
@@ -463,7 +463,6 @@ const calendarEventMarkUserConfirmed = (id: string) => `${calendar()}/event/${id
 const calendarGetSingleEvent = (id: string) => `${calendar()}/event/${id}`;
 const calendarDeleteEvent = (id: string) => `${calendar()}/event/${id}/delete`;
 const calendarEventSetStatus = (id: string) => `${calendar()}/event/${id}/set-status`;
-const calendarFilterEvents = () => `${calendar()}/events/filter`;
 
 /**************** Other Endpoints ****************/
 const version = () => `https://api.github.com/repos/Northeastern-Electric-Racing/FinishLine/releases/latest`;
