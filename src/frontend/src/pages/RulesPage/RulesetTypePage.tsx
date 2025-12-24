01d4192db43deca9b7e2d04dce3be793ed34bf7e
Ciel Bellerose <kcbellerose@icloud.com>
2025-12-24 13:49:19 -0500

    #3549 ruleset type modal

--- src/frontend/src/pages/RulesPage/RulesetTypePage.tsx
+++ src/frontend/src/pages/RulesPage/RulesetTypePage.tsx
@@ -20,6 +20,9 @@ import { datePipe } from '../../utils/pipes';
 import { NERButton } from '../../components/NERButton';
 import { useHistory } from 'react-router-dom';
 import { routes } from '../../utils/routes';
+import AddRulesetTypeModal from './components/AddRulesetTypeModal';
+import { useState } from 'react';
+import { useCreateRulesetType } from '../../hooks/rules.hooks';
 
 type RulesetTypeColumnId = 'id' | 'name' | 'lastUpdated' | 'revisions' | 'actions';
 
@@ -32,6 +35,8 @@ const RulesetTypePage: React.FC = () => {
   const history = useHistory();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [addRulesetTypeModalShow, setAddRulesetTypeModalShow] = useState(false);
 
   const headCells: readonly RulesetTypeHeadCell[] = [
     {
@@ -57,6 +62,16 @@ const RulesetTypePage: React.FC = () => {
     { id: '1', name: 'Ruleset 1', lastUpdated: new Date('2024-01-15'), revisions: 5, actions: 'Edit' },
     { id: '2', name: 'Ruleset 2', lastUpdated: new Date('2024-01-14'), revisions: 3, actions: 'Edit' }
   ];
+
+  const { mutateAsync: createRulesetType } = useCreateRulesetType();
+
+  const handleAddRulesetTypeConfirm = async (data: { name: string }) => {
+    await createRulesetType({ name: data.name });
+  };
+
+  const handleAddRulesetTypeCancel = () => {
+    setAddRulesetTypeModalShow(false);
+  };
 
   return (
     <PageLayout title="Ruleset Types">
@@ -165,29 +180,14 @@ const RulesetTypePage: React.FC = () => {
               justifyContent: { xs: 'center', md: 'flex-end' }
             }}
           >
-            <Button
-              className="viewButton"
-              variant="contained"
-              sx={{
-                borderRadius: '8px',
-                color: '#ededed',
-                backgroundColor: '#dd514c',
-                padding: { xs: '8px 16px', md: '2px 20px' },
-                mb: 1,
-                mr: { xs: 0, md: 2 },
-                display: 'flex',
-                fontSize: { xs: '14px', md: '16px' },
-                fontWeight: 700,
-                textTransform: 'none',
-                width: { xs: '100%', sm: 'auto' },
-                maxWidth: { xs: '300px', sm: 'none' },
-                '&:hover': {
-                  backgroundColor: '#c74340'
-                }
-              }}
-            >
-              Add Ruleset
-            </Button>
+            <NERButton variant="contained" onClick={() => setAddRulesetTypeModalShow(!addRulesetTypeModalShow)}>
+              Add Ruleset Type
+            </NERButton>
+            <AddRulesetTypeModal
+              open={addRulesetTypeModalShow}
+              onHide={handleAddRulesetTypeCancel}
+              onFormSubmit={handleAddRulesetTypeConfirm}
+            />
             {/* Temporary for navigation */}
             <NERButton onClick={() => history.push(`${routes.RULES}/placeholder_ruleset_id`)}>FSAE Ruleset</NERButton>
           </Box>
---
+++ src/frontend/src/pages/RulesPage/components/AddRulesetTypeModal.tsx
@@ -0,0 +1,105 @@
+import { FormControl, FormHelperText, FormLabel, TextField } from '@mui/material';
+import { Box } from '@mui/system';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import NERFormModal from '../../../components/NERFormModal';
+import { useToast } from '../../../hooks/toasts.hooks';
+
+interface RulesetTypeFormData {
+  name: string;
+}
+
+interface AddRulesetTypeModalProps {
+  open: boolean;
+  onHide: () => void;
+  onFormSubmit: (data: RulesetTypeFormData) => Promise<void>;
+}
+
+const sectionHeaderStyle = {
+  fontWeight: 'bold',
+  color: '#ef4345',
+  textDecoration: 'underline',
+  fontSize: '1rem',
+  textUnderlineOffset: '5px',
+  marginBottom: '10px'
+};
+
+const schema = yup.object({
+  name: yup.string().required('Name is required')
+});
+
+const AddRulesetTypeModal: React.FC<AddRulesetTypeModalProps> = ({ open, onHide, onFormSubmit }) => {
+  const toast = useToast();
+
+  const {
+    formState: { errors },
+    handleSubmit,
+    reset,
+    control
+  } = useForm<RulesetTypeFormData>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: ''
+    }
+  });
+
+  const handleFormSubmit = async (data: RulesetTypeFormData) => {
+    try {
+      await onFormSubmit(data);
+      toast.success('Ruleset Type Successfully Added');
+      reset();
+      onHide();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    }
+  };
+
+  const handleModalClose = () => {
+    reset();
+    onHide();
+  };
+
+  const handleReset = () => {
+    reset();
+  };
+
+  return (
+    <NERFormModal
+      open={open}
+      onHide={handleModalClose}
+      title="Add Ruleset Type"
+      reset={handleReset}
+      handleUseFormSubmit={handleSubmit}
+      onFormSubmit={handleFormSubmit}
+      formId={'add-ruleset-type-form'}
+      showCloseButton
+      submitText="Add Ruleset"
+    >
+      <Box>
+        <FormControl fullWidth error={!!errors.name}>
+          <FormLabel sx={sectionHeaderStyle}>Name Ruleset:</FormLabel>
+          <Controller
+            name="name"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                autoComplete="off"
+                placeholder="Name Ruleset"
+                error={!!errors.name}
+                fullWidth
+                sx={{ minWidth: '400px' }}
+              />
+            )}
+          />
+          <FormHelperText error>{errors.name?.message}</FormHelperText>
+        </FormControl>
+      </Box>
+    </NERFormModal>
+  );
+};
+
+export default AddRulesetTypeModal;
