de5d780c6c4b2f6696596376dc025bf5ceaf5bee
JoshuaGoldberg <joshua@barrygoldberg.net>
2026-01-01 20:23:38 -0500

    linting

--- src/frontend/src/pages/NewCalendarPage/NewCalendar.tsx
+++ src/frontend/src/pages/NewCalendarPage/NewCalendar.tsx
@@ -4,7 +4,6 @@
  */
 import { Route, Switch } from 'react-router-dom';
 import { routes } from '../../utils/routes';
-import DesignReviewDetails from '../CalendarPage/EventDetailPage/EventDetails';
 import CalendarTab from './CalendarTab';
 
 const NewCalendar: React.FC = () => {
