3da7f30a8cc8ba5259fecee86b67d492f6c64658
Ciel Bellerose <kcbellerose@icloud.com>
2026-01-02 12:20:46 -0500

    #3844 lint

--- src/frontend/src/pages/RulesPage/RulesetPage.tsx
+++ src/frontend/src/pages/RulesPage/RulesetPage.tsx
@@ -2,7 +2,6 @@
  * This file is part of NER's FinishLine and licensed under GNU AGPLv3.
  * See the LICENSE file in the repository root folder for details.
  */
-import { useHistory } from 'react-router-dom';
 import React from 'react';
 import { NERButton } from '../../components/NERButton';
 import AddNewFileModal from './components/AddNewFileModal';
