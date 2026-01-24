7d605e572e00c2d664ffab7ca880bbae72e34a03
Sarah Taylor <150694563+staysgt@users.noreply.github.com>
2026-01-24 10:15:15 -0500

    #3896 guest page schema changes

--- src/backend/src/prisma/migrations/20260124151347_guest_page_additions/migration.sql
+++ src/backend/src/prisma/migrations/20260124151347_guest_page_additions/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "Definition_Type" AS ENUM ('PROJECT_MANAGEMENT', 'DIVISIONS', 'HOME_PAGE');
+
 -- AlterTable
 ALTER TABLE "Sponsor" ADD COLUMN     "logoImageId" TEXT;
 
@@ -6,17 +9,27 @@ CREATE TABLE "Term_Definition" (
     "definitionId" TEXT NOT NULL,
     "term" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "type" "Definition_Type" NOT NULL,
+    "buttonText" TEXT,
+    "buttonLink" TEXT,
     "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "dateDeleted" TIMESTAMP(3),
     "userDeletedId" TEXT,
     "userCreatedId" TEXT NOT NULL,
     "organizationId" TEXT NOT NULL,
+    "parentTermId" TEXT,
 
     CONSTRAINT "Term_Definition_pkey" PRIMARY KEY ("definitionId")
 );
 
 -- CreateIndex
 CREATE INDEX "Term_Definition_organizationId_idx" ON "Term_Definition"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "Term_Definition_type_idx" ON "Term_Definition"("type");
+
+-- CreateIndex
+CREATE INDEX "Term_Definition_parentTermId_idx" ON "Term_Definition"("parentTermId");
 
 -- AddForeignKey
 ALTER TABLE "Term_Definition" ADD CONSTRAINT "Term_Definition_userDeletedId_fkey" FOREIGN KEY ("userDeletedId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -26,3 +39,6 @@ ALTER TABLE "Term_Definition" ADD CONSTRAINT "Term_Definition_userCreatedId_fkey
 
 -- AddForeignKey
 ALTER TABLE "Term_Definition" ADD CONSTRAINT "Term_Definition_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("organizationId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Term_Definition" ADD CONSTRAINT "Term_Definition_parentTermId_fkey" FOREIGN KEY ("parentTermId") REFERENCES "Term_Definition"("definitionId") ON DELETE SET NULL ON UPDATE CASCADE;
