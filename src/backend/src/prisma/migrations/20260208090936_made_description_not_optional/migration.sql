cc7f6f5ce27018131ab965f3e2ff399678f37432
Tony Feng <tonywf15@gmail.com>
2026-02-08 04:13:25 -0500

    #3776 made migration for description field

---
+++ src/backend/src/prisma/migrations/20260208090936_made_description_not_optional/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Made the column `description` on table `Reimbursement_Request` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Reimbursement_Request" ALTER COLUMN "description" SET NOT NULL;
