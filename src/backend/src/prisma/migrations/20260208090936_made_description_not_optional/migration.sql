895cfc54b301faa975e80e516cb116da09a00c9c
Tony Feng <tonywf15@gmail.com>
2026-02-10 19:18:49 -0500

    #3776 deleted migration for nonnull rr description, made description optional

--- src/backend/src/prisma/migrations/20260208090936_made_description_not_optional/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `description` on table `Reimbursement_Request` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Reimbursement_Request" ALTER COLUMN "description" SET NOT NULL;
