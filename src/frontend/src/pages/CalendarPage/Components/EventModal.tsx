f59e15a5d638963da0d38f7ebdf3390985e34144
sayegh.st@northeastern.edu <stephaniesayegh375@gmail.com>
2026-02-18 19:59:05 -0500

    spacing fix

--- src/frontend/src/pages/CalendarPage/Components/EventModal.tsx
+++ src/frontend/src/pages/CalendarPage/Components/EventModal.tsx
@@ -882,7 +882,7 @@ const EventModal: React.FC<BaseEventModalProps> = ({
                                   error: !!errors.startTime,
                                   helperText: errors.startTime?.message,
                                   onClick: () => setStartTimePickerOpen(true),
-                                  sx: { width: 100 }
+                                  sx: { width: 120 }
                                 },
                                 layout: {
                                   sx: {
@@ -930,7 +930,7 @@ const EventModal: React.FC<BaseEventModalProps> = ({
                                   error: !!errors.endTime,
                                   helperText: errors.endTime?.message,
                                   onClick: () => setEndTimePickerOpen(true),
-                                  sx: { width: 100 }
+                                  sx: { width: 120 }
                                 },
                                 layout: {
                                   sx: {
