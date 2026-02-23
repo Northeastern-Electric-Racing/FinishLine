48667ad32c1b649101f3885a0d3639d52a98f5ec
Richard Feng <richardfeng43@gmail.com>
2026-02-22 23:03:32 -0500

    #3872 fixed homepage

--- src/frontend/src/pages/HomePage/IntroGuestHomePage.tsx
+++ src/frontend/src/pages/HomePage/IntroGuestHomePage.tsx
@@ -7,26 +7,33 @@ import { useCurrentUser } from '../../hooks/users.hooks';
 import { useEffect } from 'react';
 import { useHomePageContext } from '../../app/HomePageContext';
 import { useCurrentOrganization } from '../../hooks/organizations.hooks';
+import LoadingIndicator from '../../components/LoadingIndicator';
 import ErrorPage from '../ErrorPage';
+import { useGetImageUrl } from '../../hooks/onboarding.hook';
 
 const IntroGuestHomePage = () => {
   const user = useCurrentUser();
   const history = useHistory();
-  const { isError: organizationIsError, error: organizationError } = useCurrentOrganization();
+  const {
+    data: organization,
+    isLoading: organizationIsLoading,
+    isError: organizationIsError,
+    error: organizationError
+  } = useCurrentOrganization();
   const { setCurrentHomePage } = useHomePageContext();
 
-  // const {
-  //   data: applyInterestImageUrl,
-  //   isLoading: applyImageLoading,
-  //   isError: applyImageIsError,
-  //   error: applyImageError
-  // } = useGetImageUrl(organization?.applyInterestImageId ?? null);
-  // const {
-  //   data: exploreGuestImageUrl,
-  //   isLoading: exploreImageLoading,
-  //   isError: exploreImageIsError,
-  //   error: exploreImageError
-  // } = useGetImageUrl(organization?.exploreAsGuestImageId ?? null);
+  const {
+    data: applyInterestImageUrl,
+    isLoading: applyImageLoading,
+    isError: applyImageIsError,
+    error: applyImageError
+  } = useGetImageUrl(organization?.applyInterestImageId ?? null);
+  const {
+    data: exploreGuestImageUrl,
+    isLoading: exploreImageLoading,
+    isError: exploreImageIsError,
+    error: exploreImageError
+  } = useGetImageUrl(organization?.exploreAsGuestImageId ?? null);
 
   useEffect(() => {
     setCurrentHomePage('guest');
@@ -35,11 +42,11 @@ const IntroGuestHomePage = () => {
   if (organizationIsError) {
     return <ErrorPage message={organizationError.message} />;
   }
-  // if (applyImageIsError) return <ErrorPage message={applyImageError.message} />;
-  // if (exploreImageIsError) return <ErrorPage message={exploreImageError.message} />;
+  if (applyImageIsError) return <ErrorPage message={applyImageError.message} />;
+  if (exploreImageIsError) return <ErrorPage message={exploreImageError.message} />;
 
-  // if (!organization || organizationIsLoading || applyImageLoading || exploreImageLoading) return <LoadingIndicator />;
-  // if (!applyInterestImageUrl || !exploreGuestImageUrl) return <LoadingIndicator />;
+  if (!organization || organizationIsLoading || applyImageLoading || exploreImageLoading) return <LoadingIndicator />;
+  if (!applyInterestImageUrl || !exploreGuestImageUrl) return <LoadingIndicator />;
 
   return (
     <PageLayout title="Home" hidePageTitle>
@@ -59,13 +66,13 @@ const IntroGuestHomePage = () => {
         <Box sx={{ display: 'flex', gap: 5 }}>
           <ImageWithButton
             title="Interested in applying"
-            imageSrc={''}
+            imageSrc={applyInterestImageUrl}
             buttonText="Learn More"
             onClick={() => history.push(routes.HOME_PNM)}
           />
           <ImageWithButton
             title="Explore Our Work as a Guest"
-            imageSrc={''}
+            imageSrc={exploreGuestImageUrl}
             buttonText="FinishLine"
             onClick={() => history.push(routes.HOME_MEMBER)}
           />
