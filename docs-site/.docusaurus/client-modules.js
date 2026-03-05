a1cfb99d3ea53d6329a3dca2d62037de72b2e9bc
Chris Pyle <pyle.c@northeastern.edu>
2026-03-05 17:18:17 -0500

    accidentally added docs site

--- docs-site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/chrispyle/NER/FinishLine/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/chrispyle/NER/FinishLine/docs-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/chrispyle/NER/FinishLine/docs-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/chrispyle/NER/FinishLine/docs-site/src/css/custom.css"),
-];
