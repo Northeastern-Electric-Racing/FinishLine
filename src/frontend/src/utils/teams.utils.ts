7f1849a26386b1517dc9c359617e67d21e081338
Raphael Bessin <bessin.raphael@gmail.com>
2026-03-08 17:52:38 -0400

    #3884 add Accept back to SubmitText

--- src/frontend/src/utils/teams.utils.ts
+++ src/frontend/src/utils/teams.utils.ts
@@ -46,6 +46,7 @@ export type SubmitText =
   | 'Create Change Request'
   | 'Update'
   | 'Submit Vendor'
+  | 'Accept'
   | 'Copy BOM';
 
 export type CancelText = 'Cancel' | 'Delete' | 'Exit' | 'No';
