a8893d1a2d00f3fb8a22b3d1311db642b05b6d38
Tony Feng <tonywf15@gmail.com>
2026-03-09 00:21:01 -0400

    #3776 combined two migrations into one file

--- src/backend/src/prisma/migrations/20260112034057_add_description_to_reimbursement_request/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Reimbursement_Request" ADD COLUMN     "description" TEXT;
--- src/backend/src/prisma/migrations/20260218232442_make_rr_description_required_with_default/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `description` on table `Reimbursement_Request` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Reimbursement_Request" ALTER COLUMN "description" SET NOT NULL,
-ALTER COLUMN "description" SET DEFAULT '';
