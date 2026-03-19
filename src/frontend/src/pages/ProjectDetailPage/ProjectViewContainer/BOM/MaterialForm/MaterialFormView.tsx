c73b4cdaaef55bd560296086448c05f9b5ded09e
Anthony Basko <75228079+nunnyu@users.noreply.github.com>
2026-03-19 18:58:54 -0400

    #3885: Reverted MaterialFormView from rebase stuff

--- src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOM/MaterialForm/MaterialFormView.tsx
+++ src/frontend/src/pages/ProjectDetailPage/ProjectViewContainer/BOM/MaterialForm/MaterialFormView.tsx
@@ -14,7 +14,7 @@ import {
 } from '@mui/material';
 import { Box } from '@mui/system';
 import { Control, Controller, FieldErrors, UseFormHandleSubmit, UseFormSetValue, UseFormWatch } from 'react-hook-form';
-import { Assembly, Manufacturer, MaterialType, ReimbursementRequest, Unit } from 'shared';
+import { Assembly, Manufacturer, MaterialType, Unit } from 'shared';
 import ReactHookTextField from '../../../../../components/ReactHookTextField';
 import { MaterialFormInput } from './MaterialForm';
 import NERFormModal from '../../../../../components/NERFormModal';
@@ -44,7 +44,6 @@ export interface MaterialFormViewProps {
   setValue: UseFormSetValue<MaterialFormInput>;
   copyFromExistingBomAction?: React.ReactNode;
   fromRRForm?: boolean;
-  reimbursementRequests?: any[];
 }
 
 const manufacturersToAutocomplete = (manufacturer: Manufacturer): { label: string; id: string } => {
@@ -70,8 +69,7 @@ const MaterialFormView: React.FC<MaterialFormViewProps> = ({
   watch,
   createManufacturer,
   setValue,
-  fromRRForm = false,
-  reimbursementRequests = []
+  fromRRForm = false
 }) => {
   const [additionalDetailsOpen, setAdditionalDetailsOpen] = useState(!fromRRForm);
 
@@ -506,94 +504,19 @@ const MaterialFormView: React.FC<MaterialFormViewProps> = ({
           )}
         </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Grid item xs={12} mt={2}>
-          <FormControl fullWidth>
-            <Controller
-              name="reimbursementRequestId"
-              control={control}
-              defaultValue={control._defaultValues.reimbursementRequestId}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  select
-                  variant="outlined"
-                  error={!!errors.reimbursementRequestId}
-                  helperText={errors.reimbursementRequestId?.message}
-                  SelectProps={{
-                    displayEmpty: true,
-                    renderValue: (selected) =>
-                      selected ? (
-                        reimbursementRequests.find((rr) => rr.reimbursementRequestId === selected)?.identifier
-                      ) : (
-                        <Typography sx={{ fontSize: '1rem', color: 'lightgray', opacity: 0.6 }}>
-                          Select Corresponding Reimbursement Request Number
-                        </Typography>
-                      )
-                  }}
-                >
-                  {reimbursementRequests.map((rr: ReimbursementRequest) => (
-                    <MenuItem key={rr.reimbursementRequestId} value={rr.reimbursementRequestId}>
-                      {rr.identifier}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              )}
-            />
-          </FormControl>
-        </Grid>
-        <Box display={'flex'} justifyContent={'flex-end'} mt={2}>
-          <FormControl fullWidth>
-            <Controller
-              name="assemblyId"
-              control={control}
-              defaultValue={control._defaultValues.assemblyId}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  select
-                  variant="outlined"
-                  error={!!errors.assemblyId}
-                  helperText={errors.assemblyId?.message}
-                  SelectProps={{
-                    displayEmpty: true,
-                    renderValue: (selected) =>
-                      selected ? (
-                        assemblies?.find((a) => a.assemblyId === selected)?.name
-                      ) : (
-                        <Typography sx={{ fontSize: '1rem', color: 'lightgray', opacity: 0.6 }}>
-                          Enter Assembly Details
-                        </Typography>
-                      )
-                  }}
-                >
-                  {assemblies?.map((assembly) => (
-                    <MenuItem key={assembly.assemblyId} value={assembly.assemblyId}>
-                      {assembly.name}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              )}
-            />
-          </FormControl>
-        </Box>
-      </Grid>
-      {submitText === 'Add' && (
+      {/*submitText === 'Add' && (
         <Grid item xs={12} sx={{ pl: 0, pr: 0 }}>
           <Box
             sx={{
               pt: 1
             }}
           >
-            {/* <Button
+            <Button
               variant="contained"
               disableElevation
-              onClick={() => {
-                setBomConfirmOpen(true);
-              }}
+              onClick={() => {}}
               sx={{
                 mx: 0,
-                my: 1,
                 textTransform: 'none',
                 bgcolor: '#EF4345',
                 color: (t) => t.palette.getContrastText('#EF4345'),
@@ -601,10 +524,10 @@ const MaterialFormView: React.FC<MaterialFormViewProps> = ({
               }}
             >
               COPY FROM EXISTING BOM
-            </Button> */}
+            </Button>
           </Box>
         </Grid>
-      )}
+      )*/}
     </NERFormModal>
   );
 };
