036865fbc4da419f1f15352c9c7f91f66558e070
Tony Feng <tonywf15@gmail.com>
2026-04-18 20:00:57 -0400

    #4090 changed migration to backfill sendslacknotifs for events based on eventtypes

--- src/backend/src/prisma/migrations/20260416232819_move_send_slack_notifs_to_event/migration.sql
+++ src/backend/src/prisma/migrations/20260416232819_move_send_slack_notifs_to_event/migration.sql
@@ -1,5 +1,12 @@
 -- AlterTable
 ALTER TABLE "Event" ADD COLUMN     "sendSlackNotifications" BOOLEAN NOT NULL DEFAULT false;
 
+-- Backfill Event.sendSlackNotifications from Event_Type before dropping the old column
+UPDATE "Event" e
+SET "sendSlackNotifications" = true
+FROM "Event_Type" et
+WHERE e."eventTypeId" = et."eventTypeId"
+AND et."sendSlackNotifications" = true;
+
 -- AlterTable
 ALTER TABLE "Event_Type" DROP COLUMN "sendSlackNotifications";
