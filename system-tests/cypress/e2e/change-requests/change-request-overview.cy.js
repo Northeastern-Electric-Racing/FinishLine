8d3c591d206d284dca45dc2052bd46f162ad04ba
wavehassman <wavehassman@gmail.com>
2026-05-13 18:13:38 -0400

    hopefully works now

--- system-tests/cypress/e2e/change-requests/change-request-overview.cy.js
+++ system-tests/cypress/e2e/change-requests/change-request-overview.cy.js
@@ -1,15 +1,45 @@
-/* eslint-disable no-undef */
-import { PROJECT_OR_WORKPACKAGE_PLACEHOLDER, SUBMIT_BUTTON, CR_ROW } from './selectors.utils';
-import { INCLUDE } from './cypress-actions.utils';
+/// <reference types="cypress" />
+import {
+  CR_ROW,
+  NEW_CHANGE_REQUEST_BUTTON,
+  ALL_CHANGE_REQUESTS_TAB,
+  CHANGE_REQUEST_TABLE
+} from '../../utils/selectors.utils';
 
-export const createChangeRequest = ({ wbsTitle = '25.1.0 - Impact Attenuator', why = 'test why' }) => {
-  cy.get(PROJECT_OR_WORKPACKAGE_PLACEHOLDER).click();
-  cy.contains(wbsTitle).click();
+import { LENGTH_GREATER_THAN, INCLUDE } from '../../utils/cypress-actions.utils';
 
-  cy.contains('Why are you making this change?').parent().find('textarea').first().type(why);
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
 
-  cy.contains(SUBMIT_BUTTON).click();
-  cy.url().should(INCLUDE, '/change-requests');
+describe('Change Request Overview', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.login('Thomas Emrax', '/change-requests');
+  });
 
-  cy.get(CR_ROW('Un-reviewed Change Requests')).contains('Change Request').should('exist');
-};
+  it('Change Requests to Review Should Display At Least One CR', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    cy.get(CR_ROW('Change Requests To Review')).children().should(LENGTH_GREATER_THAN, 0);
+  });
+
+  it('My Aproved Change Requests Should Display At Least Three CRs', () => {
+    cy.get(CR_ROW('My Approved Change Requests')).children().should(LENGTH_GREATER_THAN, 2);
+  });
+
+  it('New Change Request Button Redirects to New Change Requeest Form', () => {
+    cy.contains(NEW_CHANGE_REQUEST_BUTTON).click();
+
+    cy.url().should(INCLUDE, '/change-requests/new');
+  });
+
+  it('Can Switch to All Change Requests Table', () => {
+    cy.contains(ALL_CHANGE_REQUESTS_TAB).click();
+    cy.url().should(INCLUDE, '/change-requests/all');
+    cy.get(CHANGE_REQUEST_TABLE);
+  });
+});
