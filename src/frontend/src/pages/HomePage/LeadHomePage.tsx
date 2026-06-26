1fa581e0f3b784e0c16517dbe9701c11fc793435
wavehassman <wavehassman@gmail.com>
2026-06-25 20:51:09 -0400

    migration + frontend change

--- src/frontend/src/pages/HomePage/LeadHomePage.tsx
+++ src/frontend/src/pages/HomePage/LeadHomePage.tsx
@@ -21,6 +21,9 @@ const LeadHomePage = ({ user }: LeadHomePageProps) => {
       <Typography variant="h3" marginLeft="auto" sx={{ marginTop: 2, textAlign: 'center', pt: 3, padding: 0 }}>
         Welcome, {user.firstName}!
       </Typography>
+      <Typography variant="h6" sx={{ textAlign: 'center', color: 'warning.main' }}>
+        🚧 Sandbox Environment
+      </Typography>
       <Box
         sx={{
           display: 'flex',
