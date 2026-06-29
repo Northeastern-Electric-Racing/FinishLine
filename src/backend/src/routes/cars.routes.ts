c64f55de20d2b79a805ea400c563c0de49658cb0
Sarah Taylor <150694563+staysgt@users.noreply.github.com>
2026-06-28 20:16:27 -0400

    #4260 validate inputs

--- src/backend/src/routes/cars.routes.ts
+++ src/backend/src/routes/cars.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import CarsController from '../controllers/cars.controllers.js';
-import { nonEmptyString } from '../utils/validation.utils.js';
+import { nonEmptyString, validateInputs } from '../utils/validation.utils.js';
 import { body } from 'express-validator';
 
 const carsRouter = express.Router();
@@ -8,6 +8,6 @@ const carsRouter = express.Router();
 carsRouter.get('/', CarsController.getAllCars);
 
 carsRouter.post('/create', CarsController.createCar);
-carsRouter.post('/:carId/edit', nonEmptyString(body('name')), CarsController.editCar);
+carsRouter.post('/:carId/edit', nonEmptyString(body('name')), validateInputs, CarsController.editCar);
 
 export default carsRouter;
