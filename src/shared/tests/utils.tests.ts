4bb0ef4cb36cd13406c79d1c25174da6e833392c
Gil Glick <glick.g@northeastern.edu>
2026-09-16 21:05:45 -0400

    tests

--- src/shared/tests/utils.tests.ts
+++ src/shared/tests/utils.tests.ts
@@ -10,6 +10,25 @@ test('newDate is the same as scheduledDates', () => {
 
 test('newDate is a day later than scheduledDates', () => {
   const newDate = new Date(2026, 6, 2);
-  const scheduledDate = new Date(2026, 6, 2);
+  const scheduledDate = new Date(2026, 6, 3);
   expect(equal(isWithinBuffer(newDate, scheduledDate, 2), true));
+});
+
+test('newDate is two days later than scheduledDates', () => {
+  const newDate = new Date(2026, 6, 2);
+  const scheduledDate = new Date(2026, 6, 4);
+  expect(equal(isWithinBuffer(newDate, scheduledDate, 2), true));
+});
+
+test('newDate is three days later than scheduledDates', () => {
+  const newDate = new Date(2026, 6, 2);
+  const scheduledDate = new Date(2026, 6, 5);
+  expect(equal(isWithinBuffer(newDate, scheduledDate, 2), false));
+});
+
+test('newDate is two days later than scheduledDates', () => {
+  const newDate = new Date(2026, 6, 2);
+  const scheduledDateForward = new Date(2026, 6, 3);
+  const scheduledDateBackward = new Date(2026, 6, 1);
+  expect(equal(isWithinBuffer(newDate, scheduledDateForward, 2), isWithinBuffer(newDate, scheduledDateBackward, 2)));
 });
