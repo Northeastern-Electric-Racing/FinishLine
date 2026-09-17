48a4cf230072fb8b9dad4a4084d88a1ed1a03652
Gil Glick <glick.g@northeastern.edu>
2026-09-16 21:39:47 -0400

    updated isWithinBuffer

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -6,8 +6,8 @@
  * @param bufferDays The number of days apart the two days can be
  */
 export const isWithinBuffer = (scheduledDate: Date, newDate: Date, bufferDays: number) => {
-  const UTCScheduledDate = Date.UTC(scheduledDate.getFullYear(), scheduledDate.getMonth(), scheduledDate.getDay());
-  const UTCNewDate = Date.UTC(newDate.getFullYear(), newDate.getMonth(), newDate.getDay());
+  const UTCScheduledDate = Date.UTC(scheduledDate.getFullYear(), scheduledDate.getMonth(), scheduledDate.getDate());
+  const UTCNewDate = Date.UTC(newDate.getFullYear(), newDate.getMonth(), newDate.getDate());
 
   const millisecondDifference = UTCNewDate - UTCScheduledDate;
   const dayDifference = Math.floor(Math.abs(millisecondDifference / (1000 * 3600 * 24)));
