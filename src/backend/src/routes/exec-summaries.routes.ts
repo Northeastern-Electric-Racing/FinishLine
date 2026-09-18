358480b26b1582f80a221832e70bc060d09d7b7d
Sarah Taylor <150694563+staysgt@users.noreply.github.com>
2026-09-17 21:53:07 -0400

    #4502 export default router

--- src/backend/src/routes/exec-summaries.routes.ts
+++ src/backend/src/routes/exec-summaries.routes.ts
@@ -4,3 +4,5 @@ import ExecSummaryController from '../controllers/exec-summaries.controllers.js'
 const execSummaryRouter = express.Router();
 
 execSummaryRouter.get('/:execSummaryId', ExecSummaryController.getSingleExecutiveSummary);
+
+export default execSummaryRouter;
